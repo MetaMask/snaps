f07c869699a33087e86bc4f211e22061b49ce966
Dan Miller <danjm.com@gmail.com>
2019-09-05 15:49:27 -0230

    Add bundling file for cli

---
+++ mm-plugin.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+
+const [,, ...args] = process.argv
+let [command, source, flag, destination] = args
+
+if (flag === '-o' && !destination) {
+  destination = source.match(/(.+)\.js/)[1] + 'plugin.json'
+}
+
+if (command === 'build') {
+  fs.readFile(source, 'utf8', function(err, contents) {
+      const sourceCode = contents
+      const requestedPermissions = contents
+        .match(/ethereum\.[A-z0-9_\-$]+/g)
+        .reduce((acc, current) => ({ ...acc, [current.split('.')[1]]: {} }), {})
+      
+      const bundledPlugin = JSON.stringify({
+        sourceCode,
+        requestedPermissions,
+      }, null, 2)
+
+      fs.writeFile(destination, bundledPlugin, 'utf8', (err) => {
+        if (err) return console.log(err);
+        console.log('Plugin bundled!');
+      })
+  });
+}
