08acf528e86aacf7dd021f1fed6d1a238e4c866d
Dan Miller <danjm.com@gmail.com>
2019-09-06 14:37:07 -0230

    Use 'ethereumProvider' as global

--- mm-plugin.js
+++ mm-plugin.js
@@ -13,14 +13,14 @@ if (command === 'build') {
   fs.readFile(source, 'utf8', function(err, contents) {
       const sourceCode = contents
       const requestedPermissions = contents
-        .match(/ethereum\.[A-z0-9_\-$]+/g)
+        .match(/ethereumProvider\.[A-z0-9_\-$]+/g)
         .reduce((acc, current) => ({ ...acc, [current.split('.')[1]]: {} }), {})
       
       const bundledPlugin = JSON.stringify({
         sourceCode,
         requestedPermissions,
       }, null, 2)
-
+      
       fs.writeFile(destination, bundledPlugin, 'utf8', (err) => {
         if (err) return console.log(err);
         console.log('Plugin bundled!');
