1c322113b055817a32d95d41f4e6424eae5eb7e3
Joel Thorstensson <oed@3box.io>
2019-10-24 09:20:51 -0400

    Implement 3ID Provider example (#29)

---
+++ examples/3box/index.js
@@ -0,0 +1,10 @@
+const IdentityWallet = require('identity-wallet')
+
+let threeIdProvider
+wallet.registerRpcMessageHandler(async (origin, req) => {
+  if (!threeIdProvider) {
+    const idWallet = new IdentityWallet({ seed: '0x' + (await wallet.getAppKey()) })
+    threeIdProvider = idWallet.get3idProvider()
+  }
+  return threeIdProvider.send(req, origin)
+})
