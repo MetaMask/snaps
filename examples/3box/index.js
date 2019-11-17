58e7993321ac03e7b1497ce0389b92ec94dfe737
Joel Thorstensson <oed3933@gmail.com>
2019-11-17 17:51:32 +0100

    Fix 3box example (#39)

--- examples/3box/index.js
+++ examples/3box/index.js
@@ -3,7 +3,7 @@ const IdentityWallet = require('identity-wallet')
 let threeIdProvider
 wallet.registerRpcMessageHandler(async (origin, req) => {
   if (!threeIdProvider) {
-    const idWallet = new IdentityWallet({ seed: '0x' + (await wallet.getAppKey()) })
+    const idWallet = new IdentityWallet(() => true, { seed: '0x' + (await wallet.getAppKey()) })
     threeIdProvider = idWallet.get3idProvider()
   }
   return threeIdProvider.send(req, origin)
