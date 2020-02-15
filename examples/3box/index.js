146b4a1d0eb152f56e0fd7186f06f63303f2485a
Erik Marks <rekmarks@protonmail.com>
2020-02-15 15:51:05 -0700

    fix 3box frontend errors, SES errors remain

--- examples/3box/index.js
+++ examples/3box/index.js
@@ -3,8 +3,17 @@ const IdentityWallet = require('identity-wallet')
 let threeIdProvider
 wallet.registerRpcMessageHandler(async (origin, req) => {
   if (!threeIdProvider) {
-    const idWallet = new IdentityWallet(() => true, { seed: '0x' + (await wallet.getAppKey()) })
+    const appKey = await getAppKey()
+    const idWallet = new IdentityWallet(() => true, { seed: appKey })
     threeIdProvider = idWallet.get3idProvider()
   }
   return threeIdProvider.send(req, origin)
 })
+
+async function getAppKey () {
+  const appKey = await wallet.getAppKey()
+  if (!appKey.startsWith('0x')) {
+    return '0x' + appKey
+  }
+  return appKey
+}
