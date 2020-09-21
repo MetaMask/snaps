8101a0f3a228451f5f14568b61385dd4033618de
Erik Marks <rekmarks@protonmail.com>
2020-09-20 19:56:45 -0700

    Update post-message-stream

--- packages/snap-workers/src/lockdown.cjs
+++ packages/snap-workers/src/lockdown.cjs
@@ -3334,6 +3334,27 @@ const InertCompartment = function Compartment(
   throw new TypeError('Not available');
 };
 
+// It is not clear that
+// `StaticModuleRecord.prototype.constructor` needs to be the
+// useless `InertStaticModuleRecord` rather than
+// `StaticModuleRecord` itself. The reason we're starting off
+// extra caution is that `StaticModuleRecord` would be the only
+// remaining universally shared primordial reachable by navigation
+// that can turn strings of code into a representation closer to
+// code execution. The others, `eval`, `Function`, and `Compartment`
+// are already protected, and only `Compartment` can turn a
+// `StaticModuleRecord` into execution, which is why it would
+// probably be safe. However, since this extra caution has a tiny
+// cost, I'd rather start out more restrictive, maintaining the option
+// to loosen the rule over time.
+//
+const InertStaticModuleRecord = function StaticModuleRecord(
+  _string,
+  _url,
+) {
+  throw new TypeError('Not available');
+};
+
 /**
  * Object.getConstructorOf()
  * Helper function to improve readability, similar to Object.getPrototypeOf().
@@ -3441,6 +3462,7 @@ function getAnonymousIntrinsics() {
     '%ThrowTypeError%': ThrowTypeError,
     '%TypedArray%': TypedArray,
     '%InertCompartment%': InertCompartment,
+    // '%InertStaticModuleRecord%': InertStaticModuleRecord,
   };
 
   return intrinsics;
