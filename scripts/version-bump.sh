20a5a3b51bdfbf9fbaa2ded34ce288fadff252eb
Erik Marks <rekmarks@protonmail.com>
2021-02-25 09:46:49 -0800

    monorepo: Consolidate version bump scripts

--- scripts/version-bump.sh
+++ scripts/version-bump.sh
@@ -10,4 +10,15 @@ then
   exit 1
 fi
 
+# bump version of all packages
 lerna version "$1" --no-git-tag-version
+
+# get new version, create and push release branch
+MM_SNAP_VERSION=$(node -p 'require("./lerna.json").version')
+
+git checkout -b "${MM_SNAP_VERSION}"
+git add .
+git commit -m "${MM_SNAP_VERSION}" || true
+git push -u origin "${MM_SNAP_VERSION}"
+
+unset MM_SNAP_VERSION
