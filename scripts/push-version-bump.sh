2e9dec3ede9f1f19897364dc398a355b69126c6c
Erik Marks <rekmarks@protonmail.com>
2021-02-25 09:31:00 -0800

    monorepo: Update push-version-bump script

--- scripts/push-version-bump.sh
+++ scripts/push-version-bump.sh
@@ -7,6 +7,7 @@ set -o pipefail
 MM_SNAP_VERSION=$(node -p 'require("./lerna.json").version')
 
 git checkout -b "${MM_SNAP_VERSION}"
+git add .
 git commit -m "${MM_SNAP_VERSION}" || true
 git push -u origin "${MM_SNAP_VERSION}"
 
