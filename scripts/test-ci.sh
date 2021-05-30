81b65600d207cbbddaed068a3f43a68cabe84131
Erik Marks <25517051+rekmarks@users.noreply.github.com>
2021-05-30 11:06:54 -0700

    Standardize repository, remove lerna (#16)
    Performs a bunch of repository standardization work, and removes `lerna`. In detail:
    - Add composite TypeScript project configuration
      - This allows us to specify package dependencies using the [`references` tsconfig property](https://www.typescriptlang.org/docs/handbook/project-references.html), and have a single build command in the repository root.
    - Remove `lerna` and all related files, scripts, and configs
    - Add CI using GitHub Actions
    - Add release automation via our GitHub Actions
    - Add our standard `.yarnrc` and `.gitattributes` files to the repository root
    - Update lint scripts to lint more non-JS/TS files with `prettier`, add `eslint` caching
    - Add `@lavamoat/allow-scripts`, `setup` command
    - Add monorepo-wide `build` and `test` scripts to root `package.json` via `yarn workspaces run`
      - The `lint` script already linted the entire repository.
    - Add updated installation, building, linting, and testing instructions to readme
    - Trim useless entries from repository `.gitignore`

---
+++ scripts/test-ci.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -x
+set -e
+set -u
+set -o pipefail
+
+# We have to use xvfb due to electron
+# Ref: https://github.com/facebook-atom/jest-electron-runner/issues/47#issuecomment-508556407
+xvfb-run -e /dev/stdout yarn workspace @mm-snap/controllers test
