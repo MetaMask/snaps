b4c487ba578d9f218dd70d7afa50aa666398d938
shanejonas <jonas.shane@gmail.com>
2021-07-22 11:48:17 -0700

    remove eslint comments

--- packages/controllers/jest-setup.js
+++ packages/controllers/jest-setup.js
@@ -1,13 +1,7 @@
 // eslint-disable-next-line import/no-unassigned-import
 require('ses');
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-
-// eslint-disable-next-line no-proto
 console.log('about to lockdown');
-
-// eslint-disable-next-line no-proto
-console.log(delete ArrayBuffer.__proto__);
 
 global.lockdown({
   // TODO: Which would we use in prod?
--- packages/controllers/src/services/IframeExecutionEnvironmentService.test.ts
+++ packages/controllers/src/services/IframeExecutionEnvironmentService.test.ts
@@ -2,29 +2,27 @@ import { IframeExecutionEnvironmentService } from './IframeExecutionEnvironmentS
 
 describe('Iframe Controller', () => {
   it('can boot', async () => {
-    const webWorkerExecutionEnvironmentService = new IframeExecutionEnvironmentService(
-      {
+    const webWorkerExecutionEnvironmentService =
+      new IframeExecutionEnvironmentService({
         setupPluginProvider: () => {
           // do nothing
         },
         iframeUrl: new URL(
           'https://metamask.github.io/iframe-execution-environment/',
         ),
-      },
-    );
+      });
     expect(webWorkerExecutionEnvironmentService).toBeDefined();
   });
   it('can create a plugin worker and start the plugin', async () => {
-    const webWorkerExecutionEnvironmentService = new IframeExecutionEnvironmentService(
-      {
+    const webWorkerExecutionEnvironmentService =
+      new IframeExecutionEnvironmentService({
         setupPluginProvider: () => {
           // do nothing
         },
         iframeUrl: new URL(
           'https://metamask.github.io/iframe-execution-environment/',
         ),
-      },
-    );
+      });
     const pluginName = 'foo.bar.baz';
     const response = await webWorkerExecutionEnvironmentService.executePlugin({
       pluginName,
