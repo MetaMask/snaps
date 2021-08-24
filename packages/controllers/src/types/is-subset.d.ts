48289f739636a4c9589dded9a5a715e8c8229c3d
Erik Marks <rekmarks@protonmail.com>
2021-08-23 18:38:17 -0700

    Remove some cruft

--- packages/controllers/src/types/is-subset.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'is-subset' {
-  export type intersectObjects = (
-    arg1?: Record<string, any>,
-    arg2?: Record<string, any>,
-  ) => Record<string, any>;
-  export default function isSubset(superset: unknown, subset: unknown): boolean;
-}
