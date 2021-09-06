MetaMask/snaps

commit 9571f07e96a38e1d21f580c126b3ab0d856f2d78

Improve test coverage (#126)
Closes #74.

This PR adds unit tests for `cli.ts`, and achieves 100% coverage for everything except branches, which are left at 86%. The lower branch coverage is mainly due to solitary `if` statements whose implicit `else` clauses are never tested.

In order to achieve the increased test coverage, some internal files have been refactored. All changes should be non-breaking.

src/cmds/eval/workerEval.ts
@@ -1,12 +1,9 @@
 import { Worker } from 'worker_threads';
 import pathUtils from 'path';
 
-export function workerEval(
-  bundlePath: string,
-  getWorker = getEvalWorker,
-): Promise<null> {
+export function workerEval(bundlePath: string): Promise<null> {
   return new Promise((resolve, _reject) => {
-    getWorker(getEvalWorkerPath())
+    new Worker(getEvalWorkerPath())
       .on('exit', (exitCode: number) => {
         if (exitCode === 0) {
           resolve(null);
@@ -25,13 +22,4 @@ export function workerEval(
  */
 function getEvalWorkerPath(): string {
   return pathUtils.join(__dirname, 'evalWorker.js');
-}
-
-/**
- * @param path - The path to the file which the worker will execute.
- * @returns The worker object.
- */
-export function getEvalWorker(path: string): Worker {
-  /* istanbul ignore next */
-  return new Worker(path);
 }

src/cmds/eval/workerEval.test.ts
@@ -1,0 +1,78 @@
+import EventEmitter from 'events';
+import pathUtils from 'path';
+import { workerEval } from './workerEval';
+
+interface MockWorkerInterface extends EventEmitter {
+  constructorArgs: unknown[];
+  postMessage: () => void;
+}
+
+let mockWorkerRef: MockWorkerInterface;
+
+jest.mock('worker_threads', () => ({
+  Worker: class MockWorker extends EventEmitter implements MockWorkerInterface {
+    public constructorArgs: unknown[];
+
+    postMessage: () => void;
+
+    constructor(...args: unknown[]) {
+      super();
+      if (mockWorkerRef) {
+        throw new Error('Mock worker ref already assigned!');
+      }
+
+      // eslint-disable-next-line consistent-this
+      mockWorkerRef = this;
+      this.constructorArgs = args;
+      this.on = jest.spyOn(this as any, 'on') as any;
+      // eslint-disable-next-line jest/prefer-spy-on
+      this.postMessage = jest.fn();
+    }
+  },
+}));
+
+describe('workerEval', () => {
+  const mockBundlePath = './snap.js';
+  const workerPathRegex = /evalWorker\.js$/u;
+
+  beforeEach(() => {
+    jest.spyOn(pathUtils, 'join');
+    (mockWorkerRef as any) = undefined;
+  });
+
+  afterEach(() => {
+    global.snaps = {};
+  });
+
+  it('worker eval handles 0 exit code', async () => {
+    const evalPromise = workerEval(mockBundlePath);
+    mockWorkerRef.emit('exit', 0);
+    const result = await evalPromise;
+
+    expect(result).toBeNull();
+    expect(mockWorkerRef.constructorArgs).toStrictEqual([
+      expect.stringMatching(workerPathRegex),
+    ]);
+    expect(mockWorkerRef.on).toHaveBeenCalledTimes(1);
+    expect(mockWorkerRef.postMessage).toHaveBeenCalledWith({
+      pluginFilePath: mockBundlePath,
+    });
+  });
+
+  it('worker eval handles non-0 exit code', async () => {
+    const exitCode = 1;
+    await expect(async () => {
+      const evalPromise = workerEval(mockBundlePath);
+      mockWorkerRef.emit('exit', exitCode);
+      await evalPromise;
+    }).rejects.toThrow(`Worker exited abnormally! Code: ${exitCode}`);
+
+    expect(mockWorkerRef.constructorArgs).toStrictEqual([
+      expect.stringMatching(workerPathRegex),
+    ]);
+    expect(mockWorkerRef.on).toHaveBeenCalledTimes(1);
+    expect(mockWorkerRef.postMessage).toHaveBeenCalledWith({
+      pluginFilePath: mockBundlePath,
+    });
+  });
+});

development/buildExamples.ts
@@ -2,7 +2,7 @@ import { promises as fs } from 'fs';
 import { resolve } from 'path';
 import execa from 'execa';
 
-import { build } from '../src/cmds/build';
+import { build } from '../src/cmds/build/buildHandler';
 import { SnapsCliGlobals } from '../src/types/package';
 
 // mock the snaps global

jest.config.js
@@ -1,15 +1,24 @@
 module.exports = {
   collectCoverage: true,
+  // Ensures that we collect coverage from all source files, not just tested
+  // ones.
+  collectCoverageFrom: ['./src/**/*.ts'],
+  coveragePathIgnorePatterns: [
+    // These are just type declarations.
+    './src/types/*',
+    // TODO: Test the eval worker
+    './src/cmds/eval/eval-worker.ts',
+  ],
   coverageReporters: ['text', 'html'],
   coverageThreshold: {
     global: {
-      branches: 85,
-      functions: 85,
+      branches: 86,
+      functions: 100,
       lines: 100,
-      statements: 99,
+      statements: 100,
     },
   },
-  moduleFileExtensions: ['ts', 'js', 'json'],
+  moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'node'],
   preset: 'ts-jest',
   // "resetMocks" resets all mocks, including mocked modules, to jest.fn(),
   // between each test case.

src/builders.ts
@@ -43,6 +43,13 @@ const builders: Builders = {
     type: 'number',
     demandOption: true,
     default: 8081,
+    coerce: (arg: unknown) => {
+      const port = Number.parseInt(String(arg), 10);
+      if (Number.isNaN(port)) {
+        throw new Error(`Invalid port: ${arg}`);
+      }
+      return port;
+    },
   },
 
   sourceMaps: {

src/cli.test.ts
@@ -1,0 +1,149 @@
+import yargs from 'yargs';
+import { cli } from './cli';
+import commands from './cmds';
+
+const commandMap = ((commands as unknown) as yargs.CommandModule[]).reduce(
+  (map, commandModule) => {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    map[commandModule.command![0]] = commandModule;
+    return map;
+  },
+  {} as Record<string, yargs.CommandModule>,
+);
+
+const getMockArgv = (...args: string[]) => {
+  return ['/mock/path', '/mock/entry/path', ...args];
+};
+
+// The ".+" is because the CLI name (specified to yargs as "$0") is
+// populated programmatically based on the name of entry point file.
+// In Jest, that's sometimes "childProcess.js", sometimes other things.
+// In practice, it should always be "mm-snap".
+const HELP_TEXT_REGEX = /^\s*Usage: .+ <command> \[options\]/u;
+
+describe('cli', () => {
+  let consoleLogSpy: jest.SpyInstance;
+  let processExitSpy: jest.SpyInstance;
+
+  beforeAll(() => {
+    global.snaps = {};
+  });
+
+  beforeEach(() => {
+    consoleLogSpy = jest.spyOn(console, 'log');
+    processExitSpy = jest.spyOn(process, 'exit');
+  });
+
+  afterAll(() => {
+    delete (global as any).snaps;
+  });
+
+  it('exits if no argument was provided', () => {
+    consoleLogSpy.mockImplementationOnce((message: string) => {
+      expect(message).toMatch(HELP_TEXT_REGEX);
+    });
+    processExitSpy.mockImplementationOnce(() => {
+      throw new Error('process exited');
+    });
+
+    expect(() => cli(getMockArgv('--help'), commands)).toThrow(
+      'process exited',
+    );
+  });
+
+  it('calls "help" command', async () => {
+    processExitSpy.mockImplementationOnce((code: number) => {
+      expect(code).toStrictEqual(0);
+    });
+
+    await new Promise<void>((resolve) => {
+      consoleLogSpy.mockImplementationOnce((message: string) => {
+        expect(message).toMatch(HELP_TEXT_REGEX);
+        resolve();
+      });
+
+      cli(getMockArgv('--help'), commands);
+    });
+  });
+
+  // Commands are fully tested in their respective unit tests.
+  // This is just to ensure full coverage of cli.ts.
+  describe('locally defined commands', () => {
+    beforeEach(() => {
+      processExitSpy.mockImplementation();
+    });
+    Object.keys(commandMap).forEach((command) => {
+      it(`calls ${command}`, async () => {
+        const mockCommandHandler = jest.fn();
+
+        const finished = new Promise<void>((resolve) => {
+          mockCommandHandler.mockImplementation(() => resolve() as any);
+        });
+
+        cli(getMockArgv(command), [
+          { ...(commandMap as any)[command], handler: mockCommandHandler },
+        ]);
+        await finished;
+        expect(mockCommandHandler).toHaveBeenCalledTimes(1);
+        // TODO: Test the complete argv for each command
+        expect(mockCommandHandler).toHaveBeenCalledWith(
+          expect.objectContaining({
+            _: [command],
+            suppressWarnings: false,
+            verboseErrors: false,
+          }),
+        );
+      });
+    });
+  });
+
+  describe('command failures', () => {
+    it('handles an argument validation failure for a locally defined command', () => {
+      processExitSpy.mockImplementationOnce(() => {
+        throw new Error('process exited');
+      });
+      const consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation();
+      const mockInitHandler = jest.fn();
+
+      expect(() =>
+        cli(getMockArgv('init', '--port', 'not-a-number'), [
+          { ...commandMap.init, handler: mockInitHandler },
+        ]),
+      ).toThrow('process exited');
+
+      expect(consoleErrorSpy).toHaveBeenCalledTimes(1);
+      expect(consoleErrorSpy).toHaveBeenCalledWith('Invalid port: NaN');
+      expect(mockInitHandler).not.toHaveBeenCalled();
+    });
+
+    it('handles an argument validation failure for a locally defined command, with verbose errors', () => {
+      processExitSpy.mockImplementationOnce(() => {
+        throw new Error('process exited');
+      });
+      const consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation();
+      const mockInitHandler = jest.fn();
+
+      expect(() =>
+        cli(getMockArgv('init', '--port', 'not-a-number', `--verboseErrors`), [
+          { ...commandMap.init, handler: mockInitHandler },
+        ]),
+      ).toThrow('process exited');
+
+      expect(consoleErrorSpy).toHaveBeenCalledTimes(2);
+      expect(consoleErrorSpy).toHaveBeenNthCalledWith(1, 'Invalid port: NaN');
+      expect(mockInitHandler).not.toHaveBeenCalled();
+    });
+
+    it('handles an error thrown by a locally defined command handler', () => {
+      const mockInitHandler = jest.fn().mockImplementation(() => {
+        throw new Error('init failed');
+      });
+
+      expect(() =>
+        cli(getMockArgv('init'), [
+          { ...commandMap.init, handler: mockInitHandler },
+        ]),
+      ).toThrow('init failed');
+    });
+  });
+});

src/cmds/build/build.test.ts
@@ -1,8 +1,10 @@
-import * as evalModule from '../eval';
-import * as manifestModule from '../manifest/manifest';
+import * as snapEvalModule from '../eval/evalHandler';
+import * as manifestModule from '../manifest/manifestHandler';
 import * as fsUtils from '../../utils/validate-fs';
 import * as buildBundle from './bundle';
-import { build } from '.';
+import buildModule from '.';
+
+const build = buildModule.handler;
 
 describe('build', () => {
   describe('build', () => {
@@ -32,7 +34,9 @@ describe('build', () => {
       const bundleMock = jest
         .spyOn(buildBundle, 'bundle')
         .mockImplementation(async () => true);
-      const evalMock = jest.spyOn(evalModule, 'snapEval').mockImplementation();
+      const evalMock = jest
+        .spyOn(snapEvalModule, 'snapEval')
+        .mockImplementation();
       const manifestMock = jest
         .spyOn(manifestModule, 'manifest')
         .mockImplementation();
@@ -71,7 +75,9 @@ describe('build', () => {
       const bundleMock = jest
         .spyOn(buildBundle, 'bundle')
         .mockImplementation(async () => true);
-      const evalMock = jest.spyOn(evalModule, 'snapEval').mockImplementation();
+      const evalMock = jest
+        .spyOn(snapEvalModule, 'snapEval')
+        .mockImplementation();
       const manifestMock = jest
         .spyOn(manifestModule, 'manifest')
         .mockImplementation();
@@ -101,7 +107,9 @@ describe('build', () => {
       jest.spyOn(fsUtils, 'validateOutfileName').mockImplementation();
       jest.spyOn(fsUtils, 'validateFilePath').mockImplementation();
       jest.spyOn(fsUtils, 'validateDirPath').mockImplementation();
-      const evalMock = jest.spyOn(evalModule, 'snapEval').mockImplementation();
+      const evalMock = jest
+        .spyOn(snapEvalModule, 'snapEval')
+        .mockImplementation();
       jest.spyOn(manifestModule, 'manifest').mockImplementation();
       jest.spyOn(buildBundle, 'bundle').mockImplementation();
 

src/cmds/build/buildHandler.ts
@@ -1,0 +1,40 @@
+import {
+  getOutfilePath,
+  validateDirPath,
+  validateFilePath,
+  validateOutfileName,
+} from '../../utils';
+import { snapEval } from '../eval/evalHandler';
+import { manifest } from '../manifest/manifestHandler';
+import { YargsArgs } from '../../types/yargs';
+import { bundle } from './bundle';
+
+/**
+ * Builds all files in the given source directory to the given destination
+ * directory.
+ *
+ * Creates destination directory if it doesn't exist.
+ *
+ * @param argv - argv from Yargs
+ * @param argv.src - The source file path
+ * @param argv.dist - The output directory path
+ * @param argv.outfileName - The output file name
+ */
+export async function build(argv: YargsArgs): Promise<void> {
+  const { src, dist, outfileName } = argv;
+  if (outfileName) {
+    validateOutfileName(outfileName as string);
+  }
+  await validateFilePath(src);
+  await validateDirPath(dist, true);
+
+  const outfilePath = getOutfilePath(dist, outfileName as string);
+  const result = await bundle(src, outfilePath, argv);
+  if (result && argv.eval) {
+    await snapEval({ ...argv, bundle: outfilePath });
+  }
+
+  if (argv.manifest) {
+    manifest(argv);
+  }
+}

commit 187802d840c308058f19a8fcb650543c193c5bff

Fix development scripts (#129)
The scripts in `development` were previously migrated to TypeScript. They were somewhat broken. This PR makes them plain JavaScript again, and removes them from the build output. This alters the structure of `dist`, which has been taken into account.

This also fixes a bug introduced in #126, due to `evalWorker.ts` being renamed to `eval-worker.ts`.

.eslintrc.js
@@ -21,7 +21,7 @@ module.exports = {
     },
 
     {
-      files: ['src/cmds/eval/evalWorker.ts'],
+      files: ['src/cmds/eval/eval-worker.ts'],
       globals: {
         lockdown: true,
         Compartment: true,

development/buildExamples.js
@@ -1,18 +1,8 @@
-import { promises as fs } from 'fs';
-import { resolve } from 'path';
-import execa from 'execa';
+const { promises: fs } = require('fs');
+const { resolve } = require('path');
+const execa = require('execa');
 
-import { build } from '../src/cmds/build/buildHandler';
-import { SnapsCliGlobals } from '../src/types/package';
-
-// mock the snaps global
-declare global {
-  // eslint-disable-next-line @typescript-eslint/no-namespace
-  namespace NodeJS {
-    // eslint-disable-next-line @typescript-eslint/no-empty-interface
-    interface Global extends SnapsCliGlobals {}
-  }
-}
+const { build } = require('../dist/cmds/build/buildHandler');
 
 global.snaps = {
   verboseErrors: false,

development/generateInitTemplate.js
@@ -1,0 +1,26 @@
+const { promises: fs } = require('fs');
+const path = require('path');
+
+const EXAMPLE_PATH = 'examples/hello-snaps';
+const TEMPLATE_PATH = 'src/cmds/init/initTemplate.json';
+
+generateInitTemplate();
+
+async function generateInitTemplate() {
+  const html = await fs.readFile(path.join(EXAMPLE_PATH, 'index.html'));
+  const js = await fs.readFile(path.join(EXAMPLE_PATH, 'index.js'));
+
+  await fs.writeFile(
+    TEMPLATE_PATH,
+    JSON.stringify(
+      {
+        html: html.toString(),
+        js: js.toString(),
+      },
+      null,
+      2,
+    ),
+  );
+
+  console.log('Success: Wrote src/cmds/init/initTemplate.json');
+}

development/generateInitTemplate.ts
@@ -1,26 +1,0 @@
-import { promises as fs } from 'fs';
-import path from 'path';
-
-const EXAMPLE_PATH = 'examples/hello-snaps';
-const TEMPLATE_PATH = 'src/initTemplate.json';
-
-generateInitTemplate();
-
-async function generateInitTemplate() {
-  const html = fs.readFile(path.join(EXAMPLE_PATH, 'index.html')).toString();
-  const js = fs.readFile(path.join(EXAMPLE_PATH, 'index.js')).toString();
-
-  await fs.writeFile(
-    TEMPLATE_PATH,
-    JSON.stringify(
-      {
-        html,
-        js,
-      },
-      null,
-      2,
-    ),
-  );
-
-  console.log('success: wrote src/initTemplate.json');
-}

src/cmds/eval/workerEval.ts
@@ -2,7 +2,7 @@ import { Worker } from 'worker_threads';
 import pathUtils from 'path';
 
 export function workerEval(bundlePath: string): Promise<null> {
-  return new Promise((resolve, _reject) => {
+  return new Promise((resolve) => {
     new Worker(getEvalWorkerPath())
       .on('exit', (exitCode: number) => {
         if (exitCode === 0) {
@@ -21,5 +21,5 @@ export function workerEval(bundlePath: string): Promise<null> {
  * @returns The path to the eval worker file.
  */
 function getEvalWorkerPath(): string {
-  return pathUtils.join(__dirname, 'evalWorker.js');
+  return pathUtils.join(__dirname, 'eval-worker.js');
 }

src/cmds/eval/workerEval.test.ts
@@ -33,7 +33,7 @@ jest.mock('worker_threads', () => ({
 
 describe('workerEval', () => {
   const mockBundlePath = './snap.js';
-  const workerPathRegex = /evalWorker\.js$/u;
+  const workerPathRegex = /eval-worker\.js$/u;
 
   beforeEach(() => {
     jest.spyOn(pathUtils, 'join');