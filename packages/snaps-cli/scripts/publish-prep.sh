3d18285b107c9bc0bf1b53e893fe1fd35ffa9bc4
Erik Marks <25517051+rekmarks@users.noreply.github.com>
2021-09-29 12:53:01 -0700

    Ensure snaps-cli init template is always up to date (#94)
    We had no checks to ensure that the `snaps-cli` init template (a JSON file used for the `mm-snap init` command) was kept updated. This PR adds this check to the `snaps-cli` prepublish script, and to the regular CI workflow.

---
+++ packages/snaps-cli/scripts/publish-prep.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -x
+set -e
+set -u
+set -o pipefail
+
+yarn build:clean
+
+if [[ $(git diff --quiet) != '' ]]; then
+  echo "Working tree dirty after building"
+  exit 1
+fi
+
+yarn lint
+yarn test
