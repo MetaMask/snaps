83fe0816e9a1b791c550c308533c60cb91f2ab9d
Erik Marks <25517051+rekmarks@users.noreply.github.com>
2021-09-30 18:29:16 -0700

    Fix CI test scripts (#98)
    This PR fixes our CI test scripts to ensure that we in fact run all unit tests in CI, which is probably a good thing to do.

--- packages/controllers/test-ci.sh
+++ packages/controllers/test-ci.sh
@@ -7,5 +7,4 @@ set -o pipefail
 
 # We have to use xvfb due to electron
 # Ref: https://github.com/facebook-atom/jest-electron-runner/issues/47#issuecomment-508556407
-xvfb-run -e /dev/stdout yarn workspace @metamask/snap-controllers test &&
-yarn workspace @metamask/iframe-execution-environment-service test
+xvfb-run -e /dev/stdout yarn test
