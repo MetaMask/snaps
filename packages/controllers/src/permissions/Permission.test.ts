1a3e4ed8ef6c119999fc437a2e006131f0057f7f
Erik Marks <rekmarks@protonmail.com>
2021-11-22 14:57:39 -0800

    Add findCaveat tests

--- packages/controllers/src/permissions/Permission.test.ts
+++ packages/controllers/src/permissions/Permission.test.ts
@@ -1,4 +1,5 @@
-import { constructPermission, CaveatConstraint } from '.';
+import { findCaveat } from './Permission';
+import { constructPermission, CaveatConstraint, PermissionConstraint } from '.';
 
 describe('constructPermission', () => {
   it('constructs a permission', () => {
@@ -41,5 +42,46 @@ describe('constructPermission', () => {
         date: expect.any(Number),
       }),
     );
+  });
+});
+
+describe('findCaveat', () => {
+  it('finds a caveat', () => {
+    const permission: PermissionConstraint = {
+      id: 'arbitraryId',
+      parentCapability: 'arbitraryMethod',
+      invoker: 'arbitraryInvoker',
+      date: Date.now(),
+      caveats: [{ type: 'foo', value: 'bar' }],
+    };
+
+    expect(findCaveat(permission, 'foo')).toStrictEqual({
+      type: 'foo',
+      value: 'bar',
+    });
+  });
+
+  it('returns undefined if the specified caveat does not exist', () => {
+    const permission: PermissionConstraint = {
+      id: 'arbitraryId',
+      parentCapability: 'arbitraryMethod',
+      invoker: 'arbitraryInvoker',
+      date: Date.now(),
+      caveats: [{ type: 'foo', value: 'bar' }],
+    };
+
+    expect(findCaveat(permission, 'doesNotExist')).toBeUndefined();
+  });
+
+  it('returns undefined if the permission has no caveats', () => {
+    const permission: PermissionConstraint = {
+      id: 'arbitraryId',
+      parentCapability: 'arbitraryMethod',
+      invoker: 'arbitraryInvoker',
+      date: Date.now(),
+      caveats: null,
+    };
+
+    expect(findCaveat(permission, 'doesNotExist')).toBeUndefined();
   });
 });
