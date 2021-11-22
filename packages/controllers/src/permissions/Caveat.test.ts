0117911d76554f55ce060154c7e6e34d09895d30
Erik Marks <rekmarks@protonmail.com>
2021-11-22 15:07:46 -0800

    Complete decorateWithCaveats tests

--- packages/controllers/src/permissions/Caveat.test.ts
+++ packages/controllers/src/permissions/Caveat.test.ts
@@ -1,8 +1,6 @@
 import * as errors from './errors';
 import { decorateWithCaveats, PermissionConstraint } from '.';
 
-// This function is mostly tested through the PermissionController tests,
-// we just add a couple here for completeness.
 describe('decorateWithCaveats', () => {
   it('decorates a method with caveat', async () => {
     const methodImplementation = () => [1, 2, 3];
@@ -32,6 +30,60 @@ describe('decorateWithCaveats', () => {
 
     expect(methodImplementation()).toStrictEqual([1, 2, 3]);
     expect(await decorated({} as any)).toStrictEqual([3, 2, 1]);
+  });
+
+  it('decorates a method with multiple caveats', async () => {
+    const methodImplementation = () => [1, 2, 3];
+
+    const caveatSpecifications = {
+      reverse: {
+        type: 'reverse',
+        decorator: (method: any, _caveat: any) => async () => {
+          return (await method()).reverse();
+        },
+      },
+      slice: {
+        type: 'slice',
+        decorator: (method: any, caveat: any) => async () => {
+          return (await method()).slice(0, caveat.value);
+        },
+      },
+    };
+
+    const permission: PermissionConstraint = {
+      id: 'foo',
+      parentCapability: 'arbitraryMethod',
+      invoker: 'arbitraryInvoker',
+      date: Date.now(),
+      caveats: [
+        { type: 'reverse', value: null },
+        { type: 'slice', value: 1 },
+      ],
+    };
+
+    const decorated = decorateWithCaveats(
+      methodImplementation,
+      permission,
+      caveatSpecifications,
+    );
+
+    expect(methodImplementation()).toStrictEqual([1, 2, 3]);
+    expect(await decorated({} as any)).toStrictEqual([3]);
+  });
+
+  it('returns the unmodified method implementation if there are no caveats', () => {
+    const methodImplementation = () => [1, 2, 3];
+
+    const permission: PermissionConstraint = {
+      id: 'foo',
+      parentCapability: 'arbitraryMethod',
+      invoker: 'arbitraryInvoker',
+      date: Date.now(),
+      caveats: null,
+    };
+
+    const decorated = decorateWithCaveats(methodImplementation, permission, {});
+    expect(methodImplementation()).toStrictEqual(decorated({} as any));
   });
 
   it('throws an error if the caveat type is unrecognized', () => {
