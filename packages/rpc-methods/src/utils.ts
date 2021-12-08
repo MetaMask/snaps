7ce089d6d4d4077308d2f72e2e7a679b602a95a8
Mark Stacey <markjstacey@gmail.com>
2021-12-08 11:45:13 -0330

    Improve types and documentation for `selectHooks`
    The `selectHooks` function now has improved types and documentation.
    Previously it had no JSDoc comments at all. Now the inline docs written
    for the extension `selectHooks` function by @rekmarks has been ported
    here, and docs were added for the generic parameters as well.
    
    The types were a bit of a mess previously. There were multiple type
    signatures, for one thing. The generic parameters seemed backwards,
    because the keys for `hooks` were derived from the names to select,
    even though the names to select are a _subset_ of the hook keys. The
    generic parameters were poorly named. And there was some invalid type
    casting.
    
    Unfortunately we still need _three_ type casts here because of how
    unfriendly `.reduce` is to work with in TypeScript, but at least all
    of these casts are guaranteed to be correct.

--- packages/rpc-methods/src/utils.ts
+++ packages/rpc-methods/src/utils.ts
@@ -4,31 +4,31 @@ export function isPlainObject(
   return Boolean(value) && typeof value === 'object' && !Array.isArray(value);
 }
 
-type SelectedHooks<
-  Hooks extends Record<keyof HookNames, unknown>,
-  HookNames extends Record<string, boolean>,
-> = Pick<Hooks, keyof HookNames & keyof Hooks>;
-
-export function selectHooks<Hooks extends Record<string, unknown>>(
+/**
+ * Returns the subset of the specified `hooks` that are included in the
+ * `hookNames` object. This is a Principle of Least Authority (POLA) measure
+ * to ensure that each RPC method implementation only has access to the
+ * API "hooks" it needs to do its job.
+ *
+ * @param hooks - The hooks to select from.
+ * @param hookNames - The names of the hooks to select.
+ * @returns The selected hooks.
+ * @template Hooks - The hooks to select from.
+ * @template HookName - The names of the hooks to select.
+ */
+export function selectHooks<
+  Hooks extends Record<string, unknown>,
+  HookName extends keyof Hooks,
+>(
   hooks: Hooks,
-  hookNames?: void,
-): void;
-
-export function selectHooks<
-  Hooks extends Record<keyof HookNames, unknown>,
-  HookNames extends Record<string, boolean>,
->(hooks: Hooks, hookNames: HookNames): SelectedHooks<Hooks, HookNames>;
-
-export function selectHooks<
-  T extends Record<keyof U, unknown>,
-  U extends Record<string, boolean>,
->(hooks: T, hookNames?: U) {
+  hookNames?: Record<HookName, boolean>,
+): Pick<Hooks, HookName> | undefined {
   if (hookNames) {
     return Object.keys(hookNames).reduce((hookSubset, _hookName) => {
-      const hookName = _hookName as keyof SelectedHooks<T, U>;
+      const hookName = _hookName as HookName;
       hookSubset[hookName] = hooks[hookName];
       return hookSubset;
-    }, {} as SelectedHooks<T, U>);
+    }, {} as Partial<Pick<Hooks, HookName>>) as Pick<Hooks, HookName>;
   }
   return undefined;
 }
