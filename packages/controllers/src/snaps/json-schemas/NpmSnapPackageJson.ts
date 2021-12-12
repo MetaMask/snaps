993770b0edd0c74395158c9f93a1056a28ea4843
Erik Marks <rekmarks@protonmail.com>
2021-12-12 12:22:50 -0800

    Migrate example snaps

--- packages/controllers/src/snaps/json-schemas/NpmSnapPackageJson.ts
+++ packages/controllers/src/snaps/json-schemas/NpmSnapPackageJson.ts
@@ -4,17 +4,15 @@ type Version = string;
 type PackageName = string;
 type Private = boolean;
 type Main = string;
-type StringHVOT0U1Z = "public";
+type StringDj4X5WuP = string;
 type StringXp6NyS6W = "https://registry.npmjs.org" | "https://registry.npmjs.org/";
 type PublishConfig = {
-  access: StringHVOT0U1Z;
+  access?: StringDj4X5WuP;
   registry: StringXp6NyS6W;
   [k: string]: any;
 }
-type StringG66CDL2Y = "git";
-type StringDj4X5WuP = string;
 type Repository = {
-  type: StringG66CDL2Y;
+  type: StringDj4X5WuP;
   url: StringDj4X5WuP;
 }
 export type NpmSnapPackageJson = {
