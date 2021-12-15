0b5a588c19c9886d5ec595f60bb9f5e3d57fcfb5
Erik Marks <25517051+rekmarks@users.noreply.github.com>
2021-12-14 18:11:12 -0800

    ses@0.15.3 (#159)
    Bumps `ses` to the latest version, `0.15.3`, and updates the `lockdown` configuration per [the extension](https://github.com/MetaMask/metamask-extension/blob/f9ea9e4b432176d940001365938aa88253d54024/app/scripts/lockdown-run.js#L4-L10). This necessitated refactoring how the `SnapWorker` is bundled, as part of which the `--watch` mode for bundling was removed. It's unclear if that ever worked anyway.

--- packages/workers/bundle.js
+++ packages/workers/bundle.js
@@ -1,46 +1,55 @@
-const fs = require('fs');
+const { createWriteStream, promises: fs } = require('fs');
+const pathUtils = require('path');
+const { promisify } = require('util');
 const browserify = require('browserify');
-const watchify = require('watchify');
+const rimraf = promisify(require('rimraf'));
+const endOfStream = promisify(require('end-of-stream'));
 
-let watch = false;
-if (process.argv.length > 2) {
-  watch = Boolean(process.argv[2]);
+const MAIN_FILE_PATH = pathUtils.resolve(__dirname, 'dist/SnapWorker.js');
+const TEMP_FILE_PATH = pathUtils.resolve(__dirname, 'dist/_SnapWorker.js');
+
+/**
+ * This is to be run after `tsc`.
+ */
+main();
+
+async function main() {
+  // Run browserify on the tsc output
+  await bundle();
+
+  // Remove original SnapWorker files
+  await rimraf(pathUtils.resolve(__dirname, 'dist/SnapWorker*'));
+
+  // Get the SES lockdown contents
+  const sesContent = await fs.readFile(
+    pathUtils.resolve(
+      __dirname,
+      '../../node_modules/ses/dist/lockdown.umd.min.js',
+    ),
+  );
+
+  // Get the browserify output
+  const bundleContent = await fs.readFile(TEMP_FILE_PATH, 'utf8');
+
+  // Prepend the SES lockdown contents to the bundle contents and write to
+  // primary export file
+  await fs.writeFile(MAIN_FILE_PATH, `${sesContent}${bundleContent}`);
+
+  // Delete temporary file
+  await rimraf(TEMP_FILE_PATH);
 }
 
-const browserifyOpts = {
-  debug: false,
-  entries: ['dist/_SnapWorker.js'],
-  plugin: 'tinyify',
-};
+async function bundle() {
+  const browserifyOpts = {
+    debug: false,
+    entries: [MAIN_FILE_PATH],
+    plugin: 'tinyify',
+  };
 
-if (watch) {
-  browserifyOpts.cache = {};
-  browserifyOpts.packageCache = {};
-  browserifyOpts.plugin = [watchify];
-}
-
-const b = browserify(browserifyOpts);
-
-if (watch) {
-  b.on('update', bundle).on('log', bundleLog);
-}
-
-bundle();
-
-function bundle() {
-  b.bundle()
-    .on('error', console.error)
-    .pipe(fs.createWriteStream('dist/SnapWorker.js'));
-}
-
-function bundleLog(message) {
-  const date = new Date();
-  const time = `${display(date.getHours())}:${display(
-    date.getMinutes(),
-  )}:${display(date.getSeconds())}`;
-  console.log(`${time} ${message}`);
-
-  function display(timeNum) {
-    return timeNum >= 10 ? timeNum : `0${timeNum}`;
-  }
+  await endOfStream(
+    browserify(browserifyOpts)
+      .bundle()
+      .on('error', console.error)
+      .pipe(createWriteStream(TEMP_FILE_PATH)),
+  );
 }
