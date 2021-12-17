8d2b21938b77a6037476215e850593c4a75e44c2
Erik Marks <25517051+rekmarks@users.noreply.github.com>
2021-12-16 21:53:55 -0800

    Fix build and publish scripts (#169)
    While publishing `0.6.0`, I discovered a number of issues with the package-wide build and publishing scripts. This PR fixes these issues by adding a `build:pre-tsc` and `build:post-tsc` script to each package, and ensuring that these get executed for each package in the correct order at the time of publishing.

--- packages/snap-examples/scripts/buildExamples.js
+++ packages/snap-examples/scripts/buildExamples.js
@@ -1,12 +1,9 @@
 const { promises: fs } = require('fs');
-const { resolve, join: pathJoin } = require('path');
+const { resolve: pathResolve } = require('path');
 
 const execa = require('execa');
 
-const SNAPS_CLI_BIN_PATH = pathJoin(
-  __dirname,
-  '../../../node_modules/@metamask/snaps-cli/dist/main.js',
-);
+const SNAPS_CLI_BIN_PATH = '../../../snaps-cli/dist/main.js';
 
 const EXAMPLES_PATH = 'examples';
 
@@ -15,48 +12,55 @@ buildExamples();
 async function buildExamples() {
   const examplesDir = await fs.readdir(EXAMPLES_PATH);
 
-  examplesDir.forEach(async (exampleFile) => {
-    const exampleFilePath = resolve(EXAMPLES_PATH, exampleFile);
-    const exampleFileStat = await fs.stat(exampleFilePath);
+  await Promise.all(
+    examplesDir.map(async (exampleFile) => {
+      const exampleFilePath = pathResolve(EXAMPLES_PATH, exampleFile);
+      const exampleFileStat = await fs.stat(exampleFilePath);
 
-    if (exampleFileStat.isDirectory()) {
-      const srcPath = resolve(exampleFilePath, 'index.js');
-      const pkgPath = resolve(exampleFilePath, 'package.json');
-      const pkgStat = await fs.stat(pkgPath);
-      const srcStat = await fs.stat(srcPath);
+      if (exampleFileStat.isDirectory()) {
+        const srcPath = pathResolve(exampleFilePath, 'src/index.js');
+        const pkgPath = pathResolve(exampleFilePath, 'package.json');
+        const pkgStat = await fs.stat(pkgPath);
+        const srcStat = await fs.stat(srcPath);
 
-      if (pkgStat.isFile() && srcStat.isFile()) {
-        try {
-          // install dependencies
-          await execa('yarn', ['install'], {
-            cwd: exampleFilePath,
-          });
-        } catch (depsInstallError) {
-          console.error(
-            `Unexpected error when installing dependencies in "${exampleFilePath}.`,
-          );
-          throw depsInstallError;
-        }
-
-        try {
-          await execa(
-            SNAPS_CLI_BIN_PATH,
-            ['build', '--sourceMaps', '--stripComments'],
-            {
+        if (pkgStat.isFile() && srcStat.isFile()) {
+          try {
+            // install dependencies
+            await execa('yarn', ['install'], {
               cwd: exampleFilePath,
-            },
+            });
+          } catch (depsInstallError) {
+            console.error(
+              `Unexpected error when installing dependencies in "${exampleFilePath}.`,
+            );
+            throw depsInstallError;
+          }
+
+          try {
+            await execa(
+              'node',
+              [
+                pathResolve(exampleFilePath, SNAPS_CLI_BIN_PATH),
+                'build',
+                '--sourceMaps',
+                '--stripComments',
+              ],
+              {
+                cwd: exampleFilePath,
+              },
+            );
+          } catch (bundleError) {
+            console.error(
+              `Unexpected error while creating bundle in "${exampleFilePath}.`,
+            );
+            throw bundleError;
+          }
+        } else {
+          throw new Error(
+            `Invalid example directory "${exampleFile}". Ensure it has valid 'package.json' and 'index.js' files.`,
           );
-        } catch (bundleError) {
-          console.error(
-            `Unexpected error while creating bundle in "${exampleFilePath}.`,
-          );
-          throw bundleError;
         }
-      } else {
-        throw new Error(
-          `Invalid example directory "${exampleFile}". Ensure it has valid 'package.json' and 'index.js' files.`,
-        );
       }
-    }
-  });
+    }),
+  );
 }
---
+++ scripts/publish-all.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -x
+set -e
+set -u
+set -o pipefail
+
+# This script installs all dependencies, builds all packages, ensures that the
+# git working tree remains cleans, then asks for an npm 2FA code and publishes
+# all packages.
+
+yarn setup
+yarn build:clean
+yarn lint
+
+if [[ $(git diff --quiet) != '' ]]; then
+  echo "Working tree dirty after building"
+  exit 1
+fi
+
+echo 'Publishing preparations complete. Enter npm OTP:'
+read -r OTP
+
+if [[ -z $OTP ]]; then
+  echo "Error: No OTP specified."
+  exit 1
+fi
+
+yarn workspaces run publish "$OTP"
