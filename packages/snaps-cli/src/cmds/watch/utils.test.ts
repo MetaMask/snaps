d5b95efaaea6678b66c2fabedcdb859e05c9601b
hmalik88 <hassan.malik@consensys.net>
2022-02-17 16:10:00 -0500

    fixed tests, moved jest mocks

--- packages/snaps-cli/src/cmds/watch/utils.test.ts
+++ packages/snaps-cli/src/cmds/watch/utils.test.ts
@@ -4,10 +4,11 @@ import { snapEval } from '../eval/evalHandler';
 import { YargsArgs } from '../../types/yargs';
 import { processEval, processManifestCheck } from './utils';
 
+jest.mock('../eval/evalHandler');
+jest.mock('../manifest/manifestHandler');
 describe('utils', () => {
   describe('processEval', () => {
     it('will call snapEval if the eval property is true in argv', async () => {
-      jest.mock('../eval/evalHandler');
       const argv: Record<string, any> = {
         eval: true,
         sourceMaps: true,
@@ -22,7 +23,6 @@ describe('utils', () => {
 
   describe('processManifestCheck', () => {
     it('will call manifestHandler if the manifest property is true in argv', async () => {
-      jest.mock('../manifest/manifestHandler');
       const argv: Record<string, any> = {
         eval: true,
         sourceMaps: true,
