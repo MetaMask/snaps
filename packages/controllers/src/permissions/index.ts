3b65914618fc4a5c3c2f0c4424d8bfa36632526e
Frederik Bolding <frederik.bolding@gmail.com>
2022-03-03 12:20:59 +0100

    Delete PermissionController and SubjectMetadataController

--- packages/controllers/src/permissions/index.ts
+++
@@ -1,8 +0,0 @@
-export * from './Caveat';
-export * from './Permission';
-export * from './PermissionController';
-export * from './utils';
-
-// TODO: Move these to the appropriate package
-export * as permissionRpcMethods from './rpc-methods';
-export { endowmentPermissionBuilders } from './endowments';
