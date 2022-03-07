440c60fe7f3bbb6bac393aff0bbb8abe14d0e4ef
Erik Marks <rekmarks@protonmail.com>
2022-03-07 10:21:52 -0800

    Convert .forEach to .reduce

--- packages/execution-environments/src/common/endowments/index.ts
+++ packages/execution-environments/src/common/endowments/index.ts
@@ -27,53 +27,53 @@ const endowmentFactories = [buffer, timeout, wasm].reduce(
  * root realm global will be used.
  *
  * @param wallet - The Snap's provider object.
- * @param _endowments - The list of endowments to provide to the snap.
+ * @param endowments - The list of endowments to provide to the snap.
  * @returns An object containing the Snap's endowments.
  */
 export function createEndowments(
   wallet: SnapProvider,
-  _endowments: string[] = [],
+  endowments: string[] = [],
 ): Record<string, unknown> {
-  const endowments: Record<string, unknown> = { wallet };
-
   const attenuatedEndowments: Record<string, unknown> = {};
 
-  _endowments.forEach((endowmentName) => {
-    // First, check if the endowment has a factory, and default to that.
-    if (endowmentFactories.has(endowmentName)) {
-      if (!Object.hasOwnProperty.call(attenuatedEndowments, endowmentName)) {
-        // Call the endowment factory for the current endowment. If the factory
-        // creates multiple endowments, they will all be assigned to the
-        // `attenuatedEndowments` object, but will only be passed on to the snap
-        // if explicitly listed among its endowment.
-        // This may not have an actual use case, but, safety first.
-        Object.assign(
-          attenuatedEndowments,
-          // We just confirmed that endowmentFactories has the specified key.
-          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-          endowmentFactories.get(endowmentName)!(),
-        );
+  return endowments.reduce(
+    (allEndowments, endowmentName) => {
+      // First, check if the endowment has a factory, and default to that.
+      if (endowmentFactories.has(endowmentName)) {
+        if (!Object.hasOwnProperty.call(attenuatedEndowments, endowmentName)) {
+          // Call the endowment factory for the current endowment. If the factory
+          // creates multiple endowments, they will all be assigned to the
+          // `attenuatedEndowments` object, but will only be passed on to the snap
+          // if explicitly listed among its endowment.
+          // This may not have an actual use case, but, safety first.
+          Object.assign(
+            attenuatedEndowments,
+            // We just confirmed that endowmentFactories has the specified key.
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            endowmentFactories.get(endowmentName)!(),
+          );
+        }
+
+        allEndowments[endowmentName] = attenuatedEndowments[endowmentName];
+      } else if (endowmentName in rootRealmGlobal) {
+        // If the endowment doesn't have a factory, just use whatever is on the
+        // global object.
+        const globalValue = (rootRealmGlobal as Record<string, unknown>)[
+          endowmentName
+        ];
+
+        // Bind functions to prevent shenanigans.
+        allEndowments[endowmentName] =
+          typeof globalValue === 'function'
+            ? globalValue.bind(rootRealmGlobal)
+            : globalValue;
+      } else {
+        // If we get to this point, we've been passed an endowment that doesn't
+        // exist in our current environment.
+        throw new Error(`Unknown endowment: "${endowmentName}".`);
       }
-
-      endowments[endowmentName] = attenuatedEndowments[endowmentName];
-    } else if (endowmentName in rootRealmGlobal) {
-      // If the endowment doesn't have a factory, just use whatever is on the
-      // global object.
-      const globalValue = (rootRealmGlobal as Record<string, unknown>)[
-        endowmentName
-      ];
-
-      // Bind functions to prevent shenanigans.
-      endowments[endowmentName] =
-        typeof globalValue === 'function'
-          ? globalValue.bind(rootRealmGlobal)
-          : globalValue;
-    } else {
-      // If we get to this point, we've been passed an endowment that doesn't
-      // exist in our current environment.
-      throw new Error(`Unknown endowment: "${endowmentName}".`);
-    }
-  });
-
-  return endowments;
+      return allEndowments;
+    },
+    { wallet } as Record<string, unknown>,
+  );
 }
