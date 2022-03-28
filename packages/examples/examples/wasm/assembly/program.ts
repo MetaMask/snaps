185a60047834956f8b1c9eaffb5f4b30cc101ec7
Frederik Bolding <frederik.bolding@gmail.com>
2022-03-28 15:05:28 +0200

    Small fixes

--- packages/examples/examples/wasm/assembly/program.ts
+++ packages/examples/examples/wasm/assembly/program.ts
@@ -1,12 +1,14 @@
+// https://www.assemblyscript.org/introduction.html
+
 export function fib(n: i32): i32 {
-    var a = 0, b = 1
-    if (n > 0) {
-      while (--n) {
-        let t = a + b
-        a = b
-        b = t
-      }
-      return b
+  var a = 0, b = 1
+  if (n > 0) {
+    while (--n) {
+      let t = a + b;
+      a = b;
+      b = t;
     }
-    return a
+    return b;
   }
+  return a;
+}
