a78b730a4ead6916cf83f0048d99aba9d6d07d79
Frederik Bolding <frederik.bolding@gmail.com>
2022-04-07 12:41:42 +0200

    Remove unneeded mock

--- packages/controllers/src/snaps/SnapController.test.ts
+++ packages/controllers/src/snaps/SnapController.test.ts
@@ -275,7 +275,6 @@ const getTruncatedSnap = ({
 
 jest.mock('./utils', () => ({
   ...jest.requireActual<typeof utils>('./utils'),
-  fetchContent: fetchMock,
   fetchNpmSnap: jest.fn().mockResolvedValue({
     manifest: {
       description: 'arbitraryDescription',
