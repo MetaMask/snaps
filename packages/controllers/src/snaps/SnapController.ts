d48f722a199d9cee6656a10972ed829c22622435
Frederik Bolding <frederik.bolding@gmail.com>
2022-04-18 18:57:53 +0200

    Fix linting

--- packages/controllers/src/snaps/SnapController.ts
+++ packages/controllers/src/snaps/SnapController.ts
@@ -29,7 +29,7 @@ import {
   Json,
   timeSince,
 } from '@metamask/utils';
-import passworder from '@metamask/browser-passworder'
+import passworder from '@metamask/browser-passworder';
 import { ethErrors, serializeError } from 'eth-rpc-errors';
 import { SerializedEthereumRpcError } from 'eth-rpc-errors/dist/classes';
 import type { Patch } from 'immer';
@@ -169,8 +169,8 @@ export interface SnapRuntimeData {
    * RPC handler designated for the Snap
    */
   rpcHandler:
-  | null
-  | ((origin: string, request: Record<string, unknown>) => Promise<unknown>);
+    | null
+    | ((origin: string, request: Record<string, unknown>) => Promise<unknown>);
 }
 
 /**
@@ -920,14 +920,14 @@ export class SnapController extends BaseController<
 
     return snap
       ? (Object.keys(snap).reduce((serialized, key) => {
-        if (TRUNCATED_SNAP_PROPERTIES.has(key as any)) {
-          serialized[key as keyof TruncatedSnap] = snap[
-            key as keyof TruncatedSnap
-          ] as any;
-        }
+          if (TRUNCATED_SNAP_PROPERTIES.has(key as any)) {
+            serialized[key as keyof TruncatedSnap] = snap[
+              key as keyof TruncatedSnap
+            ] as any;
+          }
 
-        return serialized;
-      }, {} as Partial<TruncatedSnap>) as TruncatedSnap)
+          return serialized;
+        }, {} as Partial<TruncatedSnap>) as TruncatedSnap)
       : null;
   }
 
@@ -1679,11 +1679,11 @@ export class SnapController extends BaseController<
       ).text(),
       iconPath
         ? (
-          await this._fetchFunction(
-            new URL(iconPath, localhostUrl).toString(),
-            fetchOptions,
-          )
-        ).text()
+            await this._fetchFunction(
+              new URL(iconPath, localhostUrl).toString(),
+              fetchOptions,
+            )
+          ).text()
         : undefined,
     ]);
 
