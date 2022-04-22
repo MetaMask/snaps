0eb07f05c15207e91cc8faa04ea44719bcd992fe
Frederik Bolding <frederik.bolding@gmail.com>
2022-04-22 11:33:19 +0200

    Update coverage

--- packages/controllers/jest.config.js
+++ packages/controllers/jest.config.js
@@ -10,8 +10,8 @@ module.exports = {
     global: {
       branches: 67.4,
       functions: 79.76,
-      lines: 81.48,
-      statements: 81.55,
+      lines: 81.77,
+      statements: 81.84,
     },
   },
   silent: true,
