b2b73323ac2843537e926af38f49ee65380a22c2
Erik Marks <rekmarks@protonmail.com>
2022-04-22 18:08:29 +0200

    Add long-running endowment permission

--- packages/controllers/src/snaps/endowments/index.ts
+++ packages/controllers/src/snaps/endowments/index.ts
@@ -1,5 +1,7 @@
+import { longRunningEndowmentBuilder } from './long-running';
 import { networkAccessEndowmentBuilder } from './network-access';
 
 export const endowmentPermissionBuilders = {
   [networkAccessEndowmentBuilder.targetKey]: networkAccessEndowmentBuilder,
+  [longRunningEndowmentBuilder.targetKey]: longRunningEndowmentBuilder,
 } as const;
---
+++ packages/controllers/src/snaps/endowments/long-running.ts
@@ -0,0 +1,42 @@
+import {
+  PermissionSpecificationBuilder,
+  PermissionType,
+  EndowmentGetterParams,
+  ValidPermissionSpecification,
+} from '@metamask/controllers';
+
+const permissionName = 'endowment:long-running';
+
+type LongRunningEndowmentSpecification = ValidPermissionSpecification<{
+  permissionType: PermissionType.Endowment;
+  targetKey: typeof permissionName;
+  endowmentGetter: (_options?: any) => undefined;
+  allowedCaveats: null;
+}>;
+
+/**
+ * `endowment:long-running` returns nothing; it is intended to be used as a flag
+ * by the `SnapController` to make it ignore the request processing timeout
+ * during snap lifecycle management. Essentially, it allows a snap to take an
+ * infinite amount of time to process a request.
+ *
+ * @param _builderOptions - optional specification builder options.
+ * @returns The specification for the long-running endowment.
+ */
+const specificationBuilder: PermissionSpecificationBuilder<
+  PermissionType.Endowment,
+  any,
+  LongRunningEndowmentSpecification
+> = (_builderOptions?: any) => {
+  return {
+    permissionType: PermissionType.Endowment,
+    targetKey: permissionName,
+    allowedCaveats: null,
+    endowmentGetter: (_getterOptions?: EndowmentGetterParams) => undefined,
+  };
+};
+
+export const longRunningEndowmentBuilder = Object.freeze({
+  targetKey: permissionName,
+  specificationBuilder,
+} as const);
