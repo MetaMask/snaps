5525ebce216f1b1e37ed914e93baffa3e6a6a721
Frederik Bolding <frederik.bolding@gmail.com>
2022-05-11 09:38:30 +0200

    Run linting

--- packages/controllers/src/snaps/endowments/network-access.test.ts
+++ packages/controllers/src/snaps/endowments/network-access.test.ts
@@ -2,20 +2,20 @@ import { PermissionType } from '@metamask/controllers';
 import { networkAccessEndowmentBuilder } from './network-access';
 
 describe('endowment:network-access', () => {
-    it('builds the expected permission specification', () => {
-        const specification = networkAccessEndowmentBuilder.specificationBuilder(
-            {},
-        );
-        expect(specification).toStrictEqual({
-            permissionType: PermissionType.Endowment,
-            targetKey: 'endowment:network-access',
-            endowmentGetter: expect.any(Function),
-            allowedCaveats: null,
-        });
-
-        expect(specification.endowmentGetter()).toStrictEqual([
-            'fetch',
-            'WebSocket',
-        ]);
+  it('builds the expected permission specification', () => {
+    const specification = networkAccessEndowmentBuilder.specificationBuilder(
+      {},
+    );
+    expect(specification).toStrictEqual({
+      permissionType: PermissionType.Endowment,
+      targetKey: 'endowment:network-access',
+      endowmentGetter: expect.any(Function),
+      allowedCaveats: null,
     });
+
+    expect(specification.endowmentGetter()).toStrictEqual([
+      'fetch',
+      'WebSocket',
+    ]);
+  });
 });
--- packages/controllers/src/snaps/endowments/network-access.ts
+++ packages/controllers/src/snaps/endowments/network-access.ts
@@ -1,12 +1,17 @@
-import { EndowmentGetterParams, PermissionSpecificationBuilder, PermissionType, ValidPermissionSpecification } from "@metamask/controllers";
+import {
+  EndowmentGetterParams,
+  PermissionSpecificationBuilder,
+  PermissionType,
+  ValidPermissionSpecification,
+} from '@metamask/controllers';
 
 const permissionName = 'endowment:network-access';
 
 type NetworkAccessEndowmentSpecification = ValidPermissionSpecification<{
-    permissionType: PermissionType.Endowment;
-    targetKey: typeof permissionName;
-    endowmentGetter: (_options?: any) => ['fetch', 'WebSocket'];
-    allowedCaveats: null;
+  permissionType: PermissionType.Endowment;
+  targetKey: typeof permissionName;
+  endowmentGetter: (_options?: any) => ['fetch', 'WebSocket'];
+  allowedCaveats: null;
 }>;
 
 /**
@@ -18,21 +23,21 @@ type NetworkAccessEndowmentSpecification = ValidPermissionSpecification<{
  * @returns The specification for the network endowment
  */
 const specificationBuilder: PermissionSpecificationBuilder<
-    PermissionType.Endowment,
-    any,
-    NetworkAccessEndowmentSpecification
+  PermissionType.Endowment,
+  any,
+  NetworkAccessEndowmentSpecification
 > = (_builderOptions?: any) => {
-    return {
-        permissionType: PermissionType.Endowment,
-        targetKey: permissionName,
-        allowedCaveats: null,
-        endowmentGetter: (_getterOptions?: EndowmentGetterParams) => {
-            return ['fetch', 'WebSocket'];
-        },
-    };
+  return {
+    permissionType: PermissionType.Endowment,
+    targetKey: permissionName,
+    allowedCaveats: null,
+    endowmentGetter: (_getterOptions?: EndowmentGetterParams) => {
+      return ['fetch', 'WebSocket'];
+    },
+  };
 };
 
 export const networkAccessEndowmentBuilder = Object.freeze({
-    targetKey: permissionName,
-    specificationBuilder,
+  targetKey: permissionName,
+  specificationBuilder,
 } as const);
