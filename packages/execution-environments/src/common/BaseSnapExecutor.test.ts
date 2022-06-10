18289c1112850cd11dfc021b8adb47cb71621e7c
Frederik Bolding <frederik.bolding@gmail.com>
2022-06-10 12:33:27 +0200

    BREAKING: Rename `onMessage` to `onRpcMessage` and use named parameters (#533)
    * Rename onMessage and use named parameters
    
    * Fix parameters on an example
    
    * Update manifests
    
    * Allow shadowing of origin global
    
    * Rename TypeScript example function
    
    * Fix test
    
    * Update manifest

--- packages/execution-environments/src/common/BaseSnapExecutor.test.ts
+++ packages/execution-environments/src/common/BaseSnapExecutor.test.ts
@@ -173,7 +173,7 @@ describe('BaseSnapExecutor', () => {
     it("doesn't leak execution outside of expected timeshare during RPC calls", async () => {
       // The 250 timeout should run and return a value, but all later timeouts should fail to execute
       const CODE = `
-        exports.onMessage = (() => {
+        exports.onRpcMessage = (() => {
           let resolve;
           const promise = new Promise((r) => { resolve = r; });
 
@@ -227,7 +227,7 @@ describe('BaseSnapExecutor', () => {
         // Since we don't know how the handle looks like we have to actually retrieve it after creating it
         const CODE_1 = `
           let handle;
-          exports.onMessage = ((origin, request) => {
+          exports.onRpcMessage = (({ origin, request }) => {
             switch (request.method) {
               case 'set':
                 let resolve;
@@ -243,7 +243,7 @@ describe('BaseSnapExecutor', () => {
           });
         `;
         const CODE_2 = `
-          exports.onMessage = ((origin, request) => {
+          exports.onRpcMessage = (({ origin, request }) => {
             const handle = request.params[0];
             clear${name}(handle);
             return 'SNAP 2 OK';
@@ -343,7 +343,7 @@ describe('BaseSnapExecutor', () => {
 
   it('terminates a request when terminate RPC is called', async () => {
     const CODE = `
-      exports.onMessage = (() => new Promise(() => ({})));
+      exports.onRpcMessage = (() => new Promise(() => ({})));
     `;
     const executor = new TestSnapExecutor();
 
@@ -401,7 +401,7 @@ describe('BaseSnapExecutor', () => {
     console.error("Hack the planet");
     `;
     const CODE = `
-      exports.onMessage = async function() {
+      exports.onRpcMessage = async function() {
         await this.startSnap("payload", \`${PAYLOAD}\`, ['console'])
         return 'PAYLOAD SENT';
       }
