6bde79906bcdd411dffb05fc0eacf5f529ebcde7
Erik Marks <rekmarks@protonmail.com>
2022-06-20 10:25:24 -0700

    fixup! Use @throws

--- packages/cli/src/utils/validate-fs.ts
+++ packages/cli/src/utils/validate-fs.ts
@@ -19,7 +19,7 @@ export function getOutfilePath(outDir: string, outFileName: string): string {
  *
  * @param filename - The file name to validate.
  * @returns `true` if validation succeeded.
- * @throws If file name is name is invalid.
+ * @throws If the file name is name is invalid.
  */
 export function validateOutfileName(filename: string): boolean {
   if (
