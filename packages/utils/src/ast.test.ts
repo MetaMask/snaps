652ff926b5253a999c851caa441877ab1c1bffc1
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2022-06-21 16:08:47 +0200

    Use AST for bundle processing

---
+++ packages/utils/src/ast.test.ts
@@ -0,0 +1,132 @@
+import { getAST, getCode, postProcessAST } from './ast';
+
+describe('getAST', () => {
+  it('returns an AST for the provided code', () => {
+    const code = `const foo = 'bar'; console.log(foo);`;
+
+    // eslint-disable-next-line jest/no-restricted-matchers
+    expect(getAST(code)).toMatchSnapshot();
+  });
+
+  it('forwards parser errors', () => {
+    // Invalid code
+    const code = `const foo bar;`;
+
+    expect(() => getAST(code)).toThrow(
+      `'Const declarations' require an initialization value.`,
+    );
+  });
+});
+
+describe('postProcessAST', () => {
+  it('strips comments', () => {
+    const code = `
+      // This is a comment
+      const foo = 'bar';
+    `;
+
+    const ast = getAST(code);
+    const processedCode = postProcessAST(ast, { stripComments: true });
+
+    expect(getCode(processedCode)).toMatchInlineSnapshot(
+      `"const foo = 'bar';"`,
+    );
+  });
+
+  it('wraps eval', () => {
+    const code = `
+      eval(bar);
+      foo.eval(bar, baz);
+      foo.bar.eval(baz, qux);
+    `;
+
+    const ast = getAST(code);
+    const processedCode = postProcessAST(ast);
+
+    expect(getCode(processedCode)).toMatchInlineSnapshot(`
+      "(1, eval)(bar);
+      (1, foo.eval)(bar, baz);
+      (1, foo.bar.eval)(baz, qux);"
+    `);
+  });
+
+  it('removes the Buffer argument', () => {
+    const code = `
+      (function (Buffer, foo) {
+        const bar = 'baz';
+      });
+
+      function foo(Buffer) {
+        const bar = 'baz';
+      }
+    `;
+
+    const ast = getAST(code);
+    const processedCode = postProcessAST(ast);
+
+    expect(getCode(processedCode)).toMatchInlineSnapshot(`
+      "(function (foo) {
+        const bar = 'baz';
+      });
+
+      function foo(Buffer) {
+        const bar = 'baz';
+      }"
+    `);
+  });
+
+  it('inserts the regenerator runtime global when used', () => {
+    const code = `
+      regeneratorRuntime.foo();
+      regeneratorRuntime.bar();
+    `;
+
+    const ast = getAST(code);
+    const processedCode = postProcessAST(ast);
+
+    expect(getCode(processedCode)).toMatchInlineSnapshot(`
+      "var regeneratorRuntime;
+      regeneratorRuntime.foo();
+      regeneratorRuntime.bar();"
+    `);
+  });
+
+  it('does not insert the regenerator runtime global when not used', () => {
+    const code = `
+      const foo = 'regeneratorRuntime';
+    `;
+
+    const ast = getAST(code);
+    const processedCode = postProcessAST(ast);
+
+    expect(getCode(processedCode)).toMatchInlineSnapshot(
+      `"const foo = 'regeneratorRuntime';"`,
+    );
+  });
+
+  it('processes all the things', () => {
+    const code = `
+      (function (Buffer, foo) {
+        // Sets 'bar' to 'baz'
+        const bar = 'baz';
+        eval(foo);
+        foo.eval('bar');
+
+        regeneratorRuntime.foo();
+      });
+    `;
+
+    const ast = getAST(code);
+    const processedCode = postProcessAST(ast);
+
+    expect(getCode(processedCode)).toMatchInlineSnapshot(`
+      "(function (foo) {
+        var regeneratorRuntime;
+        const bar = 'baz';
+        (1, eval)(foo);
+        (1, foo.eval)('bar');
+        regeneratorRuntime.foo();
+      });"
+    `);
+  });
+});
---
+++ packages/utils/src/ast.ts
@@ -0,0 +1,147 @@
+import { parse } from '@babel/parser';
+import traverse, { Node } from '@babel/traverse';
+import generate from '@babel/generator';
+import template from '@babel/template';
+
+const COMMENTS_KEYS = [
+  'innerComments',
+  'leadingComments',
+  'trailingComments',
+] as const;
+
+/**
+ * Get the abstract syntax tree (AST) representation of the given code. This
+ * uses Babel's parser to generate the AST.
+ *
+ * @param code - The code to parse.
+ * @returns The AST representation of the code.
+ * @throws If the code contains syntax errors.
+ */
+export function getAST(code: string): Node {
+  const ast = parse(code);
+
+  if (ast.errors?.length > 0) {
+    throw new Error(
+      `Failed to parse the provided code to an AST: ${ast.errors.join('\n')}`,
+    );
+  }
+
+  return ast as Node;
+}
+
+/**
+ * Turn Babel's abstract syntax tree (AST) representation back into a string.
+ *
+ * @param ast - The AST to generate code from.
+ * @returns The generated code.
+ */
+export function getCode(ast: Node): string {
+  return generate(ast).code;
+}
+
+export type PostProcessOptions = {
+  stripComments: boolean;
+  transformHtmlComments: boolean;
+};
+
+const evalWrapper = template.smart(`
+  (1, REF)(ARGS)
+`);
+
+const objectEvalWrapper = template.smart(`
+  (1, OBJECT.REF)
+`);
+
+const regeneratorRuntimeWrapper = template.smart(`
+  var regeneratorRuntime;
+`);
+
+/**
+ * Post process code with AST.
+ *
+ * @param ast - The AST to post process.
+ * @param options - The post-process options.
+ * @param options.stripComments - Whether to strip comments. Defaults to `true`.
+ * @returns The modified AST.
+ */
+export function postProcessAST(
+  ast: Node,
+  { stripComments = true }: Partial<PostProcessOptions> = {},
+): Node {
+  traverse(ast, {
+    enter(path) {
+      const { node } = path;
+
+      // Strip comments if enabled
+      if (stripComments) {
+        // Sets the comment value to `null` if defined. Simply setting the keys
+        // to null results in a bunch of unnecessary keys being added to each
+        // node.
+        COMMENTS_KEYS.forEach((key) => {
+          if (node[key]) {
+            node[key] = null;
+          }
+        });
+      }
+
+      // Browserify provides the Buffer global as an argument to modules that
+      // use it, but this does not work in SES. Since we pass in Buffer as an
+      // endowment, we can simply remove the argument.
+      // TODO: Since we're working on the AST level, we could check the scope
+      // of the function expression, and possibly prevent false positives?
+      if (node.type === 'FunctionExpression' && node.extra?.parenthesized) {
+        node.params = node.params.filter(
+          (param) => !(param.type === 'Identifier' && param.name === 'Buffer'),
+        );
+      }
+
+      // Replace `eval(foo)` with `(1, eval)(foo)`.
+      if (
+        node.type === 'CallExpression' &&
+        node.callee.type === 'Identifier' &&
+        node.callee.name === 'eval'
+      ) {
+        path.replaceWith(
+          evalWrapper({
+            REF: node.callee,
+            ARGS: node.arguments,
+          }) as any,
+        );
+
+        // Need to skip the path here, to prevent the node from being visited
+        // again.
+        path.skip();
+      }
+
+      // Replace `object.eval(foo)` with `(1, object.eval)(foo)`.
+      if (
+        node.type === 'MemberExpression' &&
+        node.property.type === 'Identifier' &&
+        node.property.name === 'eval'
+      ) {
+        path.replaceWith(
+          objectEvalWrapper({
+            OBJECT: node.object,
+            REF: node.property,
+          }) as any,
+        );
+
+        // Need to skip the path here, to prevent the node from being visited
+        // again.
+        path.skip();
+      }
+    },
+
+    Identifier(path) {
+      const { node } = path;
+
+      // Insert `regeneratorRuntime` global if it's used in the code.
+      if (node.name === 'regeneratorRuntime') {
+        path.insertBefore(regeneratorRuntimeWrapper());
+        path.stop();
+      }
+    },
+  });
+
+  return ast;
+}
