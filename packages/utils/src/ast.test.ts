7aea18061740b212151b852a11c718481f6bf919
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2022-06-23 15:43:20 +0200

    Use AST for bundle processing (#576)
    * Use AST for bundle processing
    
    * Optimise processing a little bit
    
    * Break up HTML terminators and other fixes
    
    * Fix type export
    
    * Break up import() statements in string literals and comments
    
    * Fix some tests
    
    * Fix build
    
    * Process template literals
    
    * Fix some tests
    
    * Fix issue with empty strings and add some more docs
    
    * Update test snapshots
    
    * Add some more explanation
    
    * Add test for HTML comment terminators in operators
    
    * Stop skipping children for certain replacements
    
    * Bump Babel version and fix some stuff
    
    * Run Babel with sourceType: module and add some docs
    
    * Remove obsolete comment
    
    * Insert regeneratorRuntime hack at root
    
    * Fix regenerator runtime hack
    
    * Fix some more comments
    
    * Fix a typo
    
    * Only apply processing to nodes when necessary
    
    * Update shasums
    
    * Update snapshots
    
    * Fix coverage
    
    * Remove obsolete comment
    
    Co-authored-by: Frederik Bolding <frederik.bolding@gmail.com>

---
+++ packages/utils/src/ast.test.ts
@@ -0,0 +1,276 @@
+import { getAST, getCode, postProcessAST } from './ast';
+
+describe('getAST', () => {
+  it('returns an AST for the provided code', () => {
+    const code = `const foo = 'bar'; console.log(foo);`;
+
+    // eslint-disable-next-line jest/no-restricted-matchers
+    expect(getAST(code)).toMatchSnapshot();
+  });
+
+  it(`doesn't attach comments to the AST if configured`, () => {
+    const code = `
+      // This is a comment.
+      const foo = 'bar';
+    `;
+
+    const ast = getAST(code, false);
+    expect(getCode(ast)).not.toContain('// This is a comment.');
+  });
+
+  it('forwards parser errors', () => {
+    // Invalid code
+    const code = `const foo bar;`;
+
+    expect(() => getAST(code)).toThrow(
+      `Failed to parse the provided code to an AST:`,
+    );
+  });
+});
+
+describe('postProcessAST', () => {
+  it('wraps eval', () => {
+    const code = `
+      eval(bar);
+      foo.eval(bar, baz);
+      foo.bar.eval(baz, qux);
+      eval('<!-- foo -->');
+      foo.eval('<!-- bar -->');
+    `;
+
+    const ast = getAST(code);
+    const processedCode = postProcessAST(ast);
+
+    expect(getCode(processedCode)).toMatchInlineSnapshot(`
+      "(1, eval)(bar);
+      (1, foo.eval)(bar, baz);
+      (1, foo.bar.eval)(baz, qux);
+      (1, eval)(\\"<!\\" + \\"--\\" + \\" foo \\" + \\"--\\" + \\">\\");
+      (1, foo.eval)(\\"<!\\" + \\"--\\" + \\" bar \\" + \\"--\\" + \\">\\");"
+    `);
+  });
+
+  it('removes the Buffer argument', () => {
+    const code = `
+      (function (Buffer, foo) {
+        const bar = 'baz';
+      });
+
+      function foo(Buffer) {
+        const bar = 'baz';
+      }
+    `;
+
+    const ast = getAST(code);
+    const processedCode = postProcessAST(ast);
+
+    expect(getCode(processedCode)).toMatchInlineSnapshot(`
+      "(function (foo) {
+        const bar = 'baz';
+      });
+
+      function foo(Buffer) {
+        const bar = 'baz';
+      }"
+    `);
+  });
+
+  it('inserts the regenerator runtime global when used', () => {
+    const code = `
+      function foo() {
+        regeneratorRuntime.foo();
+      }
+
+      function bar() {
+        regeneratorRuntime.bar();
+      }
+    `;
+
+    const ast = getAST(code);
+    const processedCode = postProcessAST(ast);
+
+    expect(getCode(processedCode)).toMatchInlineSnapshot(`
+      "var regeneratorRuntime;
+
+      function foo() {
+        regeneratorRuntime.foo();
+      }
+
+      function bar() {
+        regeneratorRuntime.bar();
+      }"
+    `);
+  });
+
+  it('does not insert the regenerator runtime global when not used', () => {
+    const code = `
+      const foo = 'regeneratorRuntime';
+    `;
+
+    const ast = getAST(code);
+    const processedCode = postProcessAST(ast);
+
+    expect(getCode(processedCode)).toMatchInlineSnapshot(
+      `"const foo = 'regeneratorRuntime';"`,
+    );
+  });
+
+  it('breaks up HTML comment terminators in operators', () => {
+    const code = `
+      let a = 5;
+      let b = 7;
+      const c = a --> b;
+      const d = a <!-- b;
+    `;
+
+    const ast = getAST(code);
+    const processedCode = postProcessAST(ast);
+
+    expect(getCode(processedCode)).toMatchInlineSnapshot(`
+      "let a = 5;
+      let b = 7;
+      const c = a-- > b;
+      const d = a < ! --b;"
+    `);
+  });
+
+  it('breaks up HTML comment terminators in string literals', () => {
+    const code = `
+      const foo = '<!-- bar -->';
+    `;
+
+    const ast = getAST(code);
+    const processedCode = postProcessAST(ast);
+
+    expect(getCode(processedCode)).toMatchInlineSnapshot(
+      `"const foo = \\"<!\\" + \\"--\\" + \\" bar \\" + \\"--\\" + \\">\\";"`,
+    );
+  });
+
+  it('breaks up `import()` in string literals', () => {
+    const code = `
+      const foo = 'foo bar import() baz';
+      const bar = 'foo bar import(this works too) baz';
+    `;
+
+    const ast = getAST(code);
+    const processedCode = postProcessAST(ast);
+
+    expect(getCode(processedCode)).toMatchInlineSnapshot(`
+      "const foo = \\"foo bar \\" + \\"import\\" + \\"()\\" + \\" baz\\";
+      const bar = \\"foo bar \\" + \\"import\\" + \\"(this works too)\\" + \\" baz\\";"
+    `);
+  });
+
+  it('breaks up HTML comment terminators in template literals', () => {
+    const code = `
+      const foo = \`<!-- bar --> \${'<!-- baz -->'} \${qux}\`;
+    `;
+
+    const ast = getAST(code);
+    const processedCode = postProcessAST(ast);
+
+    expect(getCode(processedCode)).toMatchInlineSnapshot(
+      `"const foo = \`\${\\"<!\\"}\${\\"--\\"}\${\\" bar \\"}\${\\"--\\"}\${\\">\\"}\${\\" \\"}\${\\"<!\\" + \\"--\\" + \\" baz \\" + \\"--\\" + \\">\\"}\${\\" \\"}\${qux}\`;"`,
+    );
+  });
+
+  it('breaks up `import()` in template literals', () => {
+    const code = `
+      const foo = \`foo bar import() baz\`;
+      const bar = \`foo bar import(this works too) baz\`;
+      foo\`
+        foo import() \${'import(bar)'} \${qux}\
+      \`;
+    `;
+
+    const ast = getAST(code);
+    const processedCode = postProcessAST(ast);
+
+    expect(getCode(processedCode)).toMatchInlineSnapshot(`
+      "const foo = \`\${\\"foo bar \\"}\${\\"import\\"}\${\\"()\\"}\${\\" baz\\"}\`;
+      const bar = \`\${\\"foo bar \\"}\${\\"import\\"}\${\\"(this works too)\\"}\${\\" baz\\"}\`;
+      foo\`\${\\"\\\\n        foo \\"}\${\\"import\\"}\${\\"()\\"}\${\\" \\"}\${\\"import\\" + \\"(bar)\\"}\${\\" \\"}\${qux}\${\\"      \\"}\`;"
+    `);
+  });
+
+  it('breaks up HTML comment terminators in comments', () => {
+    const code = `
+      <!-- foo -->
+    `;
+
+    const ast = getAST(code);
+    const processedCode = postProcessAST(ast);
+
+    expect(getCode(processedCode)).toMatchInlineSnapshot(`"// foo -- >"`);
+  });
+
+  it('breaks up `import()` in comments', () => {
+    const code = `
+      // Foo bar import() baz
+      // Foo bar import(baz) qux
+    `;
+
+    const ast = getAST(code);
+    const processedCode = postProcessAST(ast);
+
+    expect(getCode(processedCode)).toMatchInlineSnapshot(`
+      "// Foo bar import\\\\() baz
+      // Foo bar import\\\\(baz) qux"
+    `);
+  });
+
+  it(`doesn't break on empty string literals`, () => {
+    const code = `
+      const foo = '';
+    `;
+
+    const ast = getAST(code);
+    const processedCode = postProcessAST(ast);
+
+    expect(getCode(processedCode)).toMatchInlineSnapshot(`"const foo = '';"`);
+  });
+
+  it(`doesn't process values that don't contain special tokens`, () => {
+    const code = `
+      const empty = '';
+      const foo = 'foo';
+      const bar = \`bar\${foo}\`;
+    `;
+
+    const ast = getAST(code);
+    const processedCode = postProcessAST(ast);
+
+    expect(getCode(processedCode)).toMatchInlineSnapshot(`
+      "const empty = '';
+      const foo = 'foo';
+      const bar = \`bar\${foo}\`;"
+    `);
+  });
+
+  it('processes all the things', () => {
+    const code = `
+      (function (Buffer, foo) {
+        // Sets 'bar' to 'baz'
+        const bar = '<!-- baz --> import(); import(foo);';
+        regeneratorRuntime.foo('import()');
+        eval(foo);
+        foo.eval('bar');
+      });
+    `;
+
+    const ast = getAST(code, false);
+    const processedCode = postProcessAST(ast);
+
+    expect(getCode(processedCode)).toMatchInlineSnapshot(`
+      "var regeneratorRuntime;
+
+      (function (foo) {
+        const bar = \\"<!\\" + \\"--\\" + \\" baz \\" + \\"--\\" + \\">\\" + \\" \\" + \\"import\\" + \\"()\\" + \\"; \\" + \\"import\\" + \\"(foo)\\" + \\";\\";
+        regeneratorRuntime.foo(\\"import\\" + \\"()\\");
+        (1, eval)(foo);
+        (1, foo.eval)('bar');
+      });"
+    `);
+  });
+});
--- packages/utils/src/post-process.ts
+++ packages/utils/src/post-process.ts
@@ -1,8 +1,7 @@
-import { stripComments as stripCommentsFn } from './strip';
+import { getAST, getCode, postProcessAST } from './ast';
 
 export type PostProcessOptions = {
   stripComments: boolean;
-  transformHtmlComments: boolean;
 };
 
 /**
@@ -12,81 +11,29 @@ export type PostProcessOptions = {
  * Currently:
  * - Converts certain dot notation to string notation (for indexing).
  * - Makes all direct calls to eval indirect.
- * - Wraps original bundle in anonymous function.
  * - Handles certain Babel-related edge cases.
+ * - Removes the `Buffer` provided by Browserify.
+ * - Optionally removes comments.
  *
  * @param bundleString - The bundle string.
  * @param options - The post-process options.
  * @param options.stripComments - Whether to strip comments. Defaults to `true`.
- * @param options.transformHtmlComments - Whether to transform HTML comments.
- * Defaults to `true`.
  * @returns The post-processed bundle string.
  */
 export function postProcessBundle(
   bundleString: string | null,
-  {
-    stripComments = true,
-    transformHtmlComments = true,
-  }: Partial<PostProcessOptions> = {},
+  { stripComments = true }: Partial<PostProcessOptions> = {},
 ): string | null {
   if (typeof bundleString !== 'string') {
     return null;
   }
 
-  let processedString = bundleString.trim();
-
-  if (stripComments) {
-    processedString = stripCommentsFn(processedString);
-  }
-
-  // Break up tokens that could be parsed as HTML comment terminators.
-  // The regular expressions below are written strangely so as to avoid the
-  // appearance of such tokens in our source code.
-  // Ref: https://github.com/endojs/endo/blob/70cc86eb400655e922413b99c38818d7b2e79da0/packages/ses/error-codes/SES_HTML_COMMENT_REJECTED.md
-  // This aggressive hack may change the behavior of programs that contain HTML
-  // comment terminators in string literals.
-  if (transformHtmlComments) {
-    processedString = processedString.replace(
-      new RegExp(`<!${'--'}`, 'gu'),
-      '< !--',
-    );
-
-    processedString = processedString.replace(
-      new RegExp(`${'--'}>`, 'gu'),
-      '-- >',
-    );
-  }
-
-  // stuff.eval(otherStuff) => (1, stuff.eval)(otherStuff)
-  processedString = processedString.replace(
-    /((?:\b[\w\d]*[\])]?\.)+eval)(\([^)]*\))/gu,
-    '(1, $1)$2',
-  );
-
-  // If we don't do the above, the below causes syntax errors if it encounters
-  // things of the form: "something.eval(stuff)"
-  // eval(stuff) => (1, eval)(stuff)
-  processedString = processedString.replace(
-    /(\b)(eval)(\([^)]*\))/gu,
-    '$1(1, $2)$3',
-  );
-
-  // Browserify provides the Buffer global as an argument to modules that use
-  // it, but this does not work in SES. Since we pass in Buffer as an endowment,
-  // we can simply remove the argument.
-  processedString = processedString.replace(
-    /^\(function \(Buffer\)\{$/gmu,
-    '(function (){',
-  );
+  const ast = getAST(bundleString, !stripComments);
+  const processedAST = postProcessAST(ast);
+  const processedString = getCode(processedAST, bundleString);
 
   if (processedString.length === 0) {
     throw new Error(`Bundled code is empty after postprocessing.`);
-  }
-
-  // handle some cases by declaring missing globals
-  // Babel regeneratorRuntime
-  if (processedString.indexOf('regeneratorRuntime') !== -1) {
-    processedString = `var regeneratorRuntime;\n${processedString}`;
   }
 
   return processedString;
