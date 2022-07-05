9f3cd2e7e67db0380cc4beafc6e2b99f722409fb
Erik Marks <rekmarks@protonmail.com>
2022-07-05 14:19:52 -0700

    jest-it-up

--- packages/controllers/jest.config.js
+++ packages/controllers/jest.config.js
@@ -8,10 +8,10 @@ module.exports = {
   coveragePathIgnorePatterns: ['/node_modules/', '/mocks/', '/test/'],
   coverageThreshold: {
     global: {
-      branches: 64.75,
-      functions: 84.62,
-      lines: 84.6,
-      statements: 84.63,
+      branches: 65.71,
+      functions: 84.69,
+      lines: 84.75,
+      statements: 84.78,
     },
   },
   globals: {
