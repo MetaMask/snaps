0878e7bcc2d44b93fc4a146699e8219572cf0e75
Olaf Tomalka <olaf@tomalka.me>
2022-08-20 11:43:10 +0200

    Removed ExternalResourceController

--- packages/controllers/jest.config.js
+++ packages/controllers/jest.config.js
@@ -8,10 +8,10 @@ module.exports = {
   coveragePathIgnorePatterns: ['/node_modules/', '/mocks/', '/test/'],
   coverageThreshold: {
     global: {
-      branches: 74.21,
-      functions: 87.09,
-      lines: 86.13,
-      statements: 86.2,
+      branches: 80.82,
+      functions: 94.5,
+      lines: 93.07,
+      statements: 93.11,
     },
   },
   globals: {
