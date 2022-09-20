a1f71d37bd5b901f6a5cd47818462b2af6323c2c
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2022-09-20 13:17:30 +0200

    Use node resolution for controllers

--- packages/controllers/jest.config.js
+++ packages/controllers/jest.config.js
@@ -22,12 +22,16 @@ module.exports = {
       testEnvironmentOptions: {
         resources: 'usable',
         runScripts: 'dangerously',
+        customExportConditions: ['node', 'node-addons'],
       },
     },
     {
       preset: 'ts-jest',
       testPathIgnorePatterns: ['<rootDir>/src/services/iframe/*'],
       testEnvironment: 'jsdom',
+      testEnvironmentOptions: {
+        customExportConditions: ['node', 'node-addons'],
+      },
       testRegex: ['\\.test\\.(ts|js)$'],
     },
   ],
