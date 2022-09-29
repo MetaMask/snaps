e24f52349afc5bc3518d0bf1c8e0c0dbcae3d83a
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2022-09-29 15:37:08 +0200

    Improve types and documentation of `flatten` and `flatMap` (#797)
    * Improve types and documentation of flatten and flatMap
    
    * Fix typo

--- packages/utils/src/flatMap.ts
+++ packages/utils/src/flatMap.ts
@@ -1,6 +1,14 @@
-// Flattens at depth 1
-export const flatten = (array: any[]) => {
-  return array.reduce((acc, cur) => {
+/**
+ * Flattens an array of arrays of up to 1 level deep into a single array.
+ *
+ * This is a non-recursive implementation of `Array.prototype.flat`, which is
+ * only available in ES2019 and above.
+ *
+ * @param array - The array to flatten.
+ * @returns The flattened array.
+ */
+export const flatten = <Type>(array: (Type | Type[])[]): Type[] => {
+  return array.reduce<Type[]>((acc, cur) => {
     if (Array.isArray(cur)) {
       return [...acc, ...cur];
     }
@@ -8,10 +16,20 @@ export const flatten = (array: any[]) => {
   }, []);
 };
 
-// TODO: Remove once we bump to >ES2019
-export const flatMap = <Type>(
+/**
+ * Maps an array of values using the provided callback, then flattens the result
+ * into a single array, up to 1 level deep.
+ *
+ * This is a non-recursive implementation of `Array.prototype.flatMap`, which is
+ * only available in ES2019 and above.
+ *
+ * @param array - The array to map.
+ * @param callback - The callback to map each value with.
+ * @returns The mapped and flattened array.
+ */
+export const flatMap = <Type, Return>(
   array: Type[],
-  callback: (value: Type) => any,
-) => {
+  callback: (value: Type) => Return | Return[],
+): Return[] => {
   return flatten(array.map(callback));
 };
