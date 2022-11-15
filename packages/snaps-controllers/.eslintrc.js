f5835f9568e91c9bf4c0cd9363db04cb1efa1a47
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2022-11-15 13:21:34 +0100

    Bump ESLint configs and use experimental new TypeScript rules

--- packages/snaps-controllers/.eslintrc.js
+++ packages/snaps-controllers/.eslintrc.js
@@ -1,6 +1,10 @@
 module.exports = {
   extends: ['../../.eslintrc.js'],
 
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+  },
+
   rules: {
     '@typescript-eslint/consistent-type-definitions': 'off',
   },
