89d79f792fa4eb8c6e75e50cc605702e35fd7491
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2022-11-16 14:30:38 +0100

    Add @metamask/snaps-ui package

---
+++ packages/snaps-ui/jest.config.js
@@ -0,0 +1,13 @@
+const deepmerge = require('deepmerge');
+const baseConfig = require('../../jest.config.base');
+
+module.exports = deepmerge(baseConfig, {
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+});
