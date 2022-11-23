3a827c6650be6aafca4d7b2077d9fc18ff5526b9
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2022-11-23 11:53:53 +0100

    Add UI package (#978)
    * Add @metamask/snaps-ui package
    
    * Add basic components
    
    * Add more nodes and type validation
    
    * Add builder and more validation
    
    * Update yarn.lock
    
    * Fix dependencies
    
    * Add shorthand builder style
    
    * Add reference to snaps-ui in tsconfig
    
    * Fix some lint issues
    
    * Add copyable component
    
    * Ignore specified type if any
    
    * Remove unnecessary .gitignore
    
    * Fix package version

---
+++ packages/snaps-ui/src/validation.ts
@@ -0,0 +1,26 @@
+import { assertStruct } from '@metamask/utils';
+import { is } from 'superstruct';
+
+import { Component, ComponentStruct } from './nodes';
+
+/**
+ * Check if the given value is a {@link Component}. This performs recursive
+ * validation of the component's children (if any).
+ *
+ * @param value - The value to check.
+ * @returns `true` if the value is a {@link Component}, `false` otherwise.
+ */
+export function isComponent(value: unknown): value is Component {
+  return is(value, ComponentStruct);
+}
+
+/**
+ * Assert that the given value is a {@link Component}. This performs recursive
+ * validation of the component's children (if any).
+ *
+ * @param value - The value to check.
+ * @throws If the value is not a {@link Component}.
+ */
+export function assertIsComponent(value: unknown): asserts value is Component {
+  assertStruct(value, ComponentStruct, 'Invalid component');
+}
