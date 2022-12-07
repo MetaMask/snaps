db42d3fbb3f02794ef4861c7c286922adab8018a
david0xd <david@timechaser.org>
2022-12-07 11:51:50 +0100

    Change test execution context parameter name

--- packages/snaps-execution-environments/src/common/endowments/timeout.ava.test.ts
+++ packages/snaps-execution-environments/src/common/endowments/timeout.ava.test.ts
@@ -2,12 +2,12 @@ import test from 'ava';
 
 import timeout from './timeout';
 
-test('modifying handler should not be allowed and error should be thrown', (tec) => {
+test('modifying handler should not be allowed and error should be thrown', (expect) => {
   const { setTimeout: _setTimeout } = timeout.factory();
 
   const handle = _setTimeout((param: unknown) => param, 100);
 
-  tec.throws(
+  expect.throws(
     () => {
       // @ts-expect-error Ignore because this is supposed to cause an error
       handle.whatever = 'something';
