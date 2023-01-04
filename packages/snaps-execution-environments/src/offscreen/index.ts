a1302181d58f6fa7b4536b5645da33904d5980c0
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2023-01-04 12:15:32 +0100

    Enable lockdown again

--- packages/snaps-execution-environments/src/offscreen/index.ts
+++ packages/snaps-execution-environments/src/offscreen/index.ts
@@ -1,12 +1,11 @@
 import { BrowserRuntimePostMessageStream } from '@metamask/post-message-stream';
 
-// import { executeLockdown } from '../common/lockdown/lockdown';
-// import { executeLockdownMore } from '../common/lockdown/lockdown-more';
+import { executeLockdown } from '../common/lockdown/lockdown';
+import { executeLockdownMore } from '../common/lockdown/lockdown-more';
 import { OffscreenSnapExecutor } from './OffscreenSnapExecutor';
 
-// TODO: Uncomment this.
-// executeLockdown();
-// executeLockdownMore();
+executeLockdown();
+executeLockdownMore();
 
 // The stream from the offscreen document to the execution service.
 const parentStream = new BrowserRuntimePostMessageStream({
