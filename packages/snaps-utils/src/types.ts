30483f2ac0ed7d676c0a0fa652e03888c4b43d7b
Frederik Bolding <frederik.bolding@gmail.com>
2023-01-05 12:26:13 +0100

    Use utils@3.4.0

--- packages/snaps-utils/src/types.ts
+++ packages/snaps-utils/src/types.ts
@@ -161,11 +161,6 @@ export type SnapExportsParameters =
   | ObjectParameters<SnapFunctionExports>
   | KeyringParameters;
 
-// We use a symbol property name instead of { _type: Brand }, because that would show up in IDE suggestions,
-// while internal symbols do not.
-declare const brand: unique symbol;
-export type Opaque<Base, Brand extends symbol> = Base & { [brand]: Brand };
-
 type UriOptions<T extends string> = {
   protocol?: Struct<T>;
   hash?: Struct<T>;
