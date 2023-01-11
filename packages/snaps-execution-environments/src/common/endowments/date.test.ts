b65c45dc78c18ba57edf64f366f9f8b09f800aba
Frederik Bolding <frederik.bolding@gmail.com>
2023-01-11 12:31:06 +0100

    Add another test

--- packages/snaps-execution-environments/src/common/endowments/date.test.ts
+++ packages/snaps-execution-environments/src/common/endowments/date.test.ts
@@ -2,6 +2,14 @@ import { rootRealmGlobal } from '../globalObject';
 import date from './date';
 
 describe('Date endowment', () => {
+  beforeEach(() => {
+    jest.useFakeTimers().setSystemTime(new Date('2023-01-01'));
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
   it('has expected properties', () => {
     expect(date).toMatchObject({
       names: ['Date'],
@@ -12,6 +20,15 @@ describe('Date endowment', () => {
   it('returns Date from rootRealmGlobal', () => {
     const { Date } = date.factory();
     expect(Date.UTC).toStrictEqual(rootRealmGlobal.Date.UTC);
+  });
+
+  describe('constructor', () => {
+    it('does not return the original constructor', () => {
+      const { Date } = date.factory();
+      const actual = new rootRealmGlobal.Date();
+      const newDate = new Date();
+      expect(newDate.getTime()).not.toBe(actual.getTime());
+    });
   });
 
   describe('now', () => {
