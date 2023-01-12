ea79bc7c1cc0671c7fa79490a11d4e94c135afa7
Frederik Bolding <frederik.bolding@gmail.com>
2023-01-12 15:46:24 +0100

    Disallow local snaps by default

--- packages/snaps-controllers/jest.config.js
+++ packages/snaps-controllers/jest.config.js
@@ -5,7 +5,7 @@ const baseConfig = require('../../jest.config.base');
 module.exports = deepmerge(baseConfig, {
   coverageThreshold: {
     global: {
-      branches: 88.64,
+      branches: 88.99,
       functions: 94.67,
       lines: 96,
       statements: 95.92,
