edae6be4da576770bb64ce964ddf3877041f5acf
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2023-01-31 13:20:52 +0100

    Export enums from '@metamask/snaps-types'

--- packages/snaps-types/src/types.ts
+++ packages/snaps-types/src/types.ts
@@ -7,6 +7,7 @@ export type Ethereum = StreamProvider;
 
 // Exported again for convenience.
 export type { Json, JsonRpcRequest } from '@metamask/utils';
+export { DialogType, NotificationType } from '@metamask/rpc-methods';
 export type { SnapsGlobalObject } from '@metamask/rpc-methods';
 export type {
   AccountId,
