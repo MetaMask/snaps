2c4920f9347fd7dde9c154937534eb5687a1ed29
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2023-02-01 11:48:30 +0100

    Fix AVA tests

--- packages/snaps-execution-environments/ava.config.js
+++ packages/snaps-execution-environments/ava.config.js
@@ -1,7 +1,7 @@
 module.exports = () => {
   return {
     extensions: ['ts'],
-    require: ['ts-node/register'],
+    require: ['ts-node/register', 'tsconfig-paths/register'],
     verbose: true,
     files: ['src/**/*.ava.test.ts'],
     timeout: '30s',
