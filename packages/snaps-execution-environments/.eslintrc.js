3acbd48b7fdc67717659e8b32e4de4f6bd79e41b
Frederik Bolding <frederik.bolding@gmail.com>
2023-02-21 12:20:05 +0100

    Properly sandbox iframe and inline SES in execution bundles (#1193)
    * Properly sandbox iframe
    
    * Inline SES for iframe bundles
    
    * Inline SES for iframe test bundle
    
    * Inline SES in index.html instead of in the bundle
    
    * Run inlining before test
    
    * Only run inlining on test iframe during test
    
    * Fix lint
    
    * Refactor Webpack configuration to include SES lockdown bundle inline (#1203)
    
    * Refactor Webpack configuration to include SES lockdown bundle inline
    
    * Fix dependencies
    
    * Fix some tests
    
    * Fix build:test command
    
    * Rename Webpack entry names
    
    * Fix browser config
    
    * Run webpack with --mode none
    
    * Add snapshot of resulting HTML bundle
    
    * Increase timeout for CI
    
    * Further optimise Webpack configuration
    
    * Remove unused import
    
    * Only use 'tsconfig-paths-webpack-plugin' for testing
    
    * Revert change to snaps-utils
    
    * Add some polyfills
    
    * Disable source map
    
    * Add explanations
    
    ---------
    
    Co-authored-by: Maarten Zuidhoorn <maarten@zuidhoorn.com>

--- packages/snaps-execution-environments/.eslintrc.js
+++ packages/snaps-execution-environments/.eslintrc.js
@@ -5,5 +5,19 @@ module.exports = {
     tsconfigRootDir: __dirname,
   },
 
+  overrides: [
+    {
+      files: ['*.test.ts'],
+      rules: {
+        'jsdoc/check-tag-names': [
+          'error',
+          {
+            definedTags: ['jest-environment'],
+          },
+        ],
+      },
+    },
+  ],
+
   ignorePatterns: ['src/openrpc.json', 'webpack.config.js', '__test__'],
 };
