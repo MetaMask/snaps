432f0dc7a4feb76d49a16939ba1d795109a294e7
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2023-02-24 15:00:48 +0100

    Fix Jest config

--- packages/snaps-execution-environments/jest.config.js
+++ packages/snaps-execution-environments/jest.config.js
@@ -6,17 +6,15 @@ module.exports = deepmerge(baseConfig, {
   coveragePathIgnorePatterns: ['./src/index.ts'],
   coverageThreshold: {
     global: {
-      branches: 78.76,
-      functions: 87.5,
-      lines: 89.64,
-      statements: 89.71,
+      branches: 57.06,
+      functions: 57.41,
+      lines: 57.7,
+      statements: 57.86,
     },
   },
   testTimeout: 10000,
   testEnvironment: '<rootDir>/jest.environment.js',
   testEnvironmentOptions: {
-    resources: 'usable',
-    runScripts: 'dangerously',
     customExportConditions: ['node', 'node-addons'],
   },
 });
