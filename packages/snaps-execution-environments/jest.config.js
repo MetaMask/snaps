74d10e2578bd2ea5bca4b58d1def3cfa95e6a676
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2023-02-27 15:40:08 +0100

    Ignore test-utils for coverage

--- packages/snaps-execution-environments/jest.config.js
+++ packages/snaps-execution-environments/jest.config.js
@@ -3,13 +3,13 @@ const deepmerge = require('deepmerge');
 const baseConfig = require('../../jest.config.base');
 
 module.exports = deepmerge(baseConfig, {
-  coveragePathIgnorePatterns: ['./src/index.ts'],
+  coveragePathIgnorePatterns: ['./src/index.ts', './src/common/test-utils'],
   coverageThreshold: {
     global: {
-      branches: 57.06,
-      functions: 57.41,
-      lines: 57.7,
-      statements: 57.86,
+      branches: 60.11,
+      functions: 70.07,
+      lines: 69.54,
+      statements: 69.63,
     },
   },
   testTimeout: 10000,
