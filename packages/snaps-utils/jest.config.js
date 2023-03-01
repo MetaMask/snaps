619ce879095706179be376797734669aee185ba5
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2023-03-01 12:18:44 +0100

    Remove JSDOM from snaps-utils

--- packages/snaps-utils/jest.config.js
+++ packages/snaps-utils/jest.config.js
@@ -2,21 +2,20 @@ const deepmerge = require('deepmerge');
 
 const baseConfig = require('../../jest.config.base');
 
-const coveragePathIgnorePatterns = [
-  './src/index.ts',
-  './src/index.browser.ts',
-  './src/virtual-file/index.ts',
-  './src/virtual-file/index.browser.ts',
-  './src/manifest/index.ts',
-  './src/manifest/index.browser.ts',
-  './src/test-utils',
-  './src/json-schemas',
-  // Jest currently doesn't collect coverage for child processes.
-  // https://github.com/facebook/jest/issues/5274
-  './src/eval-worker.ts',
-];
-
 module.exports = deepmerge(baseConfig, {
+  coveragePathIgnorePatterns: [
+    './src/index.ts',
+    './src/index.browser.ts',
+    './src/virtual-file/index.ts',
+    './src/virtual-file/index.browser.ts',
+    './src/manifest/index.ts',
+    './src/manifest/index.browser.ts',
+    './src/test-utils',
+    './src/json-schemas',
+    // Jest currently doesn't collect coverage for child processes.
+    // https://github.com/facebook/jest/issues/5274
+    './src/eval-worker.ts',
+  ],
   coverageThreshold: {
     global: {
       branches: 92.89,
@@ -25,26 +24,4 @@ module.exports = deepmerge(baseConfig, {
       statements: 98.94,
     },
   },
-  projects: [
-    deepmerge(baseConfig, {
-      coveragePathIgnorePatterns,
-      testMatch: ['<rootDir>/src/iframe.test.ts'],
-
-      // These options are required to run iframes in JSDOM.
-      testEnvironment: '<rootDir>/jest.environment.js',
-      testEnvironmentOptions: {
-        resources: 'usable',
-        runScripts: 'dangerously',
-        customExportConditions: ['node', 'node-addons'],
-      },
-    }),
-    deepmerge(baseConfig, {
-      coveragePathIgnorePatterns,
-      testPathIgnorePatterns: ['<rootDir>/src/iframe.test.ts'],
-      testEnvironmentOptions: {
-        customExportConditions: ['node', 'node-addons'],
-      },
-    }),
-  ],
-  testTimeout: 2500,
 });
