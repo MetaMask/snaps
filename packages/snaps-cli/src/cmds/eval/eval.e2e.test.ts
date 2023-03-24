ae32395e4d3e034f42773b468a52343395968add
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2023-03-24 11:25:14 +0100

    Debug: Add .wait() and remove .kill()

--- packages/snaps-cli/src/cmds/eval/eval.e2e.test.ts
+++ packages/snaps-cli/src/cmds/eval/eval.e2e.test.ts
@@ -2,7 +2,7 @@ import { run } from '@metamask/snaps-cli/test-utils';
 import { join } from 'path';
 
 describe('mm-snap eval', () => {
-  it.skip.each(['eval', 'e'])(
+  it.each(['eval', 'e'])(
     'evaluates a snap using "mm-snap %s"',
     async (command) => {
       await run({
@@ -10,34 +10,26 @@ describe('mm-snap eval', () => {
         options: [`--bundle ${join('__test__', 'eval.js')}`],
         workingDirectory: __dirname,
       })
+        .wait(
+          'stdout',
+          `Eval Success: evaluated '${join('__test__', 'eval.js')}' in SES!`,
+        )
         .stdout(
           `Eval Success: evaluated '${join('__test__', 'eval.js')}' in SES!`,
         )
-        .kill()
         .end();
     },
   );
 
   it('shows a message if the evaluation failed', async () => {
-    const runner = run({
+    await run({
       command: 'eval',
       options: [`--bundle ${join('__test__', 'eval-2.js')}`],
       workingDirectory: __dirname,
     })
+      .wait('stderr', 'Eval failed.')
       .stderr('Eval failed.')
       .code(1)
-      .kill();
-
-    await runner.end();
-
-    console.log('Runner connected:', runner.proc.connected);
-    console.log('Runner killed:', runner.proc.killed);
-
-    if (!runner.proc.killed) {
-      console.log('Killing runner...');
-      runner.proc.kill();
-    }
-
-    runner.proc.unref();
+      .end();
   });
 });
--- packages/snaps-cli/src/test-utils/e2e.ts
+++ packages/snaps-cli/src/test-utils/e2e.ts
@@ -24,7 +24,7 @@ export function run({
   workingDirectory = SNAP_DIR,
 }: RunOptions) {
   return runner()
-    .debug(LogLevel.Verbose)
+    .debug(LogLevel.ERROR)
     .cwd(workingDirectory)
     .spawn(
       'yarn',
