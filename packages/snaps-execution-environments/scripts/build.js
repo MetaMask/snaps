07e57ea020b4769068ba889b8869d70a25693637
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2023-04-05 10:15:41 +0200

    Revert "Refactor snaps-execution-environments build to worker threads"
    This reverts commit 23e979cd27268c6ae57be004bc44562ba1ea1fcf.

--- packages/snaps-execution-environments/scripts/build.js
+++ packages/snaps-execution-environments/scripts/build.js
@@ -6,7 +6,6 @@ const browserify = require('browserify');
 const { promises: fs } = require('fs');
 const LavaMoatBrowserify = require('lavamoat-browserify');
 const path = require('path');
-const { Worker, workerData, isMainThread } = require('worker_threads');
 const yargs = require('yargs');
 
 const defaultResolvePath = createResolvePath();
@@ -59,173 +58,147 @@ async function main() {
   );
 
   await Promise.all(
-    Object.entries(ENTRY_POINTS).map(([key, config]) => {
-      console.log(`Building ${key}.`);
+    Object.entries(ENTRY_POINTS).map(async ([key, config]) => {
+      console.log('Bundling', key);
+      const { html, node, worker, entryPoint } = config;
+      const insertGlobalVars = node
+        ? { process: undefined, ...LavaMoatBrowserify.args.insertGlobalVars }
+        : LavaMoatBrowserify.args.insertGlobalVars;
 
-      const worker = new Worker(__filename, {
-        stdout: true,
-        stderr: true,
-        workerData: { key, config, writeAutoPolicy },
+      const bundler = browserify(entryPoint, {
+        ...LavaMoatBrowserify.args,
+        insertGlobalVars,
+        extensions: ['.ts'],
+        node,
       });
 
-      worker.stderr.on('data', (data) => {
-        console.log(data.toString());
+      if (node) {
+        bundler.external([
+          'worker_threads',
+          'buffer',
+          'stream',
+          'tty',
+          'crypto',
+          'util',
+          'events',
+          'os',
+          'timers',
+          'fs',
+        ]);
+      }
+
+      bundler.transform(require('babelify'), {
+        extensions: ['.js', '.ts'],
+        presets: [
+          require('@babel/preset-typescript'),
+          [
+            require('@babel/preset-env'),
+            {
+              targets: {
+                browsers: ['chrome >= 80', 'firefox >= 78'],
+              },
+            },
+          ],
+        ],
+        plugins: [
+          [
+            require('babel-plugin-tsconfig-paths-module-resolver'),
+            {
+              resolvePath: (sourcePath, currentFile, opts) => {
+                const result = defaultResolvePath(
+                  sourcePath,
+                  currentFile,
+                  opts,
+                );
+                // Result is null for modules found in node_modules
+                if (!result) {
+                  return null;
+                }
+                // Force resolve `@metamask/snaps-utils` to browser bundle regardless of environment to reduce bundle size
+                // Use default resolver for everything else
+                if (
+                  sourcePath === '@metamask/snaps-utils' &&
+                  result.includes('../snaps-utils/src')
+                ) {
+                  return `${result}/index.browser`;
+                }
+                return result;
+              },
+            },
+          ],
+        ],
       });
 
-      return new Promise((resolve, reject) => {
-        worker.on('exit', (code) => {
-          if (code !== 0) {
-            reject(new Error(`Worker stopped with exit code ${code}`));
+      // Tree shaking
+      bundler.transform(require('@browserify/uglifyify'), { global: true });
+      bundler.plugin(require('common-shakeify'), { ecmaVersion: 2020 });
+
+      let extraOptions = {};
+
+      if (worker) {
+        extraOptions = {
+          scuttleGlobalThis: true,
+          scuttleGlobalThisExceptions: [
+            'postMessage',
+            'removeEventListener',
+            'isSecureContext',
+          ],
+        };
+      }
+
+      // Add LavaMoat to wrap bundles in LavaPack
+      // For Node.js builds, this also includes a prelude that contains SES and the LavaMoat runtime
+      // For browser builds, the prelude is skipped and inlined in a script tag before the main bundle instead
+      bundler.plugin(LavaMoatBrowserify, {
+        writeAutoPolicy,
+        policy: path.resolve(
+          __dirname,
+          `../lavamoat/browserify/${key}/policy.json`,
+        ),
+        policyName: key,
+        policyOverride: path.resolve(
+          __dirname,
+          `../lavamoat/browserify/policy-override.json`,
+        ),
+        // Prelude is included in Node, in the browser it is inlined.
+        includePrelude: node || worker,
+        ...extraOptions,
+      });
+
+      const buffer = await new Promise((resolve, reject) => {
+        bundler.bundle((error, bundle) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(bundle);
           }
-
-          console.log(`Finished building ${key}.`);
-          resolve();
         });
       });
-    }),
-  );
-}
 
-/**
- * Build snaps execution environments using Browserify and LavaMoat.
- */
-async function build() {
-  const { key, config, writeAutoPolicy } = workerData;
-  const { html, node, worker, entryPoint } = config;
+      const bundlePath = path.join(OUTPUT_PATH, key, OUTPUT_BUNDLE);
+      await fs.mkdir(path.dirname(bundlePath), { recursive: true });
+      await fs.writeFile(bundlePath, buffer);
 
-  const insertGlobalVars = node
-    ? { process: undefined, ...LavaMoatBrowserify.args.insertGlobalVars }
-    : LavaMoatBrowserify.args.insertGlobalVars;
-  const bundler = browserify(entryPoint, {
-    ...LavaMoatBrowserify.args,
-    insertGlobalVars,
-    extensions: ['.ts'],
-    node,
-  });
+      const lavaMoatRuntimeString = await fs.readFile(
+        require.resolve('@lavamoat/lavapack/src/runtime.js'),
+        'utf-8',
+      );
 
-  if (node) {
-    bundler.external([
-      'worker_threads',
-      'buffer',
-      'stream',
-      'tty',
-      'crypto',
-      'util',
-      'events',
-      'os',
-      'timers',
-      'fs',
-    ]);
-  }
+      if (html) {
+        const lavaMoatRuntime = lavaMoatRuntimeString.replace(
+          '__lavamoatSecurityOptions__',
+          JSON.stringify({
+            // Only enable for browser builds for now due to incompatiblities
+            scuttleGlobalThis: true,
+            scuttleGlobalThisExceptions: [
+              'postMessage',
+              'removeEventListener',
+              'isSecureContext',
+            ],
+          }),
+        );
 
-  bundler.transform(require('babelify'), {
-    extensions: ['.js', '.ts'],
-    presets: [
-      require('@babel/preset-typescript'),
-      [
-        require('@babel/preset-env'),
-        {
-          targets: {
-            browsers: ['chrome >= 80', 'firefox >= 78'],
-          },
-        },
-      ],
-    ],
-    plugins: [
-      [
-        require('babel-plugin-tsconfig-paths-module-resolver'),
-        {
-          resolvePath: (sourcePath, currentFile, opts) => {
-            const result = defaultResolvePath(sourcePath, currentFile, opts);
-            // Result is null for modules found in node_modules
-            if (!result) {
-              return null;
-            }
-            // Force resolve `@metamask/snaps-utils` to browser bundle regardless of environment to reduce bundle size
-            // Use default resolver for everything else
-            if (
-              sourcePath === '@metamask/snaps-utils' &&
-              result.includes('../snaps-utils/src')
-            ) {
-              return `${result}/index.browser`;
-            }
-            return result;
-          },
-        },
-      ],
-    ],
-  });
-
-  // Tree shaking
-  bundler.transform(require('@browserify/uglifyify'), { global: true });
-  bundler.plugin(require('common-shakeify'), { ecmaVersion: 2020 });
-
-  let extraOptions = {};
-
-  if (worker) {
-    extraOptions = {
-      scuttleGlobalThis: true,
-      scuttleGlobalThisExceptions: [
-        'postMessage',
-        'removeEventListener',
-        'isSecureContext',
-      ],
-    };
-  }
-
-  // Add LavaMoat to wrap bundles in LavaPack
-  // For Node.js builds, this also includes a prelude that contains SES and the LavaMoat runtime
-  // For browser builds, the prelude is skipped and inlined in a script tag before the main bundle instead
-  bundler.plugin(LavaMoatBrowserify, {
-    writeAutoPolicy,
-    policy: path.resolve(
-      __dirname,
-      `../lavamoat/browserify/${key}/policy.json`,
-    ),
-    policyName: key,
-    policyOverride: path.resolve(
-      __dirname,
-      `../lavamoat/browserify/policy-override.json`,
-    ),
-    // Prelude is included in Node, in the browser it is inlined.
-    includePrelude: node || worker,
-    ...extraOptions,
-  });
-
-  const buffer = await new Promise((resolve, reject) => {
-    bundler.bundle((error, bundle) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(bundle);
-      }
-    });
-  });
-
-  const bundlePath = path.join(OUTPUT_PATH, key, OUTPUT_BUNDLE);
-  await fs.mkdir(path.dirname(bundlePath), { recursive: true });
-  await fs.writeFile(bundlePath, buffer);
-
-  const lavaMoatRuntimeString = await fs.readFile(
-    require.resolve('@lavamoat/lavapack/src/runtime.js'),
-    'utf-8',
-  );
-
-  if (html) {
-    const lavaMoatRuntime = lavaMoatRuntimeString.replace(
-      '__lavamoatSecurityOptions__',
-      JSON.stringify({
-        // Only enable for browser builds for now due to incompatibilities
-        scuttleGlobalThis: true,
-        scuttleGlobalThisExceptions: [
-          'postMessage',
-          'removeEventListener',
-          'isSecureContext',
-        ],
-      }),
-    );
-
-    const htmlFile = `
+        const htmlFile = `
       <!DOCTYPE html>
       <html>
         <head>
@@ -236,14 +209,18 @@ async function build() {
         </head>
       </html>`;
 
-    const htmlPath = path.join(OUTPUT_PATH, key, OUTPUT_HTML);
-    await fs.mkdir(path.dirname(htmlPath), { recursive: true });
-    await fs.writeFile(htmlPath, htmlFile);
-  }
+        const htmlPath = path.join(OUTPUT_PATH, key, OUTPUT_HTML);
+        await fs.mkdir(path.dirname(htmlPath), { recursive: true });
+        await fs.writeFile(htmlPath, htmlFile);
+      }
+
+      console.log('Finished', key);
+      return buffer;
+    }),
+  );
 }
 
-const mainFunction = isMainThread ? main : build;
-mainFunction().catch((error) => {
+main().catch((error) => {
   console.error(error);
   process.exit(1);
 });
