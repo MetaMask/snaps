0fbe543357c2546def55c3f082f2252f82a08389
Frederik Bolding <frederik.bolding@gmail.com>
2023-04-05 12:36:03 +0200

    Fix export

--- packages/rpc-methods/src/index.ts
+++ packages/rpc-methods/src/index.ts
@@ -1,7 +1,7 @@
 export type { PermittedRpcMethodHooks } from './permitted';
 export {
   handlers as permittedMethods,
-  createSnapMethodMiddleware,
+  createSnapsMethodMiddleware,
 } from './permitted';
 export * from './restricted';
 export { SnapCaveatType } from '@metamask/snaps-utils';
