8b788098071ce2ec5671fcd0bec1b0fc670e1cdd
hmalik88 <hassan.malik@consensys.net>
2023-04-24 16:17:05 -0400

    fix targets

--- packages/rpc-methods/jest.config.js
+++ packages/rpc-methods/jest.config.js
@@ -10,10 +10,10 @@ module.exports = deepmerge(baseConfig, {
   ],
   coverageThreshold: {
     global: {
-      branches: 84.95,
+      branches: 84.82,
       functions: 98.75,
-      lines: 97.24,
-      statements: 97.25,
+      lines: 97.23,
+      statements: 97.24,
     },
   },
 });
