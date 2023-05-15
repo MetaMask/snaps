52ec5761ad83ad4867f48b9f17e33a53dfc1e776
Frederik Bolding <frederik.bolding@gmail.com>
2023-05-15 16:36:42 +0200

    Add test

---
+++ packages/snaps-utils/src/icon.test.ts
@@ -0,0 +1,40 @@
+import { stringToBytes } from '@metamask/utils';
+
+import {
+  SVG_MAX_BYTE_SIZE,
+  SVG_MAX_BYTE_SIZE_TEXT,
+  assertIsSnapIcon,
+} from './icon';
+import { VirtualFile } from './virtual-file';
+
+describe('assertIsSnapIcon', () => {
+  it('asserts that the file path ends with .svg', () => {
+    const icon = new VirtualFile({
+      value: stringToBytes('foo'),
+      path: 'foo.png',
+    });
+    expect(() => assertIsSnapIcon(icon)).toThrow(
+      'Expected snap icon to end in .svg',
+    );
+  });
+
+  it('asserts that the file is an appropriate size', () => {
+    const icon = new VirtualFile({
+      value: new Uint8Array(SVG_MAX_BYTE_SIZE + 1).fill(1),
+      path: 'foo.svg',
+    });
+    expect(() => assertIsSnapIcon(icon)).toThrow(
+      `The specified SVG icon exceeds the maximum size of ${SVG_MAX_BYTE_SIZE_TEXT}.`,
+    );
+  });
+
+  it('asserts that the file is a valid SVG', () => {
+    const icon = new VirtualFile({
+      value: stringToBytes('foo'),
+      path: 'foo.svg',
+    });
+    expect(() => assertIsSnapIcon(icon)).toThrow(
+      'Snap icon must be a valid SVG',
+    );
+  });
+});
--- packages/snaps-utils/src/icon.ts
+++ packages/snaps-utils/src/icon.ts
@@ -11,10 +11,10 @@ export const SVG_MAX_BYTE_SIZE_TEXT = `${Math.floor(
 export const assertIsSnapIcon = (icon: VirtualFile) => {
   assert(icon.path.endsWith('.svg'), 'Expected snap icon to end in .svg');
 
-  assert(isSvg(icon.toString('utf8')), 'Snap icon must be a valid SVG');
-
   assert(
     Buffer.byteLength(icon.value, 'utf8') <= SVG_MAX_BYTE_SIZE,
     `The specified SVG icon exceeds the maximum size of ${SVG_MAX_BYTE_SIZE_TEXT}.`,
   );
+
+  assert(isSvg(icon.toString()), 'Snap icon must be a valid SVG');
 };
