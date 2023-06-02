c0b762e41a5aaa5f42d2419f0a7d03d33408168a
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2023-06-02 13:30:28 +0200

    Lint each package in a separate CI job (#1463)
    * Lint each package in a separate CI job
    
    * Add dependencies to each package
    
    * Ignore examples folder in examples package ESLint
    
    * Fix LavaMoat policy
    
    * Add missing files
    
    * Fix linting of LavaMoat policy
    
    * Apply suggestions from code review
    
    Co-authored-by: Frederik Bolding <frederik.bolding@gmail.com>
    
    * Lint changelog in examples and snaps-simulator
    
    * Temporarily remove constraints
    
    * Filter PRIVATE_WORKSPACES from all workspaces
    
    * Run Prettier from root
    
    * Update package.json
    
    Co-authored-by: Frederik Bolding <frederik.bolding@gmail.com>
    
    ---------
    
    Co-authored-by: Frederik Bolding <frederik.bolding@gmail.com>

--- scripts/child-workspace-package-names-as-json.ts
+++ scripts/child-workspace-package-names-as-json.ts
@@ -8,16 +8,30 @@ const PRIVATE_WORKSPACES = ['@metamask/snaps-simulator'];
  * The entrypoint to this script.
  */
 async function main() {
+  const args = process.argv.slice(2);
+  const all = args.includes('--all');
+  const extraArgs = all ? [] : ['--no-private'];
+
   const { stdout } = await execa('yarn', [
     'workspaces',
     'list',
-    '--no-private',
     '--json',
+    ...extraArgs,
   ]);
 
   const workspaces = stdout.split('\n').map((line) => JSON.parse(line));
   const childWorkspaceNames = workspaces
-    .filter((workspace) => workspace.location !== '.')
+    .filter((workspace) => {
+      if (PRIVATE_WORKSPACES.includes(workspace.name)) {
+        return false;
+      }
+
+      if (all) {
+        return true;
+      }
+
+      return workspace.location !== '.';
+    })
     .map((workspace) => workspace.name);
 
   // eslint-disable-next-line no-console
