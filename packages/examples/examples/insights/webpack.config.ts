d728aabea0d4920cda46d205ce9566db79f8cfc1
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2023-06-07 23:12:55 +0200

    Attempt to run E2E without dist files

---
+++ packages/examples/examples/insights/webpack.config.ts
@@ -0,0 +1,45 @@
+import { resolve } from 'path';
+import TerserPlugin from 'terser-webpack-plugin';
+import { TsconfigPathsPlugin } from 'tsconfig-paths-webpack-plugin';
+import { Configuration } from 'webpack';
+
+const config: Configuration = {
+  entry: './src/index.ts',
+  mode: 'production',
+  stats: 'errors-only',
+
+  output: {
+    filename: 'bundle.js',
+    path: resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/u,
+        use: {
+          loader: 'swc-loader',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    plugins: [
+      // @ts-expect-error - `TsconfigPathsPlugin` is typed incorrectly.
+      new TsconfigPathsPlugin({
+        configFile: resolve(__dirname, 'tsconfig.json'),
+        baseUrl: __dirname,
+      }),
+    ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        minify: TerserPlugin.swcMinify,
+      }),
+    ],
+  },
+};
+
+export default config;
