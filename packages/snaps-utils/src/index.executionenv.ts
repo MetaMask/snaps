9cb33818691f4194d422d1bc102a946d1d804008
Frederik Bolding <frederik.bolding@gmail.com>
2023-06-07 11:21:03 +0200

    Execution environment bundling refactor (#1458)
    * Minify execution bundles
    
    * Add special snaps-utils entry point + minify LM runtime
    
    * Disable sourceMap
    
    * Exclude crypto module from execution environments
    
    * Only construct the htmlFile once
    
    * Add missing export
    
    * Remove minify-stream and use terser ourselves
    
    * Update comments
    
    * Stop minifying SES/LM

---
+++ packages/snaps-utils/src/index.executionenv.ts
@@ -0,0 +1,5 @@
+// Special entrypoint for execution environments for bundle sizing reasons
+export * from './handlers';
+export * from './logging';
+export * from './namespace';
+export * from './types';
