bdd8a631907f0e6f0d1e8f6dddff91f3f9f99c72
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2023-06-08 16:47:19 +0200

    Use @ts-expect-error

--- packages/snaps-simulator/src/utils/environment.ts
+++ packages/snaps-simulator/src/utils/environment.ts
@@ -1,19 +1,10 @@
-declare global {
-  // eslint-disable-next-line @typescript-eslint/no-namespace
-  namespace NodeJS {
-    // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
-    interface ProcessEnv {
-      // eslint-disable-next-line @typescript-eslint/naming-convention
-      SNAPS_TEST: boolean | string;
-    }
-  }
-}
-
 /**
  * Returns true if the current build is a test build.
  *
  * @returns True if the current build is a test build. Otherwise, false.
  */
 export function isTestBuild() {
+  // @ts-expect-error - Webpack replaces the value of SNAPS_TEST with a boolean
+  // at build time, but TypeScript doesn't know that.
   return process.env.SNAPS_TEST === true || process.env.SNAPS_TEST === 'true';
 }
