554e9c6b09e039d9f111bcb16a05e13c23ec7108
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2023-06-12 09:49:23 +0200

    Add documentation and E2E tests to dialog snap

--- packages/test-snaps/packages/dialogs/README.md
+++ packages/test-snaps/packages/dialogs/README.md
@@ -1,3 +1,34 @@
-# Dialog Test Snap
+# `@metamask/dialog-example-snap`
 
-A MetaMask Test Snap that uses the snap_dialog permission.
+This snap demonstrates the use of `snap_dialog` to display a dialog to the user.
+
+## Snap manifest
+
+> **Note**: Using `snap_dialog` requires the `snap_dialog` permission. Refer to
+> [the documentation](https://docs.metamask.io/snaps/reference/rpc-api/#snap_getbip44entropy)
+> for more information.
+
+Along with other permissions, the manifest of this snap includes the
+`snap_dialog` permission:
+
+```json
+{
+  "initialPermissions": {
+    "snap_dialog": {}
+  }
+}
+```
+
+This permission does not require any additional configuration.
+
+## Snap usage
+
+This snap exposes an `onRpcRequest` handler, which supports the following
+JSON-RPC methods, one for each type of dialog:
+
+- `showAlert`: Displays an alert dialog to the user.
+- `showConfirmation`: Displays a confirmation dialog to the user.
+- `showPrompt`: Displays a prompt dialog to the user.
+
+For the sake of simplicity, the methods do not accept any parameters, and
+return the value returned by `snap_dialog` directly.
--- packages/test-snaps/packages/dialogs/src/index.ts
+++ packages/test-snaps/packages/dialogs/src/index.ts
@@ -1,35 +1,80 @@
-import { DialogType } from '@metamask/rpc-methods';
-import { OnRpcRequestHandler } from '@metamask/snaps-types';
+import { rpcErrors } from '@metamask/rpc-errors';
+import { OnRpcRequestHandler, DialogType } from '@metamask/snaps-types';
 import { panel, text, heading } from '@metamask/snaps-ui';
 
+/**
+ * Handle incoming JSON-RPC requests from the dapp, sent through the
+ * `wallet_invokeSnap` method. This handler handles three methods, one for each
+ * type of dialog:
+ *
+ * - `showAlert`: Show an alert dialog.
+ * - `showConfirmation`: Show a confirmation dialog.
+ * - `showPrompt`: Show a prompt dialog.
+ *
+ * The dialogs are shown using the [`snap_dialog`](https://docs.metamask.io/snaps/reference/rpc-api/#snap_dialog)
+ * method.
+ *
+ * @param params - The request parameters.
+ * @param params.request - The JSON-RPC request object.
+ * @returns The JSON-RPC response.
+ * @see https://docs.metamask.io/snaps/reference/exports/#onrpcrequest
+ * @see https://docs.metamask.io/snaps/reference/rpc-api/#wallet_invokesnap
+ * @see https://docs.metamask.io/snaps/reference/rpc-api/#snap_dialog
+ */
 export const onRpcRequest: OnRpcRequestHandler = async ({ request }) => {
   switch (request.method) {
-    case 'dialogAlert':
+    case 'showAlert':
       return snap.request({
         method: 'snap_dialog',
         params: {
+          // For this example we're using `DialogType.Alert`, but you can also
+          // use the literal string value "alert" here.
           type: DialogType.Alert,
-          content: panel([heading('Alert Dialog'), text('Text here')]),
+          content: panel([
+            heading('Alert Dialog'),
+            text('This is an alert dialog. It has a single button: "OK".'),
+          ]),
         },
       });
-    case 'dialogConf':
+
+    case 'showConfirmation':
       return snap.request({
         method: 'snap_dialog',
         params: {
+          // For this example we're using `DialogType.Confirmation`, but you can
+          // also use the literal string value "confirmation" here.
           type: DialogType.Confirmation,
-          content: panel([heading('Confirmation Dialog'), text('Text here')]),
+          content: panel([
+            heading('Confirmation Dialog'),
+            text(
+              'This is a confirmation dialog. It has two buttons: "OK" and "Cancel," letting the user choose whether to proceed with an action.',
+            ),
+          ]),
         },
       });
-    case 'dialogPrompt':
+
+    case 'showPrompt':
       return snap.request({
         method: 'snap_dialog',
         params: {
+          // For this example we're using `DialogType.Prompt`, but you can also
+          // use the literal string value "prompt" here.
           type: DialogType.Prompt,
-          content: panel([heading('Prompt Dialog'), text('Text here')]),
-          placeholder: 'placeholder',
+          content: panel([
+            heading('Prompt Dialog'),
+            text(
+              'This is a prompt dialog. In addition to the "OK" and "Cancel" buttons, it has a text input field.',
+            ),
+          ]),
+          placeholder: 'This is shown in the input.',
         },
       });
+
     default:
-      throw new Error('Method not found.');
+      throw rpcErrors.methodNotFound({
+        data: {
+          method: request.method,
+        },
+      });
   }
 };
