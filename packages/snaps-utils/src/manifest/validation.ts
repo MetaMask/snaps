e40bec7c5b67ec1078a2de8a3e280767743a19b6
hmalik88 <hassan.malik@consensys.net>
2023-06-14 17:34:26 -0400

    added audit bip32 validation fix

--- packages/snaps-utils/src/manifest/validation.ts
+++ packages/snaps-utils/src/manifest/validation.ts
@@ -47,11 +47,26 @@ const FORBIDDEN_PATHS: string[][] = FORBIDDEN_COIN_TYPES.map((coinType) => [
   `${coinType}'`,
 ]);
 
-const BIP32_INDEX_REGEX = /^\d+'?$/u;
+const BIP32_INDEX_REGEX = /^(\d+)'?$/u;
+
+const isValidBip32Path = (path: string[]) => {
+  return path.slice(1).every((part) => {
+    const pathIndex = Number(BIP32_INDEX_REGEX.exec(part)?.[1]);
+    if (
+      Number.isInteger(pathIndex) &&
+      pathIndex >= 0 &&
+      pathIndex <= 2 ** 31 - 1
+    ) {
+      return true;
+    }
+    return false;
+  });
+};
+
 export const Bip32PathStruct = refine(
   array(string()),
   'BIP-32 path',
-  (path) => {
+  (path: string[]) => {
     if (path.length === 0) {
       return 'Path must be a non-empty BIP-32 derivation path array';
     }
@@ -64,7 +79,7 @@ export const Bip32PathStruct = refine(
       return 'Paths must have a length of at least three.';
     }
 
-    if (path.slice(1).some((part) => !BIP32_INDEX_REGEX.test(part))) {
+    if (!isValidBip32Path(path)) {
       return 'Path must be a valid BIP-32 derivation path array.';
     }
 
