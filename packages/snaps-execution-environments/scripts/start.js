009e3123341f2f9ceea50a754e340b56c53edfdb
hmalik88 <hassan.malik@consensys.net>
2023-06-15 06:48:28 -0400

    fix port number

--- packages/snaps-execution-environments/scripts/start.js
+++ packages/snaps-execution-environments/scripts/start.js
@@ -14,7 +14,7 @@ start();
  * integer port.
  */
 async function start() {
-  const port = parseInt(process.argv[2] ?? 6969, 10);
+  const port = parseInt(process.argv[2] ?? 6363, 10);
   if (!Number.isSafeInteger(port) || port < 0) {
     throw new Error(`Invalid port: "${port}"`);
   }
