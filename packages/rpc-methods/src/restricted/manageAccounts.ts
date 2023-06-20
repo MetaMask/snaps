e6db6b72f2005f642e5598914ff5c32af31c2de6
Gustavo Antunes <17601467+gantunesr@users.noreply.github.com>
2023-06-20 08:26:33 -0400

    Update manageAccounts method

--- packages/rpc-methods/src/restricted/manageAccounts.ts
+++ packages/rpc-methods/src/restricted/manageAccounts.ts
@@ -1,3 +1,4 @@
+import { SnapKeyring } from '@metamask/eth-snap-keyring';
 import {
   SubjectType,
   PermissionType,
@@ -5,49 +6,21 @@ import {
   ValidPermissionSpecification,
   PermissionSpecificationBuilder,
 } from '@metamask/permission-controller';
-// import { SnapCaveatType, isChainId } from '@metamask/snaps-utils';
-import { isCaipAccount } from '@metamask/snaps-utils';
 import { Json, NonEmptyArray } from '@metamask/utils';
 import { ethErrors } from 'eth-rpc-errors';
 
 export const methodName = 'snap_manageAccounts';
 
 export type ManageAccountParams = {
-  action: ManageAccountsOperation;
+  action: string;
   accountId?: string;
 };
-
-export enum ManageAccountsOperation {
-  ListAccounts = 'list',
-  CreateAccount = 'create',
-  ReadAccount = 'read',
-  UpdateAccount = 'update',
-  RemoveAccount = 'remove',
-}
-
-export enum ValidManageAccountMethods {
-  ListAccounts = 'listAccounts',
-  CreateAccount = 'createAccount',
-  ReadAccount = 'readAccount',
-  UpdateAccount = 'updateAccount',
-  RemoveAccount = 'remove',
-  DeleteAccount = 'deleteAccount',
-  DeleteAccountByOrigin = 'deleteAccountByOrigin',
-}
 
 export type ManageAccountsMethodHooks = {
   /**
    * Gets the snap keyring implementation.
    */
-  getSnapKeyring: () => Promise<{
-    listAccounts(origin: string): Promise<string[]>;
-    createAccount(origin: string, accountId: string): Promise<boolean>;
-    readAccount(origin: string, accountId: string): Promise<Json>;
-    updateAccount(origin: string, accountId: string, methodArgs?: any): any;
-    removeAccount(origin: string, accountId: string): Promise<boolean>;
-    deleteAccount(accountId: string): any;
-    deleteAccountByOrigin(origin: string): any;
-  }>;
+  getSnapKeyring: (snapOrigin: string) => Promise<SnapKeyring>;
 
   /**
    * Saves the snap keyring, should be called after mutable operations.
@@ -104,67 +77,12 @@ export function manageAccountsImplementation({
       throw ethErrors.rpc.invalidParams('Invalid ManageAccount Arguments');
     }
 
-    const keyring = await getSnapKeyring();
-
-    if (params.action === ManageAccountsOperation.ListAccounts) {
-      const accounts = await keyring.listAccounts(origin);
-      return accounts;
-    }
-
-    if (!params.accountId) {
-      throw ethErrors.rpc.invalidParams(
-        'Invalid ManageAccount Arguments: Missing accountId',
-      );
-    }
-
-    // validate CAIP-10
-    if (!isCaipAccount(params.accountId)) {
-      throw ethErrors.rpc.invalidParams(
-        `Invalid ManageAccount Arguments: Invalid CAIP10 Account ${params.accountId}`,
-      );
-    }
-
-    switch (params.action) {
-      case ManageAccountsOperation.CreateAccount: {
-        const created = await keyring.createAccount(origin, params.accountId);
-        if (created) {
-          await saveSnapKeyring();
-        }
-        return created;
-      }
-      case ManageAccountsOperation.ReadAccount:
-        return keyring.readAccount(origin, params.accountId);
-      case ManageAccountsOperation.UpdateAccount: {
-        const updatedAccount = keyring.updateAccount(origin, params.accountId);
-        if (updatedAccount) {
-          await saveSnapKeyring();
-        }
-        return updatedAccount;
-      }
-
-      case ManageAccountsOperation.RemoveAccount: {
-        // NOTE: we don't call removeAccount() on the keyringController
-        // NOTE: as it prunes empty keyrings and we don't want that behavior
-        const knownAddress = await keyring.removeAccount(
-          origin,
-          params.accountId,
-        );
-        if (!knownAddress) {
-          throw ethErrors.rpc.invalidParams(
-            `Invalid ManageAccount Request: Unknown account ${params.accountId}`,
-          );
-        }
-        await saveSnapKeyring('address');
-        return knownAddress;
-      }
-
-      default: {
-        throw ethErrors.rpc.invalidRequest(
-          // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-          `Invalid ManageAccount Request: The request ${params.action} is not supported`,
-        );
-      }
-    }
+    const keyring = await getSnapKeyring(origin);
+    return await keyring.handleKeyringSnapMessage(
+      origin,
+      params,
+      saveSnapKeyring,
+    );
   };
 }
 
