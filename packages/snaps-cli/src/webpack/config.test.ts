f7e159af18a20a9e656544bf9eaf8dac18561288
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2023-07-03 18:58:39 +0200

    Fix more tests

--- packages/snaps-cli/src/webpack/config.test.ts
+++ packages/snaps-cli/src/webpack/config.test.ts
@@ -4,6 +4,13 @@ import { getMockConfig } from '../test-utils';
 import { getDefaultConfiguration } from './config';
 
 jest.dontMock('fs');
+
+jest.mock('path', () => ({
+  // For compatibility with Windows, we make `path` always use POSIX-style
+  // paths.
+  ...jest.requireActual('path/posix'),
+}));
+
 jest.mock('module', () => ({
   // Built-in modules are different across Node versions, so we need to mock
   // them out.
