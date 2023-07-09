31fb6e6307f1afcbbce0a0eaeaa86b0e3c2ba3e6
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2023-07-09 11:06:51 +0200

    Fix snap manifests

--- packages/examples/packages/wasm/snap.config.ts
+++ packages/examples/packages/wasm/snap.config.ts
@@ -4,6 +4,9 @@ import { resolve } from 'path';
 const config: SnapConfig = {
   bundler: 'webpack',
   input: resolve(__dirname, 'src/index.ts'),
+  server: {
+    port: 8019,
+  },
   experimental: {
     wasm: true,
   },
