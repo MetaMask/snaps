9b5ed07f50ee97ae1e093e2b5de711643ab667ea
Bowen Sanders <bowensanders@gmail.com>
2023-07-11 14:43:59 -0700

    fixed names in package.json (#1581)

--- packages/test-snaps/src/features/snaps/notifications/constants.ts
+++ packages/test-snaps/src/features/snaps/notifications/constants.ts
@@ -1,2 +1,2 @@
-export const NOTIFICATIONS_SNAP_ID = 'npm:@metamask/notifications-example-snap';
+export const NOTIFICATIONS_SNAP_ID = 'npm:@metamask/notification-example-snap';
 export const NOTIFICATIONS_SNAP_PORT = 8016;
