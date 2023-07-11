5073ad03497ad11df618c78aaea8e442fe4c6a00
bowensanders <bowensanders@gmail.com>
2023-07-11 14:13:35 -0700

    fixed names in package.json

--- packages/test-snaps/src/features/snaps/cronjobs/constants.ts
+++ packages/test-snaps/src/features/snaps/cronjobs/constants.ts
@@ -1,2 +1,2 @@
-export const CRONJOBS_SNAP_ID = 'npm:@metamask/cronjobs-example-snap';
+export const CRONJOBS_SNAP_ID = 'npm:@metamask/cronjob-example-snap';
 export const CRONJOBS_SNAP_PORT = 8004;
--- packages/test-snaps/src/features/snaps/dialogs/constants.ts
+++ packages/test-snaps/src/features/snaps/dialogs/constants.ts
@@ -1,2 +1,2 @@
-export const DIALOGS_SNAP_ID = 'npm:@metamask/dialogs-example-snap';
+export const DIALOGS_SNAP_ID = 'npm:@metamask/dialog-example-snap';
 export const DIALOGS_SNAP_PORT = 8005;
