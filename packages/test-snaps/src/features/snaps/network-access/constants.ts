b104ee16f639a026d2c2cc943c623a9a9845a237
bowensanders <bowensanders@gmail.com>
2023-07-19 09:59:58 -0700

    fixed npm name

--- packages/test-snaps/src/features/snaps/network-access/constants.ts
+++ packages/test-snaps/src/features/snaps/network-access/constants.ts
@@ -1,3 +1,2 @@
-export const NETWORK_ACCESS_SNAP_ID =
-  'npm:@metamask/network-access-example-snap';
+export const NETWORK_ACCESS_SNAP_ID = 'npm:@metamask/network-example-snap';
 export const NETWORK_ACCESS_PORT = 8015;
