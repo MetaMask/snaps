d2dd5b4ec1518929fb1bc2535a29669698a717ba
Guillaume Roux <guillaumeroux123@gmail.com>
2023-07-20 17:45:34 +0200

    update registry database before updating blocked snaps

--- packages/snaps-controllers/src/snaps/SnapController.ts
+++ packages/snaps-controllers/src/snaps/SnapController.ts
@@ -100,6 +100,7 @@ import type {
   SnapsRegistryInfo,
   SnapsRegistryMetadata,
   SnapsRegistryRequest,
+  UpdateDatabase,
 } from './registry';
 import { SnapsRegistryStatus } from './registry';
 import { RequestQueue } from './RequestQueue';
@@ -472,7 +473,8 @@ export type AllowedActions =
   | UpdateCaveat
   | UpdateRequestState
   | GetResult
-  | GetMetadata;
+  | GetMetadata
+  | UpdateDatabase;
 
 export type AllowedEvents = ExecutionServiceEvents;
 
@@ -943,6 +945,8 @@ export class SnapController extends BaseController<
    * for more information.
    */
   async updateBlockedSnaps(): Promise<void> {
+    await this.messagingSystem.call('SnapsRegistry:update');
+
     const blockedSnaps = await this.messagingSystem.call(
       'SnapsRegistry:get',
       Object.values(this.state.snaps).reduce<SnapsRegistryRequest>(
