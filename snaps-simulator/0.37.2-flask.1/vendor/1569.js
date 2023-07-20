f6f72211d5c123dd8dc270c56eb41f94232b44f3
Mrtenz <Mrtenz@users.noreply.github.com>
2023-07-20 17:15:21 +0000

    deploy: d7c43c34e370c8786682d2ebca2682be0221953c

---
+++ snaps-simulator/0.37.2-flask.1/vendor/1569.js
@@ -0,0 +1,6 @@
+"use strict";(self.webpackChunk_=self.webpackChunk_||[]).push([[1569],{51569:(e,t,n)=>{n.r(t),n.d(t,{conf:()=>s,language:()=>o});/*!-----------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Version: 0.38.0(0e330ae453813de4e6cf272460fb79c7117073d0)
+ * Released under the MIT license
+ * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
+ *-----------------------------------------------------------------------------*/var s={comments:{lineComment:"#"}},o={defaultToken:"keyword",ignoreCase:!0,tokenPostfix:".azcli",str:/[^#\s]/,tokenizer:{root:[{include:"@comment"},[/\s-+@str*\s*/,{cases:{"@eos":{token:"key.identifier",next:"@popall"},"@default":{token:"key.identifier",next:"@type"}}}],[/^-+@str*\s*/,{cases:{"@eos":{token:"key.identifier",next:"@popall"},"@default":{token:"key.identifier",next:"@type"}}}]],type:[{include:"@comment"},[/-+@str*\s*/,{cases:{"@eos":{token:"key.identifier",next:"@popall"},"@default":"key.identifier"}}],[/@str+\s*/,{cases:{"@eos":{token:"string",next:"@popall"},"@default":"string"}}]],comment:[[/#.*$/,{cases:{"@eos":{token:"comment",next:"@popall"}}}]]}}}}]);
