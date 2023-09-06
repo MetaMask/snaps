4b3f48d5018a676bc36311c43552d697ed81efae
Daniel Rocha <68558152+danroc@users.noreply.github.com>
2023-09-06 17:07:59 +0200

    Delegate call to `saveSnapKeyring` to `SnapKeyring`
    The `rpc-methods` repo should not have knowledge about the logic behind
    the `SnapKeyring` nor decide when its state needs to be saved.
    
    This PR passes the `saveSnapKeyring` callback function to `SnapKeyring`,
    and stops checking the methods being called.
    
    The `manageAccounts` method is still 100% covered, but since some code
    was removed, the global coverage threshold decreased.

--- packages/rpc-methods/src/restricted/manageAccounts.test.ts
+++ packages/rpc-methods/src/restricted/manageAccounts.test.ts
@@ -104,7 +104,7 @@ describe('manageAccountsImplementation', () => {
         params: {},
       }),
     ).rejects.toThrow(
-      'Expected the value to satisfy a union of `object | object`, but received: [object Object]',
+      'At path: method -- Expected a string, but received: undefined',
     );
   });
 
@@ -128,7 +128,7 @@ describe('manageAccountsImplementation', () => {
         params: { method: 123, params: {} },
       }),
     ).rejects.toThrow(
-      'Expected the value to satisfy a union of `object | object`, but received: [object Object]',
+      'At path: method -- Expected a string, but received: 123',
     );
   });
 
@@ -158,10 +158,14 @@ describe('manageAccountsImplementation', () => {
     });
 
     expect(createAccountSpy).toHaveBeenCalledTimes(1);
-    expect(createAccountSpy).toHaveBeenCalledWith(MOCK_SNAP_ID, {
-      method: 'deleteAccount',
-      params: { accountId: MOCK_CAIP_10_ACCOUNT },
-    });
+    expect(createAccountSpy).toHaveBeenCalledWith(
+      MOCK_SNAP_ID,
+      {
+        method: 'deleteAccount',
+        params: { accountId: MOCK_CAIP_10_ACCOUNT },
+      },
+      saveSnapKeyring,
+    );
     expect(requestResponse).toBe(true);
     createAccountSpy.mockClear();
   });
