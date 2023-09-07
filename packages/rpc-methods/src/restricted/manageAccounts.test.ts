10c25702e2e5a869e866a7ea7248bb88b74f32da
Daniel Rocha <68558152+danroc@users.noreply.github.com>
2023-09-07 12:03:11 +0200

    Revert change on `SnapMessageStruct` schema

--- packages/rpc-methods/src/restricted/manageAccounts.test.ts
+++ packages/rpc-methods/src/restricted/manageAccounts.test.ts
@@ -98,7 +98,7 @@ describe('manageAccountsImplementation', () => {
         params: {},
       }),
     ).rejects.toThrow(
-      'At path: method -- Expected a string, but received: undefined',
+      'Expected the value to satisfy a union of `object | object`, but received: [object Object]',
     );
   });
 
@@ -120,7 +120,7 @@ describe('manageAccountsImplementation', () => {
         params: { method: 123, params: {} },
       }),
     ).rejects.toThrow(
-      'At path: method -- Expected a string, but received: 123',
+      'Expected the value to satisfy a union of `object | object`, but received: [object Object]',
     );
   });
 
