6b0c8975da0c467107fe78bc12101d6b3bd49542
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2023-10-18 15:24:55 +0200

    Update install-chrome script

--- scripts/install-chrome.sh
+++ scripts/install-chrome.sh
@@ -5,12 +5,12 @@ set -u
 set -o pipefail
 
 # To get the latest version, see <https://www.ubuntuupdates.org/ppa/google_chrome?dist=stable>
-CHROME_VERSION='116.0.5845.110-1'
+CHROME_VERSION='118.0.5993.88-1'
 CHROME_BINARY="google-chrome-stable_${CHROME_VERSION}_amd64.deb"
 CHROME_BINARY_URL="https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/${CHROME_BINARY}"
 
 # To retrieve this checksum, run the `wget` and `shasum` commands below
-CHROME_BINARY_SHA512SUM='6a540a41c72c191680e50f299ad14a5c53f5ae53f8e458ed70759d89fa325b3ac80f56146e804603b82e4ece5a2d160dbb3f9ab73e20369e9a3f8c99a817b395'
+CHROME_BINARY_SHA512SUM='cae6a5cd8632ad350b41f4dfaf80449e6cf19d0b02816b9a1600f54b15df2adf5c4ded3792bfbe3855fa11a79ea256622f50180aa3c6779cedd75a55e7a6da9d'
 
 wget -O "${CHROME_BINARY}" -t 5 "${CHROME_BINARY_URL}"
 
