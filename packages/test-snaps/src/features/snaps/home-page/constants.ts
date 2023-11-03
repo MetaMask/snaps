d80fa9449f4f2d803e07047c209c50a0a658d471
Frederik Bolding <frederik.bolding@gmail.com>
2023-11-03 10:30:06 +0100

    Add test-snaps entry

---
+++ packages/test-snaps/src/features/snaps/home-page/constants.ts
@@ -0,0 +1,5 @@
+import packageJson from '@metamask/home-page-example-snap/package.json';
+
+export const HOME_PAGE_SNAP_ID = 'npm:@metamask/home-page-example-snap';
+export const HOME_PAGE_SNAP_PORT = 8025;
+export const HOME_PAGE_VERSION = packageJson.version;
