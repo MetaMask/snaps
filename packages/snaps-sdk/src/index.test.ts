bcf6e7535ae3ab27989879f407ae6d054ab11946
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2023-11-08 10:48:40 +0100

    Add snaps-sdk package boilerplate

---
+++ packages/snaps-sdk/src/index.test.ts
@@ -0,0 +1,5 @@
+// TODO: Add tests, and remove these comments.
+// eslint-disable-next-line import/unambiguous
+describe('index', () => {
+  it.todo('should be tested');
+});
---
+++ packages/snaps-sdk/src/index.ts
@@ -0,0 +1,2 @@
+// TODO: Add SDK exports here, and remove these comments.
+// eslint-disable-next-line import/unambiguous
