956800faf1e2d88aadf38cbcb57fda569ee216d9
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2023-11-13 11:47:00 +0100

    Add type tests to SDK

--- packages/snaps-sdk/src/types/methods/dialog.test.ts
+++ packages/snaps-sdk/src/types/methods/dialog.test.ts
@@ -1,3 +1,12 @@
+import { expectTypeOf } from 'expect-type';
+
+import type { text } from '../../ui';
+import type {
+  ConfirmationDialog,
+  PromptDialog,
+  AlertDialog,
+  DialogParams,
+} from './dialog';
 import { DialogType } from './dialog';
 
 describe('DialogType', () => {
@@ -6,5 +15,125 @@ describe('DialogType', () => {
     expect(DialogType.Alert).toBe('alert');
     expect(DialogType.Confirmation).toBe('confirmation');
     expect(DialogType.Prompt).toBe('prompt');
+  });
+});
+
+describe('AlertDialog', () => {
+  it('accepts the type as enum', () => {
+    expectTypeOf<{
+      type: DialogType.Alert;
+      content: ReturnType<typeof text>;
+    }>().toMatchTypeOf<AlertDialog>();
+  });
+
+  it('accepts the type as string', () => {
+    expectTypeOf<{
+      type: 'alert';
+      content: ReturnType<typeof text>;
+    }>().toMatchTypeOf<AlertDialog>();
+  });
+
+  it('does not accept other types', () => {
+    expectTypeOf<{
+      type: DialogType.Confirmation;
+      content: ReturnType<typeof text>;
+    }>().not.toMatchTypeOf<AlertDialog>();
+
+    expectTypeOf<{
+      type: 'confirmation';
+      content: ReturnType<typeof text>;
+    }>().not.toMatchTypeOf<AlertDialog>();
+  });
+});
+
+describe('ConfirmationDialog', () => {
+  it('accepts the type as enum', () => {
+    expectTypeOf<{
+      type: DialogType.Confirmation;
+      content: ReturnType<typeof text>;
+    }>().toMatchTypeOf<ConfirmationDialog>();
+  });
+
+  it('accepts the type as string', () => {
+    expectTypeOf<{
+      type: 'confirmation';
+      content: ReturnType<typeof text>;
+    }>().toMatchTypeOf<ConfirmationDialog>();
+  });
+
+  it('does not accept other types', () => {
+    expectTypeOf<{
+      type: DialogType.Alert;
+      content: ReturnType<typeof text>;
+    }>().not.toMatchTypeOf<ConfirmationDialog>();
+
+    expectTypeOf<{
+      type: 'alert';
+      content: ReturnType<typeof text>;
+    }>().not.toMatchTypeOf<ConfirmationDialog>();
+  });
+});
+
+describe('PromptDialog', () => {
+  it('accepts the type as enum', () => {
+    expectTypeOf<{
+      type: DialogType.Prompt;
+      content: ReturnType<typeof text>;
+    }>().toMatchTypeOf<PromptDialog>();
+  });
+
+  it('accepts the type as string', () => {
+    expectTypeOf<{
+      type: 'prompt';
+      content: ReturnType<typeof text>;
+    }>().toMatchTypeOf<PromptDialog>();
+  });
+
+  it('does not accept other types', () => {
+    expectTypeOf<{
+      type: DialogType.Alert;
+      content: ReturnType<typeof text>;
+    }>().not.toMatchTypeOf<PromptDialog>();
+
+    expectTypeOf<{
+      type: 'alert';
+      content: ReturnType<typeof text>;
+    }>().not.toMatchTypeOf<PromptDialog>();
+  });
+});
+
+describe('DialogParams', () => {
+  it('accepts the type as enum', () => {
+    expectTypeOf<{
+      type: DialogType.Alert;
+      content: ReturnType<typeof text>;
+    }>().toMatchTypeOf<DialogParams>();
+
+    expectTypeOf<{
+      type: DialogType.Confirmation;
+      content: ReturnType<typeof text>;
+    }>().toMatchTypeOf<DialogParams>();
+
+    expectTypeOf<{
+      type: DialogType.Prompt;
+      content: ReturnType<typeof text>;
+    }>().toMatchTypeOf<DialogParams>();
+  });
+
+  it('accepts the type as string', () => {
+    expectTypeOf<{
+      type: 'alert';
+      content: ReturnType<typeof text>;
+    }>().toMatchTypeOf<DialogParams>();
+
+    expectTypeOf<{
+      type: 'confirmation';
+      content: ReturnType<typeof text>;
+    }>().toMatchTypeOf<DialogParams>();
+
+    expectTypeOf<{
+      type: 'prompt';
+      content: ReturnType<typeof text>;
+    }>().toMatchTypeOf<DialogParams>();
   });
 });
---
+++ packages/snaps-sdk/src/types/permissions.test.ts
@@ -0,0 +1,83 @@
+import { expectTypeOf } from 'expect-type';
+
+import type {
+  Bip32Entropy,
+  Bip44Entropy,
+  Cronjob,
+  InitialPermissions,
+  RequestedSnap,
+} from './permissions';
+
+describe('Cronjob', () => {
+  it('has an expression and JSON-RPC request', () => {
+    const cronjob = {
+      expression: '* * * * *',
+      request: {
+        method: 'bar',
+        params: {
+          foo: 'bar',
+        },
+      },
+    };
+
+    expectTypeOf(cronjob).toMatchTypeOf<Cronjob>();
+  });
+});
+
+describe('Bip32Entropy', () => {
+  it('supports secp256k1', () => {
+    const entropy = {
+      curve: 'secp256k1' as const,
+      path: ['m', "44'"],
+    };
+
+    expectTypeOf(entropy).toMatchTypeOf<Bip32Entropy>();
+  });
+
+  it('supports ed25519', () => {
+    const entropy = {
+      curve: 'ed25519' as const,
+      path: ['m', "44'"],
+    };
+
+    expectTypeOf(entropy).toMatchTypeOf<Bip32Entropy>();
+  });
+});
+
+describe('Bip44Entropy', () => {
+  it('has a coin type', () => {
+    const entropy = {
+      coinType: 60,
+    };
+
+    expectTypeOf(entropy).toMatchTypeOf<Bip44Entropy>();
+  });
+});
+
+describe('RequestedSnap', () => {
+  it('has an optional version', () => {
+    const snap = {
+      version: '1.0.0',
+    };
+
+    expectTypeOf(snap).toMatchTypeOf<RequestedSnap>();
+  });
+
+  it('works without version', () => {
+    const snap = {};
+
+    expectTypeOf(snap).toMatchTypeOf<RequestedSnap>();
+  });
+});
+
+describe('InitialPermissions', () => {
+  it('does not accept anything for certain permissions', () => {
+    const permissions = {
+      'endowment:network-access': {
+        foo: 'bar',
+      },
+    };
+
+    expectTypeOf(permissions).not.toMatchTypeOf<InitialPermissions>();
+  });
+});
