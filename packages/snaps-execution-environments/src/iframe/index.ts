d592536992b40fc5c1a48a5332da7f65f3bf18c6
naugtur <naugtur@gmail.com>
2023-11-22 14:07:31 +0100

    initial attempt to lavamoat the webpack build. not exploding, but not working with policy yet

--- packages/snaps-execution-environments/src/iframe/index.ts
+++ packages/snaps-execution-environments/src/iframe/index.ts
@@ -1,11 +1,6 @@
-// eslint-disable-next-line import/no-unassigned-import
-import 'ses';
-
 import { executeLockdownEvents } from '../common/lockdown/lockdown-events';
 import { executeLockdownMore } from '../common/lockdown/lockdown-more';
 import { IFrameSnapExecutor } from './IFrameSnapExecutor';
-
-lockdown();
 
 // Lockdown is already applied in LavaMoat
 executeLockdownMore();
