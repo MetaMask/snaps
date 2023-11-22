9117a3175d0ef0f39e5173404d4add4f58719a03
naugtur <naugtur@gmail.com>
2023-11-22 14:07:31 +0100

    initial attempt to lavamoat the webpack build. not exploding, but not working with policy yet

--- packages/snaps-execution-environments/webpack.config.ts
+++ packages/snaps-execution-environments/webpack.config.ts
@@ -1,15 +1,21 @@
+// @ts-expect-error - no type declaration
+import LavaMoatPlugin from '@lavamoat/webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import { resolve } from 'path';
 import { TsconfigPathsPlugin } from 'tsconfig-paths-webpack-plugin';
 import type { Configuration } from 'webpack';
 import { ProvidePlugin } from 'webpack';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { merge } from 'webpack-merge';
 
 // eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires
 const swc = require('../../.swcrc.build.json');
+// eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires
+const policy = require('./lavamoat/browserify/iframe/policy.json');
 
 const baseConfig: Configuration = {
   mode: 'production',
+  // devtool: 'source-map', // the default eval-driven inline sourcemap will fail with lavamoat
   output: {
     filename: 'bundle.js',
   },
@@ -51,6 +57,16 @@ const baseConfig: Configuration = {
     new HtmlWebpackPlugin(),
     new ProvidePlugin({
       process: 'process/browser',
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      openAnalyzer: false,
+    }),
+    new LavaMoatPlugin({
+      policy,
+      diagnosticsVerbosity: 1,
+      readableResourceIds: true,
+      HtmlWebpackPluginInterop: true,
     }),
   ],
 };
