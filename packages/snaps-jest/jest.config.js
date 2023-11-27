0aebb674ac6750f8b7da14e05c807dd731689df5
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2023-11-27 22:28:27 +0100

    Increase test timeout

--- packages/snaps-jest/jest.config.js
+++ packages/snaps-jest/jest.config.js
@@ -3,6 +3,7 @@ const deepmerge = require('deepmerge');
 const baseConfig = require('../../jest.config.base');
 
 module.exports = deepmerge(baseConfig, {
+  testTimeout: 30000,
   coverageThreshold: {
     global: {
       branches: 56.06,
