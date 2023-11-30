e03e772f90fd0ec5f2c2a1c29f357cda319c4f2d
JSoufer <jonathan.ferreira@consensys.net>
2023-11-30 14:07:50 +0000

    feat: integrate webview mobile service into snaps service structure

---
+++ packages/snaps-controllers/src/services/webview/WebviewExecutionService.ts
@@ -0,0 +1,43 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+import type { Job, ExecutionServiceArgs } from '../AbstractExecutionService';
+import { AbstractExecutionService } from '../AbstractExecutionService';
+import { ProxyPostMessageStream } from '../ProxyPostMessageStream';
+import snapsState from './SnapsState';
+
+export class WebviewExecutionService extends AbstractExecutionService<Window> {
+  #snapDuplexMap?: ProxyPostMessageStream[];
+
+  constructor({ messenger, setupSnapProvider }: ExecutionServiceArgs) {
+    super({
+      messenger,
+      setupSnapProvider,
+    });
+    this.#snapDuplexMap = {};
+  }
+
+  protected async initEnvStream(jobId: string): Promise<{
+    worker: Window;
+    stream: ProxyPostMessageStream;
+  }> {
+    const iframeWindow = snapsState.webview;
+    const { stream } = snapsState;
+
+    // The WebviewExecutionService wraps the stream into a Duplex
+    // to pass the jobId to the Proxy Service
+
+    const snapStream = new ProxyPostMessageStream({
+      stream,
+      jobId,
+    });
+
+    this.#snapDuplexMap[jobId] = snapStream;
+
+    return { worker: iframeWindow, stream: snapStream };
+  }
+
+  protected terminateJob(jobWrapper: Job<Window>): void {
+    this.#snapDuplexMap[jobWrapper.id].destroy();
+    delete this.#snapDuplexMap[jobWrapper.id];
+  }
+}
