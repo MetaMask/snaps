1cd649d590ac7f8d74fe135ec6d78ab3dcdf67c9
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2023-11-30 14:17:10 +0100

    Only serve Snap files from CLI (#1979)
    Previously we would serve any files from the server root
    (`config.server.root`) when running the Snaps CLI. After this change,
    only files relevant to the Snap are served, like the manifest, bundle,
    and any auxiliary files.
    
    Related to MetaMask/MetaMask-planning#1437.

---
+++ packages/snaps-cli/src/webpack/server.ts
@@ -0,0 +1,183 @@
+import type { SnapManifest } from '@metamask/snaps-utils';
+import {
+  logError,
+  NpmSnapFileNames,
+  readJsonFile,
+} from '@metamask/snaps-utils';
+import type { IncomingMessage, Server, ServerResponse } from 'http';
+import { createServer } from 'http';
+import type { AddressInfo } from 'net';
+import { join, relative, resolve as resolvePath, sep, posix } from 'path';
+import serveMiddleware from 'serve-handler';
+
+import type { ProcessedConfig } from '../config';
+
+/**
+ * Get the relative path from one path to another.
+ *
+ * Note: This is a modified version of `path.relative` that uses Posix
+ * separators for URL-compatibility.
+ *
+ * @param from - The path to start from.
+ * @param to - The path to end at.
+ * @returns The relative path.
+ */
+function getRelativePath(from: string, to: string) {
+  return relative(from, to).split(sep).join(posix.sep);
+}
+
+/**
+ * Get the allowed paths for the static server. This includes the output file,
+ * the manifest file, and any auxiliary/localization files.
+ *
+ * @param config - The config object.
+ * @param manifest - The Snap manifest object.
+ * @returns An array of allowed paths.
+ */
+export function getAllowedPaths(
+  config: ProcessedConfig,
+  manifest: SnapManifest,
+) {
+  const auxiliaryFiles =
+    manifest.source.files?.map((file) =>
+      getRelativePath(
+        config.server.root,
+        resolvePath(config.server.root, file),
+      ),
+    ) ?? [];
+
+  const localizationFiles =
+    manifest.source.locales?.map((localization) =>
+      getRelativePath(
+        config.server.root,
+        resolvePath(config.server.root, localization),
+      ),
+    ) ?? [];
+
+  return [
+    getRelativePath(
+      config.server.root,
+      resolvePath(
+        config.server.root,
+        config.output.path,
+        config.output.filename,
+      ),
+    ),
+    getRelativePath(
+      config.server.root,
+      resolvePath(config.server.root, NpmSnapFileNames.Manifest),
+    ),
+    ...auxiliaryFiles,
+    ...localizationFiles,
+  ];
+}
+
+/**
+ * Get a static server for development purposes.
+ *
+ * Note: We're intentionally not using `webpack-dev-server` here because it
+ * adds a lot of extra stuff to the output that we don't need, and it's
+ * difficult to customize.
+ *
+ * @param config - The config object.
+ * @returns An object with a `listen` method that returns a promise that
+ * resolves when the server is listening.
+ */
+export function getServer(config: ProcessedConfig) {
+  /**
+   * Get the response for a request. This is extracted into a function so that
+   * we can easily catch errors and send a 500 response.
+   *
+   * @param request - The request.
+   * @param response - The response.
+   * @returns A promise that resolves when the response is sent.
+   */
+  async function getResponse(
+    request: IncomingMessage,
+    response: ServerResponse,
+  ) {
+    const manifestPath = join(config.server.root, NpmSnapFileNames.Manifest);
+    const { result } = await readJsonFile<SnapManifest>(manifestPath);
+    const allowedPaths = getAllowedPaths(config, result);
+
+    const path = request.url?.slice(1);
+    const allowed = allowedPaths.some((allowedPath) => path === allowedPath);
+
+    if (!allowed) {
+      response.statusCode = 404;
+      response.end();
+      return;
+    }
+
+    await serveMiddleware(request, response, {
+      public: config.server.root,
+      directoryListing: false,
+      headers: [
+        {
+          source: '**/*',
+          headers: [
+            {
+              key: 'Cache-Control',
+              value: 'no-cache',
+            },
+            {
+              key: 'Access-Control-Allow-Origin',
+              value: '*',
+            },
+          ],
+        },
+      ],
+    });
+  }
+
+  const server = createServer((request, response) => {
+    getResponse(request, response).catch(
+      /* istanbul ignore next */
+      (error) => {
+        logError(error);
+        response.statusCode = 500;
+        response.end();
+      },
+    );
+  });
+
+  /**
+   * Start the server on the port specified in the config.
+   *
+   * @param port - The port to listen on.
+   * @returns A promise that resolves when the server is listening. The promise
+   * resolves to an object with the port and the server instance. Note that if
+   * the `config.server.port` is `0`, the OS will choose a random port for us,
+   * so we need to get the port from the server after it starts.
+   */
+  const listen = async (port = config.server.port) => {
+    return new Promise<{
+      port: number;
+      server: Server;
+      close: () => Promise<void>;
+    }>((resolve, reject) => {
+      try {
+        server.listen(port, () => {
+          const close = async () => {
+            await new Promise<void>((resolveClose, rejectClose) => {
+              server.close((closeError) => {
+                if (closeError) {
+                  return rejectClose(closeError);
+                }
+
+                return resolveClose();
+              });
+            });
+          };
+
+          const address = server.address() as AddressInfo;
+          resolve({ port: address.port, server, close });
+        });
+      } catch (listenError) {
+        reject(listenError);
+      }
+    });
+  };
+
+  return { listen };
+}
