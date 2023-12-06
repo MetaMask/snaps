727148c42dea1b153b9f5cd60ae80894ebefb9dd
Frederik Bolding <frederik.bolding@gmail.com>
2023-12-06 12:09:48 +0100

    Fix test

--- packages/snaps-rpc-methods/src/restricted/manageState.test.ts
+++ packages/snaps-rpc-methods/src/restricted/manageState.test.ts
@@ -65,6 +65,11 @@ describe('snap_manageState', () => {
   });
 
   describe('getManageStateImplementation', () => {
+    // We can remove this once we drop Node 18
+    Object.defineProperty(globalThis, 'CryptoKey', {
+      value: webcrypto.CryptoKey,
+    });
+
     it('gets snap state', async () => {
       const mockSnapState = {
         some: {
