441e2f4c3177a3ea8e815ee3db31a42d1177c572
Guillaume Roux <guillaumeroux123@gmail.com>
2024-01-03 08:59:05 +0100

    add tests to new rpc methods

---
+++ packages/snaps-rpc-methods/src/restricted/showInterface.test.ts
@@ -0,0 +1,89 @@
+import { PermissionType, SubjectType } from '@metamask/permission-controller';
+import { rpcErrors } from '@metamask/rpc-errors';
+import { text } from '@metamask/snaps-sdk';
+import { MOCK_SNAP_ID } from '@metamask/snaps-utils/test-utils';
+
+import {
+  showInterfaceBuilder,
+  getShowInterfaceImplementation,
+} from './showInterface';
+
+describe('showInterfaceBuilder', () => {
+  it('has the expected shape', () => {
+    expect(showInterfaceBuilder).toStrictEqual({
+      targetName: 'snap_showInterface',
+      specificationBuilder: expect.any(Function),
+      methodHooks: {
+        showInterface: true,
+      },
+    });
+  });
+
+  it('returns the expected specification', () => {
+    const methodHooks = {
+      showInterface: jest.fn(),
+    };
+
+    expect(
+      showInterfaceBuilder.specificationBuilder({ methodHooks }),
+    ).toStrictEqual({
+      permissionType: PermissionType.RestrictedMethod,
+      targetName: 'snap_showInterface',
+      allowedCaveats: null,
+      methodImplementation: expect.any(Function),
+      subjectTypes: [SubjectType.Snap],
+    });
+  });
+});
+
+describe('getShowInterfaceImplementation', () => {
+  it('returns the expected result', async () => {
+    const showInterface = jest.fn().mockResolvedValue('foo');
+
+    const methodHooks = {
+      showInterface,
+    };
+
+    const implementation = getShowInterfaceImplementation(methodHooks);
+
+    const result = await implementation({
+      method: 'snap_showInterface',
+      params: {
+        ui: text('foo'),
+      },
+      context: {
+        origin: MOCK_SNAP_ID,
+      },
+    });
+
+    expect(result).toBe('foo');
+  });
+
+  it('throws on invalid params', async () => {
+    const showInterface = jest.fn().mockResolvedValue('foo');
+
+    const methodHooks = {
+      showInterface,
+    };
+
+    const implementation = getShowInterfaceImplementation(methodHooks);
+
+    await expect(
+      implementation({
+        method: 'snap_showInterface',
+        params: {
+          // @ts-expect-error invalid params
+          ui: 'foo',
+        },
+        context: {
+          origin: MOCK_SNAP_ID,
+        },
+      }),
+    ).rejects.toThrow(
+      rpcErrors.invalidParams({
+        message:
+          'Invalid params: At path: ui -- Expected the value to satisfy a union of `object | object | object | object | object | object | object | object | object | object | object | object`, but received: "foo".',
+      }),
+    );
+  });
+});
---
+++ packages/snaps-rpc-methods/src/restricted/updateInterface.test.ts
@@ -0,0 +1,91 @@
+import { PermissionType, SubjectType } from '@metamask/permission-controller';
+import { rpcErrors } from '@metamask/rpc-errors';
+import { text } from '@metamask/snaps-sdk';
+import { MOCK_SNAP_ID } from '@metamask/snaps-utils/test-utils';
+
+import {
+  updateInterfaceBuilder,
+  getUpdateInterfaceImplementation,
+} from './updateInterface';
+
+describe('updateInterfaceBuilder', () => {
+  it('has the expected shape', () => {
+    expect(updateInterfaceBuilder).toStrictEqual({
+      targetName: 'snap_updateInterface',
+      specificationBuilder: expect.any(Function),
+      methodHooks: {
+        updateInterface: true,
+      },
+    });
+  });
+
+  it('returns the expected specification', () => {
+    const methodHooks = {
+      updateInterface: jest.fn(),
+    };
+
+    expect(
+      updateInterfaceBuilder.specificationBuilder({ methodHooks }),
+    ).toStrictEqual({
+      permissionType: PermissionType.RestrictedMethod,
+      targetName: 'snap_updateInterface',
+      allowedCaveats: null,
+      methodImplementation: expect.any(Function),
+      subjectTypes: [SubjectType.Snap],
+    });
+  });
+});
+
+describe('getUpdateInterfaceImplementation', () => {
+  it('returns the expected result', async () => {
+    const updateInterface = jest.fn().mockResolvedValue('foo');
+
+    const methodHooks = {
+      updateInterface,
+    };
+
+    const implementation = getUpdateInterfaceImplementation(methodHooks);
+
+    const result = await implementation({
+      method: 'snap_updateInterface',
+      params: {
+        id: 'foo',
+        ui: text('foo'),
+      },
+      context: {
+        origin: MOCK_SNAP_ID,
+      },
+    });
+
+    expect(result).toBe('foo');
+  });
+
+  it('throws on invalid params', async () => {
+    const updateInterface = jest.fn().mockResolvedValue('foo');
+
+    const methodHooks = {
+      updateInterface,
+    };
+
+    const implementation = getUpdateInterfaceImplementation(methodHooks);
+
+    await expect(
+      implementation({
+        method: 'snap_updateInterface',
+        params: {
+          id: 'foo',
+          // @ts-expect-error invalid params
+          ui: 'foo',
+        },
+        context: {
+          origin: MOCK_SNAP_ID,
+        },
+      }),
+    ).rejects.toThrow(
+      rpcErrors.invalidParams({
+        message:
+          'Invalid params: At path: ui -- Expected the value to satisfy a union of `object | object | object | object | object | object | object | object | object | object | object | object`, but received: "foo".',
+      }),
+    );
+  });
+});
