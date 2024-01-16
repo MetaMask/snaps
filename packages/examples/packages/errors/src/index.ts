2675b77926b40c33188772a8375ea62141cb4f02
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2024-01-16 15:53:43 +0100

    Revert "Revert changes to error example"
    This reverts commit c18d3028d0761342474f32dd5351258ee0f05545.

--- packages/examples/packages/errors/src/index.ts
+++ packages/examples/packages/errors/src/index.ts
@@ -2,14 +2,10 @@ import type { OnRpcRequestHandler } from '@metamask/snaps-sdk';
 
 export const onRpcRequest: OnRpcRequestHandler = async () => {
   // eslint-disable-next-line no-new, @typescript-eslint/no-floating-promises
-  new Promise<void>(() => {
-    let value = 0;
-    while (value < 100) {
-      // eslint-disable-next-line no-plusplus
-      value++;
-    }
-
-    throw new Error('Random error inside a promise.');
+  new Promise<void>((_resolve, reject) => {
+    setTimeout(() => {
+      reject(new Error('Random error inside a promise.'));
+    }, 1000);
   });
 
   return 'Hello, world!';
