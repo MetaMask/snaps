305899b35258e2eabc8038bee18629cbf044a76a
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2024-01-19 11:35:35 +0100

    Simplify type a bit

--- packages/snaps-sdk/src/types/permissions.ts
+++ packages/snaps-sdk/src/types/permissions.ts
@@ -22,42 +22,42 @@ export type RequestedSnap = {
   version?: string;
 };
 
-export type InitialPermissions = {
-  'endowment:cronjob'?: {
+export type InitialPermissions = Partial<{
+  'endowment:cronjob': {
     jobs: Cronjob[];
   };
   'endowment:ethereum-provider': EmptyObject;
-  'endowment:keyring'?: {
-    allowedOrigins?: string[];
+  'endowment:keyring': {
+    allowedOrigins: string[];
   };
   'endowment:lifecycle-hooks': EmptyObject;
-  'endowment:name-lookup'?: ChainId[];
-  'endowment:network-access'?: EmptyObject;
+  'endowment:name-lookup': ChainId[];
+  'endowment:network-access': EmptyObject;
   'endowment:page-home': EmptyObject;
-  'endowment:rpc'?: {
-    dapps?: boolean;
-    snaps?: boolean;
-    allowedOrigins?: string[];
+  'endowment:rpc': {
+    dapps: boolean;
+    snaps: boolean;
+    allowedOrigins: string[];
   };
-  'endowment:signature-insight'?: {
-    allowSignatureOrigin?: boolean;
+  'endowment:signature-insight': {
+    allowSignatureOrigin: boolean;
   };
-  'endowment:transaction-insight'?: {
-    allowTransactionOrigin?: boolean;
+  'endowment:transaction-insight': {
+    allowTransactionOrigin: boolean;
   };
-  'endowment:webassembly'?: EmptyObject;
+  'endowment:webassembly': EmptyObject;
 
   /* eslint-disable @typescript-eslint/naming-convention */
-  eth_accounts?: EmptyObject;
-  snap_dialog?: EmptyObject;
-  snap_getBip32Entropy?: Bip32Entropy[];
-  snap_getBip32PublicKey?: Bip32Entropy[];
-  snap_getBip44Entropy?: Bip44Entropy[];
-  snap_getEntropy?: EmptyObject;
-  snap_getLocale?: EmptyObject;
-  snap_manageAccounts?: EmptyObject;
-  snap_manageState?: EmptyObject;
-  snap_notify?: EmptyObject;
-  wallet_snap?: Record<string, RequestedSnap>;
+  eth_accounts: EmptyObject;
+  snap_dialog: EmptyObject;
+  snap_getBip32Entropy: Bip32Entropy[];
+  snap_getBip32PublicKey: Bip32Entropy[];
+  snap_getBip44Entropy: Bip44Entropy[];
+  snap_getEntropy: EmptyObject;
+  snap_getLocale: EmptyObject;
+  snap_manageAccounts: EmptyObject;
+  snap_manageState: EmptyObject;
+  snap_notify: EmptyObject;
+  wallet_snap: Record<string, RequestedSnap>;
   /* eslint-enable @typescript-eslint/naming-convention */
-};
+}>;
