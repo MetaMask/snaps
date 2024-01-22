ecc79069bcabd154bdf82b73b0da2728be6abb7a
Hassan Malik <41640681+hmalik88@users.noreply.github.com>
2024-01-21 20:19:26 -0500

    Add signature insights to `test-snaps` (#2115)
    Added a section for the signature insights example snap. 
    
    **Note:** This is merely for installation purposes when testing, but the
    e2e tests for signature insights in the extension will utilize the
    test-dapp.

---
+++ packages/test-snaps/src/features/snaps/signature-insights/constants.ts
@@ -0,0 +1,6 @@
+import packageJson from '@metamask/signature-insights-example-snap/package.json';
+
+export const SIGNATURE_INSIGHTS_SNAP_ID =
+  'npm:@metamask/signature-insights-example-snap';
+export const SIGNATURE_INSIGHTS_SNAP_PORT = 8017;
+export const SIGNATURE_INSIGHTS_VERSION = packageJson.version;
