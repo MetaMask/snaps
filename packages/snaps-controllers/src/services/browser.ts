7dadc0e87c3e1613037bdb864466fd072fe033ab
JSoufer <jonathan.ferreira@consensys.net>
2024-01-22 15:58:58 +0000

    fix: adding missing export

--- packages/snaps-controllers/src/services/browser.ts
+++ packages/snaps-controllers/src/services/browser.ts
@@ -4,4 +4,5 @@ export * from './ExecutionService';
 export * from './ProxyPostMessageStream';
 export * from './iframe';
 export * from './offscreen';
+export * from './webview';
 export { WebWorkerExecutionService } from './webworker';
