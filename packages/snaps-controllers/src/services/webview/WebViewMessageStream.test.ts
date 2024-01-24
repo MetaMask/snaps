5342fa6da6e00c5464677d908800a7dfd10a1f2f
JSoufer <jonathan.ferreira@consensys.net>
2024-01-24 14:29:23 +0000

    chore: add tests for WebViewMessageStream

---
+++ packages/snaps-controllers/src/services/webview/WebViewMessageStream.test.ts
@@ -0,0 +1,73 @@
+import { sleep } from '@metamask/snaps-utils/test-utils';
+
+import { WebViewMessageStream } from './WebViewMessageStream';
+
+describe('WebViewMessageStream', () => {
+  let mockWebView: any;
+  let getWebView: any;
+  let stream: WebViewMessageStream;
+
+  beforeEach(() => {
+    mockWebView = {
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      injectJavaScript: jest.fn(),
+    };
+    getWebView = jest.fn().mockResolvedValue(mockWebView);
+    stream = new WebViewMessageStream({
+      name: 'testStream',
+      target: 'targetStream',
+      getWebView,
+    });
+  });
+
+  it('initializes correctly', async () => {
+    expect(getWebView).toHaveBeenCalled();
+    await sleep(1); // wait for getWebView promise to resolve
+    expect(mockWebView.addEventListener).toHaveBeenCalledWith(
+      'message',
+      expect.any(Function),
+    );
+  });
+
+  it('handles _postMessage correctly', () => {
+    stream._postMessage({ foo: 'bar' });
+    expect(mockWebView.injectJavaScript).toHaveBeenCalledWith(
+      // We expect XSS encode approach to be used here encoding foo: 'bar' to value below
+      `window.postMessage('eyJ0YXJnZXQiOiJ0YXJnZXRTdHJlYW0iLCJkYXRhIjoiU1lOIn0=')`,
+    );
+  });
+
+  it('handles _onMessage correctly', () => {
+    const mockedOnData = jest.fn();
+    stream._onData = mockedOnData;
+
+    const messageEvent = {
+      data: JSON.stringify({
+        target: 'testStream',
+        data: { foo: 'bar' },
+      }),
+    };
+    stream._onMessage(messageEvent);
+    expect(mockedOnData).toHaveBeenCalledWith({ foo: 'bar' });
+  });
+
+  it('ignores _onMessage with wrong target', () => {
+    const messageEvent = {
+      data: JSON.stringify({
+        target: 'wrongTarget',
+        data: { foo: 'bar' },
+      }),
+    };
+    stream._onMessage(messageEvent);
+    expect(stream.read()).toBeNull();
+  });
+
+  it('handles _destroy correctly', () => {
+    stream._destroy();
+    expect(mockWebView.removeEventListener).toHaveBeenCalledWith(
+      'message',
+      expect.any(Function),
+    );
+  });
+});
