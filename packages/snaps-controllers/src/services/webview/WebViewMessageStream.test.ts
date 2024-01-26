ecfbcd02e2ff9e464a919f70eb019e59f5ebfdfd
JSoufer <jonathan.ferreira@consensys.net>
2024-01-26 12:59:29 +0000

    chore: makes test run in isolation

--- packages/snaps-controllers/src/services/webview/WebViewMessageStream.test.ts
+++ packages/snaps-controllers/src/services/webview/WebViewMessageStream.test.ts
@@ -1,22 +1,12 @@
 import { sleep } from '@metamask/snaps-utils/test-utils';
 
 import { createWebViewObjects } from '../../test-utils/webview';
-import type { WebViewMessageStream } from './WebViewMessageStream';
 
 describe('WebViewMessageStream', () => {
-  let mockWebView: any;
-  let getWebView: any;
-  let stream: WebViewMessageStream;
-
   it('initializes correctly', async () => {
-    const testObjects = createWebViewObjects();
-    mockWebView = testObjects.mockWebView;
-    getWebView = testObjects.mockGetWebView;
+    const { mockWebView, mockGetWebView } = createWebViewObjects();
 
-    // Initialize stream. Different from other post-message streams, we have to wait for the WebView to fully load before we can continue using the stream.
-    stream = testObjects.mockStream;
-
-    expect(getWebView).toHaveBeenCalled();
+    expect(mockGetWebView).toHaveBeenCalled();
     await sleep(1); // wait for getWebView promise to resolve
     expect(mockWebView.addEventListener).toHaveBeenCalledWith(
       'message',
@@ -24,45 +14,55 @@ describe('WebViewMessageStream', () => {
     );
   });
 
-  it('handles _postMessage(write) correctly', () => {
-    const message = { foo: 'bar' };
-    stream.write(message);
+  it('handles _postMessage(write) correctly', async () => {
+    const { mockWebView, mockStream } = createWebViewObjects();
 
+    const message = { foo: 'bar' };
+    await sleep(1);
+
+    mockStream.write(message);
     expect(mockWebView.injectJavaScript).toHaveBeenCalledWith(
       `window.postMessage('eyJ0YXJnZXQiOiJ0YXJnZXRTdHJlYW0iLCJkYXRhIjoiU1lOIn0=')`,
     );
 
-    stream.destroy();
+    mockStream.destroy();
   });
 
-  it('calls _onMessage when a message event is emitted', () => {
+  it('calls _onMessage when a message event is emitted', async () => {
+    const { mockWebView, mockStream } = createWebViewObjects();
+
     const mockMessageEvent = {
       data: JSON.stringify({
         target: 'test',
         data: 'Test message',
       }),
     };
-
-    const onMessageSpy = jest.spyOn(stream, '_onMessage' as any);
+    await sleep(1);
+    const onMessageSpy = jest.spyOn(mockStream, '_onMessage' as any);
     const listener = mockWebView.addEventListener.mock.calls[0][1];
     listener(mockMessageEvent);
 
     expect(onMessageSpy).toHaveBeenCalled();
   });
 
-  it('ignores _onMessage with wrong target', () => {
+  it('ignores _onMessage with wrong target', async () => {
+    const { mockStream } = createWebViewObjects();
     const messageEvent = {
       data: {
         target: 'wrongTarget',
         data: { foo: 'bar' },
       },
     };
-    stream.read(messageEvent as any);
-    expect(stream.read()).toBeNull();
+
+    await sleep(1);
+    mockStream.read(messageEvent as any);
+    expect(mockStream.read()).toBeNull();
   });
 
-  it('handles destroy correctly', () => {
-    stream.destroy();
+  it('handles destroy correctly', async () => {
+    const { mockWebView, mockStream } = createWebViewObjects();
+    await sleep(1);
+    mockStream.destroy();
     expect(mockWebView.removeEventListener).toHaveBeenCalledWith(
       'message',
       expect.any(Function),
