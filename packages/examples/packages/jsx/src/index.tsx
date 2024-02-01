553d0fd8ea894ca47fe268a6c75b4fae7b35fec8
Olaf Tomalka <olaf@tomalka.me>
2024-02-01 13:26:37 +0100

    Got JSX import working in TypeScript in JSX example

--- packages/examples/packages/jsx/src/index.tsx
+++ packages/examples/packages/jsx/src/index.tsx
@@ -3,10 +3,12 @@ import { Panel, Heading, Text } from '@metamask/snaps-sdk/jsx-runtime';
 export const onHomePage = async () => {
   return {
     content: (
-      <Panel>
-        <Heading>Hello world!</Heading>
-        <Text>Welcome to my Snap home page!</Text>
-      </Panel>
+      <>
+        <Panel>
+          <Heading>Hello world!</Heading>
+          <Text>Welcome to my Snap home page!</Text>
+        </Panel>
+      </>
     ),
   };
 };
