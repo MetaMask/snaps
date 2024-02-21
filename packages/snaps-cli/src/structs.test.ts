1752a78b340371da0667012144517e41077e392a
Frederik Bolding <frederik.bolding@gmail.com>
2024-02-21 16:52:13 +0100

    Move mock

--- packages/snaps-cli/src/structs.test.ts
+++ packages/snaps-cli/src/structs.test.ts
@@ -3,11 +3,12 @@ import { create, is } from 'superstruct';
 
 import { file } from './structs';
 
+// Mock resolve so these tests work on Windows
+jest.mock('path', () => ({ resolve }));
+
 describe('file', () => {
   it('resolves a file path relative to the current working directory', () => {
     jest.spyOn(process, 'cwd').mockReturnValue('/foo/bar');
-    // Mock resolve so these tests work on Windows
-    jest.mock('path', () => ({ resolve }));
 
     expect(is('packages/snaps-utils/src/structs.test.ts', file())).toBe(true);
     expect(create('packages/snaps-utils/src/structs.test.ts', file())).toBe(
