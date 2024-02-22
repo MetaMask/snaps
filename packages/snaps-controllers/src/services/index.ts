b5426b8a66ae848760196693b69d8a4488bfac7b
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2024-02-22 11:07:42 +0100

    Add /node and /react-native exports to snaps-controllers

--- packages/snaps-controllers/src/services/index.ts
+++ packages/snaps-controllers/src/services/index.ts
@@ -2,7 +2,5 @@ export * from './AbstractExecutionService';
 export * from './ExecutionService';
 export * from './ProxyPostMessageStream';
 export * from './iframe';
-export * from './node';
 export * from './offscreen';
-export * from './webview';
 export { WebWorkerExecutionService } from './webworker';
