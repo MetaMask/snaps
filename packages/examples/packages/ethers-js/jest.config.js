8f9d87a57baae1dc0c593dbf2dd526406027c63a
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2024-02-22 14:19:32 +0100

    Update example Jest configs

--- packages/examples/packages/ethers-js/jest.config.js
+++ packages/examples/packages/ethers-js/jest.config.js
@@ -4,16 +4,6 @@ const baseConfig = require('../../../../jest.config.base');
 
 module.exports = deepmerge(baseConfig, {
   preset: '@metamask/snaps-jest',
-  testEnvironmentOptions: {
-    server: {
-      // For this particular example we have to configure a port, as the snap
-      // uses `snap_getEntropy`, which uses the snap ID
-      // (`local:http://localhost:PORT`) to derive entropy. If we were to use a
-      // random port every time, the derived entropy would be different for
-      // every test run, making our tests non-deterministic.
-      port: 48934,
-    },
-  },
 
   // Since `@metamask/snaps-jest` runs in the browser, we can't collect
   // coverage information.
@@ -22,6 +12,11 @@ module.exports = deepmerge(baseConfig, {
   // This is required for the tests to run inside the `MetaMask/snaps`
   // repository. You don't need this in your own project.
   moduleNameMapper: {
+    '^@metamask/(.+)/node$': [
+      '<rootDir>/../../../$1/src/node',
+      '<rootDir>/../../../../node_modules/@metamask/$1/node',
+      '<rootDir>/node_modules/@metamask/$1/node',
+    ],
     '^@metamask/(.+)$': [
       '<rootDir>/../../../$1/src',
       '<rootDir>/../../../../node_modules/@metamask/$1',
