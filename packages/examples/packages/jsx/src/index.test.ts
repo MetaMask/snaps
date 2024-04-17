1278e1326ef52b88962a3c1f77a784966da7a8dc
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2024-04-17 13:04:06 +0200

    Add test to JSX Snap

---
+++ packages/examples/packages/jsx/src/index.test.ts
@@ -0,0 +1,24 @@
+import { expect } from '@jest/globals';
+import { installSnap } from '@metamask/snaps-jest';
+
+describe('onRpcRequest', () => {
+  // TODO: Test JSX functionality.
+
+  it('throws an error if the requested method does not exist', async () => {
+    const { request } = await installSnap();
+
+    const response = await request({
+      method: 'foo',
+    });
+
+    expect(response).toRespondWithError({
+      code: -32601,
+      message: 'The method does not exist / is not available.',
+      stack: expect.any(String),
+      data: {
+        method: 'foo',
+        cause: null,
+      },
+    });
+  });
+});
