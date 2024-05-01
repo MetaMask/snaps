30c88f85bcc3c7987609fe1061756151e2a04ce9
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2024-05-01 21:11:46 +0200

    Forward keys to JSX component (#2379)
    I assumed keys were part of the props, but it's actually a separate
    parameter for the `jsx` function. I've updated the runtime to properly
    pass keys to components.

--- packages/snaps-sdk/src/jsx/jsx-runtime.test.tsx
+++ packages/snaps-sdk/src/jsx/jsx-runtime.test.tsx
@@ -11,6 +11,16 @@ describe('jsx', () => {
     });
   });
 
+  it('renders a JSX component with a key', () => {
+    const element = <Text key="foo">Hello</Text>;
+
+    expect(element).toStrictEqual({
+      type: 'Text',
+      key: 'foo',
+      props: { children: 'Hello' },
+    });
+  });
+
   it('does not validate the element', () => {
     // @ts-expect-error - Invalid props.
     expect(() => <Text foo="bar" />).not.toThrow();
@@ -60,6 +70,33 @@ describe('jsxs', () => {
             children: [
               'Hello, ',
               { type: 'Bold', key: null, props: { children: 'world' } },
+            ],
+          },
+        },
+      },
+    });
+  });
+
+  it('renders a nested JSX component with a key', () => {
+    const element = (
+      <Box>
+        <Text key="foo">
+          Hello, <Bold key="bar">world</Bold>
+        </Text>
+      </Box>
+    );
+
+    expect(element).toStrictEqual({
+      type: 'Box',
+      key: null,
+      props: {
+        children: {
+          type: 'Text',
+          key: 'foo',
+          props: {
+            children: [
+              'Hello, ',
+              { type: 'Bold', key: 'bar', props: { children: 'world' } },
             ],
           },
         },
