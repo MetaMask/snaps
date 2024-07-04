8a4c3d256bef5c8fd8eb70671eeb7c4342c010cc
Mrtenz <Mrtenz@users.noreply.github.com>
2024-07-04 17:49:10 +0000

    deploy: d9dd2accc69973fc3b885d9e4ff5b5b7067271db

---
+++ storybook/pr/2531/assets/Value.stories-98d45c22.js
@@ -0,0 +1,8 @@
+import{j as i}from"./component-2bcf5de3.js";import{V as n}from"./Value-4e6178bf.js";const m={title:"UI/Value",component:n,argTypes:{value:{description:"The value to display.",table:{type:{summary:"string"}}},extra:{description:"The extra meta data to display.",table:{type:{summary:"string"}}}}},e={render:p=>i(n,{...p}),args:{value:"$1200.00",extra:"0.12 ETH"}};var a,t,r,s,o;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render: props => <Value {...props} />,
+  args: {
+    value: '$1200.00',
+    extra: '0.12 ETH'
+  }
+}`,...(r=(t=e.parameters)==null?void 0:t.docs)==null?void 0:r.source},description:{story:`The value component renders a value with extra meta data. It can be used to
+display some data with a description, like an ETH value with a USD value.`,...(o=(s=e.parameters)==null?void 0:s.docs)==null?void 0:o.description}}};const u=["Default"];export{e as Default,u as __namedExportsOrder,m as default};
