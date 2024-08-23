ece79121d35b5c96d00b51c1c7c8c5f3df95e7f5
Olaf Tomalka <olaf@tomalka.me>
2024-08-23 16:31:19 +0200

    Hide caniuse warning in the CLI

--- packages/snaps-cli/src/main.ts
+++ packages/snaps-cli/src/main.ts
@@ -5,6 +5,17 @@ import { logError } from '@metamask/snaps-utils';
 import { cli } from './cli';
 import commands from './commands';
 
+/*
+ * Disable the following error, we target an old version
+ * of a browser, and this error is not useful to our users.
+ *
+ * Browserslist: caniuse-lite is outdated. Please run:
+ *  npx update-browserslist-db@latest
+ *  Why you should do it regularly: https://github.com/browserslist/update-db#readme
+ */
+// eslint-disable-next-line n/no-process-env
+process.env.BROWSERSLIST_IGNORE_OLD_DATA = '1';
+
 cli(process.argv, commands).catch((error) => {
   logError(error);
   process.exitCode = 1;
