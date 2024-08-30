c0137cb3bd5de4a1333c5f5b06e8daa79d3391f6
Frederik Bolding <frederik.bolding@gmail.com>
2024-08-30 12:20:22 +0200

    feat: Allow CAIP-10 addresses

--- packages/snaps-sdk/src/jsx/components/Address.ts
+++ packages/snaps-sdk/src/jsx/components/Address.ts
@@ -1,3 +1,5 @@
+import type { CaipAccountId } from '@metamask/utils';
+
 import { createSnapComponent } from '../component';
 
 /**
@@ -7,22 +9,26 @@ import { createSnapComponent } from '../component';
  * valid Ethereum address, starting with `0x`.
  */
 export type AddressProps = {
-  address: `0x${string}`;
+  address: `0x${string}` | CaipAccountId;
 };
 
 const TYPE = 'Address';
 
 /**
- * An address component, which is used to display an Ethereum address.
+ * An address component, which is used to display a CAIP-10 address or a Ethereum address.
  *
  * This component does not accept any children.
  *
  * @param props - The props of the component.
- * @param props.address - The (Ethereum) address to display. This should be a
- * valid Ethereum address, starting with `0x`.
+ * @param props.address - The address to display. This should be a
+ * valid Ethereum address, starting with `0x` or a valid CAIP-10 address.
  * @returns An address element.
  * @example
  * <Address address="0x1234567890123456789012345678901234567890" />
+ * @example
+ * <Address address="eip155:1:0x1234567890123456789012345678901234567890" />
+ * @example
+ * <Address address="bip122:000000000019d6689c085ae165831e93:128Lkh3S7CkDTBZ8W7BbpsN3YYizJMp8p6" />
  */
 export const Address = createSnapComponent<AddressProps, typeof TYPE>(TYPE);
 
