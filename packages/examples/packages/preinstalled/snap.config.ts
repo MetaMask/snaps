14cebe8e22a50f1c014f518f5443a04d0eafe3fb
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2024-09-12 19:23:43 +0200

    Add preinstalled example Snap

---
+++ packages/examples/packages/preinstalled/snap.config.ts
@@ -0,0 +1,13 @@
+import type { SnapConfig } from '@metamask/snaps-cli';
+
+const config: SnapConfig = {
+  input: './src/index.tsx',
+  server: {
+    port: 8031,
+  },
+  stats: {
+    buffer: false,
+  },
+};
+
+export default config;
