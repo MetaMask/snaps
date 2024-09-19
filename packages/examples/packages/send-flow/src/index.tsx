8e2b6840c5679bf7f8b34d06d455ed8330df7f5a
Guillaume Roux <guillaumeroux123@gmail.com>
2024-09-19 16:39:21 +0200

    fix CI and refactor

--- packages/examples/packages/send-flow/src/index.tsx
+++ packages/examples/packages/send-flow/src/index.tsx
@@ -9,33 +9,9 @@ import {
 } from '@metamask/snaps-sdk';
 
 import { SendFlow } from './components';
-import jazzicon1 from './images/jazzicon1.svg';
-import jazzicon2 from './images/jazzicon2.svg';
-import type { Account, SendFormState, SendFlowContext } from './types';
+import { accountsArray, accounts } from './data';
+import type { SendFormState, SendFlowContext } from './types';
 import { formValidation, generateSendFlow } from './utils';
-
-/**
- * Example accounts data.
- */
-const accounts: Record<string, Account> = {
-  bc1qxy2kgdygjrsqtzq2n0yrf2493p83kkfjhx0wlh: {
-    name: 'My Bitcoin Account',
-    address: 'bc1qxy2kgdygjrsqtzq2n0yrf2493p83kkfjhx0wlh',
-    balance: { amount: 1.8, fiat: 92000 },
-    icon: jazzicon1,
-  },
-  bc1pmpg8yzpty4xgp497qdydkcqt90zz68n48wzwm757vk8nrlkat99q272xm3: {
-    name: 'Savings Account',
-    address: 'bc1pmpg8yzpty4xgp497qdydkcqt90zz68n48wzwm757vk8nrlkat99q272xm3',
-    balance: { amount: 2.5, fiat: 150000 },
-    icon: jazzicon2,
-  },
-};
-
-/**
- * Example accounts data as an array.
- */
-const accountsArray = Object.values(accounts);
 
 /**
  * Handle incoming JSON-RPC requests from the dapp, sent through the
