abf67d31643c057232775f5568a55b0394f27fb6
Guillaume Roux <guillaumeroux123@gmail.com>
2024-09-25 12:24:21 +0200

    add validation for `href` prop

--- packages/snaps-sdk/src/jsx/components/Address.test.tsx
+++ packages/snaps-sdk/src/jsx/components/Address.test.tsx
@@ -14,4 +14,22 @@ describe('Address', () => {
       },
     });
   });
+
+  it('renders an address with a link', () => {
+    const result = (
+      <Address
+        address="0x1234567890123456789012345678901234567890"
+        href="https://foo.bar"
+      />
+    );
+
+    expect(result).toStrictEqual({
+      type: 'Address',
+      key: null,
+      props: {
+        address: '0x1234567890123456789012345678901234567890',
+        href: 'https://foo.bar',
+      },
+    });
+  });
 });
