5e45b46b81fb729d36b31169a81e550c975eed7c
Guillaume Roux <guillaumeroux123@gmail.com>
2024-09-27 14:00:20 +0200

    add test for invalid symbols

--- packages/snaps-utils/src/currency.test.ts
+++ packages/snaps-utils/src/currency.test.ts
@@ -9,4 +9,15 @@ describe('currency', () => {
     expect(create('usd', CurrencyStruct)).toBe('usd');
     expect(create('USD', CurrencyStruct)).toBe('usd');
   });
+
+  it.each([undefined, 42, {}, [], 'eur'])(
+    'returns a struct that rejects invalid currency symbols',
+    (value) => {
+      const CurrencyStruct = currency('usd');
+
+      expect(() => create(value, CurrencyStruct)).toThrow(
+        /Expected the literal `"usd"`, but received: .*/u,
+      );
+    },
+  );
 });
