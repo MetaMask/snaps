d215014318baced5036cc5ab51f79b16dd47bda0
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2024-10-04 11:25:34 +0200

    Add more tests

--- packages/snaps-webpack-plugin/src/manifest.test.ts
+++ packages/snaps-webpack-plugin/src/manifest.test.ts
@@ -1,16 +1,22 @@
 import { getSnapManifest } from '@metamask/snaps-utils/test-utils';
+import { promises as fs } from 'fs';
 import { resolve } from 'path';
 
 import { writeManifest } from './manifest';
 
+jest.mock('fs', () => ({
+  ...jest.requireActual('fs'),
+  promises: {
+    writeFile: jest.fn(),
+  },
+}));
+
 describe('writeManifest', () => {
   it('formats the manifest with Prettier before writing to disk', async () => {
-    const fn = jest.fn();
     const manifest = JSON.stringify(getSnapManifest());
+    await writeManifest('test.json', manifest);
 
-    await writeManifest('test.json', manifest, fn);
-
-    expect(fn.mock.calls[0][1]).toMatchInlineSnapshot(`
+    expect(jest.mocked(fs.writeFile).mock.calls[0][1]).toMatchInlineSnapshot(`
       "{
         "version": "1.0.0",
         "description": "The test example snap!",
@@ -41,16 +47,13 @@ describe('writeManifest', () => {
   });
 
   it('uses a custom Prettier config if found', async () => {
-    const fn = jest.fn();
     const manifest = JSON.stringify(getSnapManifest());
-
     await writeManifest(
       resolve(__dirname, '__fixtures__', 'foo.json'),
       manifest,
-      fn,
     );
 
-    expect(fn.mock.calls[0][1]).toMatchInlineSnapshot(`
+    expect(jest.mocked(fs.writeFile).mock.calls[0][1]).toMatchInlineSnapshot(`
       "{
           "version": "1.0.0",
           "description": "The test example snap!",
@@ -75,6 +78,42 @@ describe('writeManifest', () => {
               "endowment:rpc": { "snaps": true, "dapps": false }
           },
           "manifestVersion": "0.1"
+      }
+      "
+    `);
+  });
+
+  it('accepts a custom write function', async () => {
+    const fn = jest.fn();
+    const manifest = JSON.stringify(getSnapManifest());
+    await writeManifest('test.json', manifest, fn);
+
+    expect(fn).toHaveBeenCalledTimes(1);
+    expect(fn.mock.calls[0][1]).toMatchInlineSnapshot(`
+      "{
+        "version": "1.0.0",
+        "description": "The test example snap!",
+        "proposedName": "@metamask/example-snap",
+        "repository": {
+          "type": "git",
+          "url": "https://github.com/MetaMask/example-snap.git"
+        },
+        "source": {
+          "shasum": "rNyfINgNh161cBmUop+F7xlE+GSEDZH53Y/HDpGLGGg=",
+          "location": {
+            "npm": {
+              "filePath": "dist/bundle.js",
+              "packageName": "@metamask/example-snap",
+              "registry": "https://registry.npmjs.org",
+              "iconPath": "images/icon.svg"
+            }
+          }
+        },
+        "initialPermissions": {
+          "snap_dialog": {},
+          "endowment:rpc": { "snaps": true, "dapps": false }
+        },
+        "manifestVersion": "0.1"
       }
       "
     `);
