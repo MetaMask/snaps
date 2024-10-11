ee1f1598630dbac6a99df009996b47abb2b9bb04
Frederik Bolding <frederik.bolding@gmail.com>
2024-10-11 15:38:46 +0200

    Add basic pairing flow

--- packages/snaps-controllers/src/devices/DeviceController.ts
+++ packages/snaps-controllers/src/devices/DeviceController.ts
@@ -4,23 +4,17 @@ import type {
   ControllerStateChangeEvent,
 } from '@metamask/base-controller';
 import { BaseController } from '@metamask/base-controller';
-import type { GetPermissions } from '@metamask/permission-controller';
 
-import type { DeleteInterface } from '../interface';
-import type { GetAllSnaps, HandleSnapRequest } from '../snaps';
 import type {
   TransactionControllerUnapprovedTransactionAddedEvent,
   SignatureStateChange,
   TransactionControllerTransactionStatusUpdatedEvent,
 } from '../types';
+import { createDeferredPromise } from '@metamask/utils';
 
 const controllerName = 'DeviceController';
 
-export type DeviceControllerAllowedActions =
-  | HandleSnapRequest
-  | GetAllSnaps
-  | GetPermissions
-  | DeleteInterface;
+export type DeviceControllerAllowedActions = never;
 
 export type DeviceControllerGetStateAction = ControllerGetStateAction<
   typeof controllerName,
@@ -49,10 +43,18 @@ export type DeviceControllerMessenger = RestrictedControllerMessenger<
   DeviceControllerAllowedEvents['type']
 >;
 
-export type Device = {};
+export enum DeviceType {
+  HID,
+  Bluetooth,
+}
+
+export type Device = {
+  type: DeviceType;
+};
 
 export type DeviceControllerState = {
   devices: Record<string, Device>;
+  pairing?: { snapId: string };
 };
 
 export type DeviceControllerArgs = {
@@ -67,24 +69,82 @@ export class DeviceController extends BaseController<
   DeviceControllerState,
   DeviceControllerMessenger
 > {
+  #pairing?: {
+    promise: Promise<unknown>;
+    resolve: (result: unknown) => void;
+    reject: (error: unknown) => void;
+  };
+
   constructor({ messenger, state }: DeviceControllerArgs) {
     super({
       messenger,
       metadata: {
         devices: { persist: true, anonymous: false },
+        pairing: { persist: false, anonymous: false },
       },
       name: controllerName,
       state: { ...state, devices: {} },
     });
   }
 
-  async requestDevices() {
-    const devices = await (navigator as any).hid.requestDevice({ filters: [] });
-    return devices;
+  async requestDevices(snapId: string) {
+    const device = await this.#requestPairing({ snapId });
+
+    console.log('Paired device', device);
+    // TODO: Persist device
+    // TODO: Grant permission to use device
   }
 
-  async getDevices() {
-    const devices = await (navigator as any).hid.getDevices();
-    return devices;
+  async #hasPermission(snapId: string, device: Device) {
+    // TODO: Verify Snap has permission to use device.
+    return true;
+  }
+
+  // Get actually connected devices
+  async #getDevices() {
+    // TODO: Merge multiple device implementations
+    return (navigator as any).hid.getDevices();
+  }
+
+  #isPairing() {
+    return this.#pairing !== undefined;
+  }
+
+  async #requestPairing({ snapId }: { snapId: string }) {
+    if (this.#isPairing()) {
+      // TODO: Potentially await existing pairing flow?
+      throw new Error('A pairing is already underway.');
+    }
+
+    const { promise, resolve, reject } = createDeferredPromise<unknown>();
+
+    this.#pairing = { promise, resolve, reject };
+    this.update((draftState) => {
+      draftState.pairing = { snapId };
+    });
+
+    return promise;
+  }
+
+  resolvePairing(device: unknown) {
+    if (!this.#isPairing()) {
+      return;
+    }
+
+    this.#pairing?.resolve(device);
+    this.update((draftState) => {
+      delete draftState.pairing;
+    });
+  }
+
+  rejectPairing() {
+    if (!this.#isPairing()) {
+      return;
+    }
+
+    this.#pairing?.reject(new Error('Pairing rejected'));
+    this.update((draftState) => {
+      delete draftState.pairing;
+    });
   }
 }
