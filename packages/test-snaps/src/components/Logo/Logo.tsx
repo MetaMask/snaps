bca51f05421b8fc77490fbff418d856a4f483260
Frederik Bolding <frederik.bolding@gmail.com>
2024-10-24 10:49:33 +0200

    Remove unused condition

--- packages/test-snaps/src/components/Logo/Logo.tsx
+++ packages/test-snaps/src/components/Logo/Logo.tsx
@@ -2,11 +2,6 @@ import type { FunctionComponent } from 'react';
 import { Container } from 'react-bootstrap';
 
 export const Logo: FunctionComponent = () => {
-  // Prevent SSR from rendering the logo.
-  if (typeof document === 'undefined') {
-    return <Container style={{ height: '150px' }} />;
-  }
-
   return (
     <Container
       style={{
