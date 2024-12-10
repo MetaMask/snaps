a6190796ab1847699ee0ec67910dc1797ca789fd
Frederik Bolding <frederik.bolding@gmail.com>
2024-12-10 11:49:52 +0100

    feat: Add platformVersion to manifest

--- packages/snaps-utils/src/manifest/validators/index.ts
+++ packages/snaps-utils/src/manifest/validators/index.ts
@@ -8,8 +8,7 @@ export * from './is-snap-manifest';
 export * from './manifest-localization';
 export * from './package-json-recommended-fields';
 export * from './package-name-match';
-// TODO: Uncomment the following line after the next release.
-// export * from './platform-version';
+export * from './platform-version';
 export * from './repository-match';
 export * from './version-match';
 export * from './icon-declared';
