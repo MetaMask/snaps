f4efd98616f578023ac794200e45bb095a7d55e7
Hassan Malik <hbmalik88@gmail.com>
2024-12-11 11:53:42 -0500

    update example snap

--- packages/examples/packages/cronjobs/src/index.ts
+++ packages/examples/packages/cronjobs/src/index.ts
@@ -1,4 +1,7 @@
-import type { OnCronjobHandler } from '@metamask/snaps-sdk';
+import type {
+  OnCronjobHandler,
+  OnRpcRequestHandler,
+} from '@metamask/snaps-sdk';
 import { panel, text, heading, MethodNotFoundError } from '@metamask/snaps-sdk';
 
 /**
@@ -27,6 +30,51 @@ export const onCronjob: OnCronjobHandler = async ({ request }) => {
             heading('Cronjob'),
             text('This dialog was triggered by a cronjob.'),
           ]),
+        },
+      });
+    case 'fireNotification':
+      return snap.request({
+        method: 'snap_notify',
+        params: {
+          type: 'inApp',
+          message: 'Hello world!',
+        },
+      });
+    default:
+      // eslint-disable-next-line @typescript-eslint/no-throw-literal
+      throw new MethodNotFoundError({ method: request.method });
+  }
+};
+
+/**
+ * Handle incoming JSON-RPC requests from the dapp, sent through the
+ * `wallet_invokeSnap` method. This handler handles two methods:
+ *
+ * - `scheduleNotification`: Schedule a notification in the future.
+ *
+ * @param params - The request parameters.
+ * @param params.request - The JSON-RPC request object.
+ * @returns The JSON-RPC response.
+ * @see https://docs.metamask.io/snaps/reference/exports/#onrpcrequest
+ * @see https://docs.metamask.io/snaps/reference/rpc-api/#wallet_invokesnap
+ */
+export const onRpcRequest: OnRpcRequestHandler = async ({ request }) => {
+  switch (request.method) {
+    case 'scheduleNotification':
+      return snap.request({
+        method: 'snap_scheduleBackgroundEvent',
+        params: {
+          date: new Date().toISOString(),
+          request: {
+            method: 'fireNotification',
+          },
+        },
+      });
+    case 'cancelNotification':
+      return snap.request({
+        method: 'snap_cancelBackgroundEvent',
+        params: {
+          id: request.params?.id,
         },
       });
     default:
