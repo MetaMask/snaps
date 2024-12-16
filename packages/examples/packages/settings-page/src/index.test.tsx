c0e850e2acf9da80c01152c358f07141aa8b58e0
Guillaume Roux <hello@guillaumerx.fr>
2024-12-16 11:21:28 +0100

    add onRpcRequest test

--- packages/examples/packages/settings-page/src/index.test.tsx
+++ packages/examples/packages/settings-page/src/index.test.tsx
@@ -14,3 +14,31 @@ describe('onSettingsPage', () => {
     expect(screen).toRender(<SettingsPage />);
   });
 });
+
+describe('onRpcRequest', () => {
+  describe('getSettings', () => {
+    it('returns the settings state', async () => {
+      const { request, onSettingsPage } = await installSnap();
+
+      const settingPageResponse = await onSettingsPage();
+
+      const screen = settingPageResponse.getInterface();
+
+      await screen.clickElement('setting1');
+
+      await screen.selectFromRadioGroup('setting2', 'option1');
+
+      await screen.selectInDropdown('setting3', 'option2');
+
+      expect(
+        await request({
+          method: 'getSettings',
+        }),
+      ).toRespondWith({
+        setting1: true,
+        setting2: 'option1',
+        setting3: 'option2',
+      });
+    });
+  });
+});
