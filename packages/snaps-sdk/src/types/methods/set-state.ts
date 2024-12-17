b324ad2ff6645d784c71a9841f101c386dd83730
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2024-12-17 12:45:19 +0100

    Update packages/snaps-sdk/src/types/methods/set-state.ts
    Co-authored-by: Frederik Bolding <frederik.bolding@gmail.com>

--- packages/snaps-sdk/src/types/methods/set-state.ts
+++ packages/snaps-sdk/src/types/methods/set-state.ts
@@ -9,8 +9,8 @@ import type { Json } from '@metamask/utils';
  * @property value - The value to set the state to.
  * @property encrypted - Whether to use the separate encrypted state, or the
  * unencrypted state. Defaults to the encrypted state. Encrypted state can only
- * be used if the extension is unlocked, while unencrypted state can be used
- * whether the extension is locked or unlocked.
+ * be used if the client is unlocked, while unencrypted state can be used
+ * whether the client is locked or unlocked.
  */
 export type SetStateParams = {
   key?: string;
