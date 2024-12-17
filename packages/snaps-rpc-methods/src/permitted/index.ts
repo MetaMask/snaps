9144281787f9bf6f7e217989d643da5d87455605
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2024-12-17 11:21:11 +0100

    Add missing hook

--- packages/snaps-rpc-methods/src/permitted/index.ts
+++ packages/snaps-rpc-methods/src/permitted/index.ts
@@ -1,3 +1,4 @@
+import type { ClearStateHooks } from './clearState';
 import type { CreateInterfaceMethodHooks } from './createInterface';
 import type { ProviderRequestMethodHooks } from './experimentalProviderRequest';
 import type { GetAllSnapsHooks } from './getAllSnaps';
@@ -11,7 +12,8 @@ import type { ResolveInterfaceMethodHooks } from './resolveInterface';
 import type { SetStateHooks } from './setState';
 import type { UpdateInterfaceMethodHooks } from './updateInterface';
 
-export type PermittedRpcMethodHooks = GetAllSnapsHooks &
+export type PermittedRpcMethodHooks = ClearStateHooks &
+  GetAllSnapsHooks &
   GetClientStatusHooks &
   GetSnapsHooks &
   GetStateHooks &
