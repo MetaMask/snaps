ce07fbbe61ad0b83aa0a6bbc69dba373aeecc0ac
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2024-12-18 14:14:10 +0100

    Update coverage

--- packages/snaps-rpc-methods/jest.config.js
+++ packages/snaps-rpc-methods/jest.config.js
@@ -12,7 +12,7 @@ module.exports = deepmerge(baseConfig, {
     global: {
       branches: 92.98,
       functions: 97.46,
-      lines: 97.93,
+      lines: 97.94,
       statements: 97.52,
     },
   },
