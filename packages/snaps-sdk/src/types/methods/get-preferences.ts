9366e90abf2b657a9f418d95d6a8f84f1a0d0b83
Guillaume Roux <hello@guillaumerx.fr>
2025-01-07 12:38:38 +0100

    Add JSDoc to `GetPreferencesResult`

--- packages/snaps-sdk/src/types/methods/get-preferences.ts
+++ packages/snaps-sdk/src/types/methods/get-preferences.ts
@@ -9,6 +9,10 @@ export type GetPreferencesParams = never;
  * The result returned by the `snap_getPreferences` method.
  *
  * It is the user selected preferences from the MetaMask extension.
+ *
+ * @property locale - The user's selected locale.
+ * @property currency - The user's selected currency.
+ * @property hideBalances - Whether the user has chosen to hide balances.
  */
 export type GetPreferencesResult = {
   locale: string;
