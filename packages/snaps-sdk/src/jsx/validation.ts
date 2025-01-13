6069bb04b504775c4100edaf6dadd4f435584b22
David Drazic <david@timechaser.org>
2025-01-13 15:05:20 +0100

    Add backgroundColor property to Container component (#2950)
    This PR adds `backgroundColor` property to `Container` component.
    
    Related task: https://github.com/MetaMask/snaps/issues/2906
    Related extension PR:
    https://github.com/MetaMask/metamask-extension/pull/29095
    
    ### Notes
    - Background colors that can be used are predefined as: `default` and
    `alternative`.
    - Background colors are meant to mirror `backgroundDefault` and
    `backgroundAlternative` colors from MetaMask extension design system
    (https://github.com/MetaMask/metamask-extension/blob/main/ui/helpers/constants/design-system.ts#L54).
    - Color names are simplified on the Snaps side to make it easier for
    developers to use.
    
    ### Examples (experiments with extension integration)
    
    Confirmation example:
    ![Screenshot 2024-12-11 at 13 14
    02](https://github.com/user-attachments/assets/f2b77202-d9c4-403e-87a1-ad36d44299c9)
    
    Source code used for content:
    ```typescript
          return snap.request({
            method: 'snap_dialog',
            params: {
              content: (
                <Container backgroundColor="default">
                  <Box>
                    <Text>Testing container background.</Text>
                    <Button variant="primary">Primary button</Button>
                    <Button variant="destructive">Destructive button</Button>
                    <Button disabled={true}>Disabled button</Button>
                  </Box>
                  <Footer>
                    <Button>Accept</Button>
                    <Button name="footer_button">Cancel</Button>
                  </Footer>
                </Container>
              ),
            },
          });
    ```
    
    Transaction insight example where background color is deliberately
    ignored:
    ![Screenshot 2024-12-11 at 13 07
    40](https://github.com/user-attachments/assets/b7b3a593-8407-4f92-a629-edd85c8f88dc)
    Source code used for content:
    ```typescript
        return {
          content: (
            <Container backgroundColor="alternative">
              <Box>
                <Text>Testing container background on transaction insight.</Text>
                <Text>Normal transaction.</Text>
              </Box>
            </Container>
          ),
          severity: SeverityLevel.Critical,
        };
    ```

--- packages/snaps-sdk/src/jsx/validation.ts
+++ packages/snaps-sdk/src/jsx/validation.ts
@@ -877,6 +877,9 @@ export const ContainerStruct: Describe<ContainerElement> = element(
       [GenericSnapElement, FooterElement] | GenericSnapElement,
       null
     >,
+    backgroundColor: optional(
+      nullUnion([literal('default'), literal('alternative')]),
+    ),
   },
 );
 
