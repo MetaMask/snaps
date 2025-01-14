be470b8149f5bbde64b5943e4daf8ada3c9f26ba
Frederik Bolding <frederik.bolding@gmail.com>
2025-01-14 19:10:39 +0100

    feat: Mock chain ID and network version calls in simulation

---
+++ packages/snaps-simulation/src/middleware/internal-methods/net-version.test.ts
@@ -0,0 +1,28 @@
+import type { Json, PendingJsonRpcResponse } from '@metamask/utils';
+
+import { getNetworkVersionHandler } from './net-version';
+
+describe('getNetworkVersionHandler', () => {
+  it('returns the network version', async () => {
+    const end = jest.fn();
+    const result: PendingJsonRpcResponse<Json> = {
+      jsonrpc: '2.0' as const,
+      id: 1,
+    };
+
+    await getNetworkVersionHandler(
+      {
+        jsonrpc: '2.0',
+        id: 1,
+        method: 'net_version',
+        params: [],
+      },
+      result,
+      jest.fn(),
+      end,
+    );
+
+    expect(end).toHaveBeenCalled();
+    expect(result.result).toBe('0x01');
+  });
+});
