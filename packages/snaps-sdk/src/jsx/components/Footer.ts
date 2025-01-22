fa7b15e8a8042dcca142aa4bcc5163f250985eb4
Hassan Malik <hbmalik88@gmail.com>
2025-01-22 16:42:40 -0500

    add requireCompleteView prop

--- packages/snaps-sdk/src/jsx/components/Footer.ts
+++ packages/snaps-sdk/src/jsx/components/Footer.ts
@@ -5,9 +5,11 @@ import type { ButtonElement } from './form';
  * The props of the {@link Footer} component.
  *
  * @property children - The single or multiple buttons in the footer.
+ * @property requireCompleteView - Controls whether footer buttons require all content to be viewed before becoming active.
  */
 export type FooterProps = {
   children: ButtonElement | [ButtonElement, ButtonElement];
+  requireCompleteView?: boolean;
 };
 
 const TYPE = 'Footer';
@@ -15,13 +17,20 @@ const TYPE = 'Footer';
 /**
  * A footer component, which is used to create a footer with buttons.
  *
+ * Note: The `requireCompleteView` prop also activates an arrow that allows you to scroll to bottom.
+ *
  * @param props - The props of the component.
+ * @param props.requireCompleteView - Controls whether footer buttons require all content to be viewed before becoming active.
  * @param props.children - The single or multiple buttons in the footer.
  * @returns A footer element.
  * @example
  * <Footer>
  *   <Button name="cancel">Cancel</Button>
  *   <Button name="confirm">Confirm</Button>
+ * </Footer>
+ * @example
+ * <Footer requireCompleteView>
+ *   <Button name="accept">Accept</Button>
  * </Footer>
  */
 export const Footer = createSnapComponent<FooterProps, typeof TYPE>(TYPE);
