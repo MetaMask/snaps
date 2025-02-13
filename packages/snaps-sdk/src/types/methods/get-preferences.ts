00f2d4b2a8b656e1359e35eddf23214f9bf85f67
Hassan Malik <hbmalik88@gmail.com>
2025-02-13 05:15:48 -0500

    update jsdoc

--- packages/snaps-sdk/src/types/methods/get-preferences.ts
+++ packages/snaps-sdk/src/types/methods/get-preferences.ts
@@ -19,7 +19,7 @@ export type GetPreferencesParams = never;
  * @property batchCheckBalances - Whether to fetch balances in an aggregated manner.
  * @property displayNftMedia - Whether to display NFT media.
  * @property useNftDetection - Whether to auto-detect NFTs.
- * @property usePriceCheck - Whether to get token price data from CoinGecko and CryptoCompare.
+ * @property usePriceCheck - Whether to get token price data from an external source.
  */
 export type GetPreferencesResult = {
   locale: string;
