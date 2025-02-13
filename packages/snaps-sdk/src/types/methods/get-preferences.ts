efd7279af861b969d92f73556ff43de197582646
Hassan Malik <hbmalik88@gmail.com>
2025-02-13 04:35:48 -0500

    update types

--- packages/snaps-sdk/src/types/methods/get-preferences.ts
+++ packages/snaps-sdk/src/types/methods/get-preferences.ts
@@ -13,9 +13,23 @@ export type GetPreferencesParams = never;
  * @property locale - The user's selected locale.
  * @property currency - The user's selected currency.
  * @property hideBalances - Whether the user has chosen to hide balances.
+ * @property useSecurityAlerts - Whether to run transactions and signatures through blockaid.
+ * @property useSimulations - Whether to simulate transactions and signatures.
+ * @property useTokenDetection - Whether to auto-detect tokens.
+ * @property batchCheckBalances - Whether to fetch balances in an aggregated manner.
+ * @property displayNftMedia - Whether to display NFT media.
+ * @property useNftDetection - Whether to auto-detect NFTs.
+ * @property usePriceCheck - Whether to get token price data from CoinGecko and CryptoCompare.
  */
 export type GetPreferencesResult = {
   locale: string;
   currency: string;
   hideBalances: boolean;
+  useSecurityAlerts: boolean;
+  useSimulations: boolean;
+  useTokenDetection: boolean;
+  batchCheckBalances: boolean;
+  displayNftMedia: boolean;
+  useNftDetection: boolean;
+  usePriceCheck: boolean;
 };
