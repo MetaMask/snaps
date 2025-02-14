054106eb089bf8fddcf7695b74fc3a379417bbd7
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2025-02-14 10:58:34 +0100

    Fix Linux support for `update-chrome` script

--- scripts/update-chrome.sh
+++ scripts/update-chrome.sh
@@ -4,10 +4,6 @@ set -e
 set -u
 set -o pipefail
 
-#!/bin/bash
-
-yarn add -D chromedriver
-
 CHROME_VERSION=$(curl -s https://dl.google.com/linux/chrome/deb/dists/stable/main/binary-amd64/Packages.gz | \
     gunzip -c | \
     grep -A 1 "Package: google-chrome-stable" | grep "Version:" | awk '{print $2}')
@@ -24,7 +20,7 @@ SCRIPT_PATH="./scripts/install-chrome.sh"
 wget -O "${CHROME_BINARY}" -t 5 "${CHROME_BINARY_URL}"
 CHROME_BINARY_SHA512SUM=$(shasum -a 512 "${CHROME_BINARY}" | awk '{print $1}')
 
-sed -i '' "s/^CHROME_VERSION='.*'/CHROME_VERSION='${CHROME_VERSION}'/" "${SCRIPT_PATH}"
-sed -i '' "s/^CHROME_BINARY_SHA512SUM='.*'/CHROME_BINARY_SHA512SUM='${CHROME_BINARY_SHA512SUM}'/" "${SCRIPT_PATH}"
+sed -i "s/^CHROME_VERSION='.*'/CHROME_VERSION='${CHROME_VERSION}'/" "${SCRIPT_PATH}"
+sed -i "s/^CHROME_BINARY_SHA512SUM='.*'/CHROME_BINARY_SHA512SUM='${CHROME_BINARY_SHA512SUM}'/" "${SCRIPT_PATH}"
 
 rm -rf "${CHROME_BINARY}"
