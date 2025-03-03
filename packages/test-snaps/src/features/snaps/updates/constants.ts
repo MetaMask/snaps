a5b0c6e6ac6299f5d72160bc6f267d7a398eb00f
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2025-03-03 15:56:44 +0100

    Use Webpack plugin example Snap for testing updates

--- packages/test-snaps/src/features/snaps/updates/constants.ts
+++ packages/test-snaps/src/features/snaps/updates/constants.ts
@@ -1,3 +1,3 @@
-export const UPDATES_SNAP_ID = 'npm:@metamask/bip32-example-snap';
-export const UPDATES_SNAP_OLD_VERSION = '0.35.0-flask.1';
-export const UPDATES_SNAP_NEW_VERSION = '0.35.2-flask.1';
+export const UPDATES_SNAP_ID = 'npm:@metamask/webpack-plugin-example-snap';
+export const UPDATES_SNAP_OLD_VERSION = '2.0.0';
+export const UPDATES_SNAP_NEW_VERSION = '2.1.3';
