e442cead5d106ae56697f59cf6a7993f5a6be92e
Guillaume Roux <hello@guillaumerx.fr>
2025-04-01 15:26:24 +0200

    update execution env imports

---
+++ packages/snaps-utils/src/handlers/index.executionenv.ts
@@ -0,0 +1,2 @@
+export { HandlerType, type SnapHandler } from './types';
+export { SNAP_EXPORT_NAMES } from './exports';
--- packages/snaps-utils/src/index.executionenv.ts
+++ packages/snaps-utils/src/index.executionenv.ts
@@ -1,7 +1,6 @@
 // Special entrypoint for execution environments for bundle sizing reasons
 export * from './errors';
-export type { SnapHandler } from './handlers';
-export { HandlerType, SNAP_EXPORT_NAMES } from './handlers';
+export * from './handlers';
 export * from './iframe';
 export * from './logging';
 export * from './types';
