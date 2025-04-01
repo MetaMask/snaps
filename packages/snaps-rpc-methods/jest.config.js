9b558ae2b664d9669df8d318f5a408bde38c4a72
Guillaume Roux <hello@guillaumerx.fr>
2025-04-01 12:04:32 +0200

    update coverage

--- packages/snaps-rpc-methods/jest.config.js
+++ packages/snaps-rpc-methods/jest.config.js
@@ -10,10 +10,10 @@ module.exports = deepmerge(baseConfig, {
   ],
   coverageThreshold: {
     global: {
-      branches: 94.98,
-      functions: 98.64,
-      lines: 98.76,
-      statements: 98.45,
+      branches: 94.95,
+      functions: 98.62,
+      lines: 98.75,
+      statements: 98.43,
     },
   },
 });
