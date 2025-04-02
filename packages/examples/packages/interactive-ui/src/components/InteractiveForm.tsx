8bfaf950730758dd6de9cbf2c6dd01946fca361e
Frederik Bolding <frederik.bolding@gmail.com>
2025-04-02 16:04:13 +0200

    feat: Add disabled state to interactive UI example (#3293)
    Makes a couple of improvements to the interactive UI example Snap:
    - Use custom dialog type with footers instead of regular buttons
    - Add a `disabled` parameter which disables all of the components for
    testing and visual inspection
    - Remove broken "Get interface state" button
    
    
    ![image](https://github.com/user-attachments/assets/91fb1988-55e0-47fc-ab3a-1d1ee3d8270a)

--- packages/examples/packages/interactive-ui/src/components/InteractiveForm.tsx
+++ packages/examples/packages/interactive-ui/src/components/InteractiveForm.tsx
@@ -14,6 +14,8 @@ import {
   Dropdown,
   Option,
   Checkbox,
+  Container,
+  Footer,
 } from '@metamask/snaps-sdk/jsx';
 
 /**
@@ -46,54 +48,72 @@ export type InteractiveFormState = {
   'example-selector': string;
 };
 
-export const InteractiveForm: SnapComponent = () => {
+export const InteractiveForm: SnapComponent<{ disabled?: boolean }> = ({
+  disabled,
+}) => {
   return (
-    <Box>
-      <Heading>Interactive UI Example Snap</Heading>
-      <Form name="example-form">
-        <Field label="Example Input">
-          <Input name="example-input" placeholder="Enter something..." />
-        </Field>
-        <Field label="Example Dropdown">
-          <Dropdown name="example-dropdown">
-            <Option value="option1">Option 1</Option>
-            <Option value="option2">Option 2</Option>
-            <Option value="option3">Option 3</Option>
-          </Dropdown>
-        </Field>
-        <Field label="Example RadioGroup">
-          <RadioGroup name="example-radiogroup">
-            <Radio value="option1">Option 1</Radio>
-            <Radio value="option2">Option 2</Radio>
-            <Radio value="option3">Option 3</Radio>
-          </RadioGroup>
-        </Field>
-        <Field label="Example Checkbox">
-          <Checkbox name="example-checkbox" label="Checkbox" />
-        </Field>
-        <Field label="Example Selector">
-          <Selector
-            name="example-selector"
-            title="Choose an option"
-            value="option1"
-          >
-            <SelectorOption value="option1">
-              <Card title="Option 1" value="option1" />
-            </SelectorOption>
-            <SelectorOption value="option2">
-              <Card title="Option 2" value="option2" />
-            </SelectorOption>
-            <SelectorOption value="option3">
-              <Card title="Option 3" value="option3" />
-            </SelectorOption>
-          </Selector>
-        </Field>
-        <Box center>
-          <Button type="submit" name="submit">
-            Submit
-          </Button>
-        </Box>
-      </Form>
-    </Box>
+    <Container>
+      <Box>
+        <Heading>Interactive UI Example Snap</Heading>
+        <Form name="example-form">
+          <Field label="Example Input">
+            <Input
+              name="example-input"
+              placeholder="Enter something..."
+              disabled={disabled}
+            />
+          </Field>
+          <Field label="Example Dropdown">
+            <Dropdown name="example-dropdown" disabled={disabled}>
+              <Option value="option1">Option 1</Option>
+              <Option value="option2">Option 2</Option>
+              <Option value="option3">Option 3</Option>
+            </Dropdown>
+          </Field>
+          <Field label="Example RadioGroup">
+            <RadioGroup name="example-radiogroup" disabled={disabled}>
+              <Radio value="option1">Option 1</Radio>
+              <Radio value="option2">Option 2</Radio>
+              <Radio value="option3">Option 3</Radio>
+            </RadioGroup>
+          </Field>
+          <Field label="Example Checkbox">
+            <Checkbox
+              name="example-checkbox"
+              label="Checkbox"
+              disabled={disabled}
+            />
+          </Field>
+          <Field label="Example Selector">
+            <Selector
+              name="example-selector"
+              title="Choose an option"
+              value="option1"
+              disabled={disabled}
+            >
+              <SelectorOption value="option1">
+                <Card title="Option 1" value="option1" />
+              </SelectorOption>
+              <SelectorOption value="option2">
+                <Card title="Option 2" value="option2" />
+              </SelectorOption>
+              <SelectorOption value="option3">
+                <Card title="Option 3" value="option3" />
+              </SelectorOption>
+            </Selector>
+          </Field>
+        </Form>
+      </Box>
+      <Footer>
+        <Button
+          type="submit"
+          name="submit"
+          form="example-form"
+          disabled={disabled}
+        >
+          Submit
+        </Button>
+      </Footer>
+    </Container>
   );
 };
