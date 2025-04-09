57f75748317a0a630c756ed9a490d823112af556
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2025-04-09 14:34:14 +0200

    Show current Snap ID

--- packages/snaps-sandbox/src/components/Sandbox.tsx
+++ packages/snaps-sandbox/src/components/Sandbox.tsx
@@ -1,9 +1,10 @@
-import { Box, Container, Grid, Heading, HStack } from '@chakra-ui/react';
+import { Box, Container, Grid, Heading, HStack, Stack } from '@chakra-ui/react';
 import type { FunctionComponent } from 'react';
 
 import { Request } from './Request';
 import { RequestButton } from './RequestButton';
 import { Response } from './Response';
+import { Status } from './Status.js';
 
 /**
  * The main component of the Snaps Sandbox.
@@ -12,35 +13,42 @@ import { Response } from './Response';
  */
 export const Sandbox: FunctionComponent = () => {
   return (
-    <Container width={['100%', null, null, '10/12']}>
-      <Grid
-        templateColumns="1fr 1fr"
-        templateRows="auto 1fr"
-        gap="4"
-        height="100%"
-      >
-        <HStack gap="3">
-          <Heading as="h3" size="lg">
-            Request
-          </Heading>
-          <RequestButton />
-        </HStack>
-
-        <Heading as="h3" size="lg">
-          Response
-        </Heading>
-
-        <Box
-          height="100%"
-          borderRightWidth="1px"
-          borderRightStyle="solid"
-          borderColor="gray.200"
-        >
-          <Request />
+    <Box width="100%" height="100vh" paddingY="6">
+      <Stack height="100%">
+        <Box marginX="6" alignSelf="flex-end">
+          <Status />
         </Box>
+        <Container width={['100%', null, null, '10/12']} flexGrow="1">
+          <Grid
+            templateColumns="1fr 1fr"
+            templateRows="auto 1fr"
+            gap="4"
+            height="100%"
+          >
+            <HStack gap="3">
+              <Heading as="h3" size="lg">
+                Request
+              </Heading>
+              <RequestButton />
+            </HStack>
 
-        <Response />
-      </Grid>
-    </Container>
+            <Heading as="h3" size="lg">
+              Response
+            </Heading>
+
+            <Box
+              height="100%"
+              borderRightWidth="1px"
+              borderRightStyle="solid"
+              borderColor="gray.200"
+            >
+              <Request />
+            </Box>
+
+            <Response />
+          </Grid>
+        </Container>
+      </Stack>
+    </Box>
   );
 };
