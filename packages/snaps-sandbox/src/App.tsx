9e711dfbf418b061218c2b1ac518c07d181f36b5
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2025-04-09 14:34:14 +0200

    Show current Snap ID

--- packages/snaps-sandbox/src/App.tsx
+++ packages/snaps-sandbox/src/App.tsx
@@ -1,4 +1,4 @@
-import { Flex, HStack } from '@chakra-ui/react';
+import { HStack } from '@chakra-ui/react';
 import type { FunctionComponent } from 'react';
 
 import { Sandbox } from './components';
@@ -7,14 +7,6 @@ import { Sidebar } from './features/sidebar';
 export const App: FunctionComponent = () => (
   <HStack gap="0">
     <Sidebar />
-    <Flex
-      justify="center"
-      flexGrow="1"
-      maxWidth="100%"
-      minHeight="100vh"
-      paddingY="24"
-    >
-      <Sandbox />
-    </Flex>
+    <Sandbox />
   </HStack>
 );
