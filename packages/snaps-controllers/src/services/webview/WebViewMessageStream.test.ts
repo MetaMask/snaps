d25f095f2c94c6e0965d5b172e5fc4aea3d307df
Frederik Bolding <frederik.bolding@gmail.com>
2025-04-24 10:07:40 +0200

    Fix tests

--- packages/snaps-controllers/src/services/webview/WebViewMessageStream.test.ts
+++ packages/snaps-controllers/src/services/webview/WebViewMessageStream.test.ts
@@ -22,12 +22,11 @@ describe('WebViewMessageStream', () => {
     expect(await responsePromise).toBe(555);
 
     expect(mockWebViewA.injectJavaScript).toHaveBeenCalledWith(
-      `window.postMessage([123,34,116,97,114,103,101,116,34,58,34,98,34,44,34,100,97,116,97,34,58,34,83,89,78,34,125])`,
+      `window.postMessage("{\\"target\\":\\"b\\",\\"data\\":\\"SYN\\"}")`,
     );
 
-    // Inject { target: "foo", data: 111 }
     mockWebViewA.injectJavaScript(
-      `window.postMessage([123,34,116,97,114,103,101,116,34,58,34,102,111,111,34,44,34,100,97,116,97,34,58,49,49,49,125])`,
+      `window.postMessage("{\\"target\\":\\"foo\\",\\"data\\":111}")`,
     );
 
     const listener = jest.fn();
