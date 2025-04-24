919acb9aa94eb87d40eb6c4d8317f99dd5f08a16
Frederik Bolding <frederik.bolding@gmail.com>
2025-04-24 16:07:47 +0200

    Add export

--- packages/snaps-utils/src/index.executionenv.ts
+++ packages/snaps-utils/src/index.executionenv.ts
@@ -3,5 +3,6 @@ export * from './errors';
 export * from './handlers/exports';
 export * from './handlers/types';
 export * from './iframe';
+export * from './json';
 export * from './logging';
 export * from './types';
