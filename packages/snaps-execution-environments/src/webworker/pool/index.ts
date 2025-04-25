d7f6ff414171b1dd936c73156e72ee92a47f2f99
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2025-04-25 15:16:05 +0200

    Remove web worker executor and environment

--- packages/snaps-execution-environments/src/webworker/pool/index.ts
+++
@@ -1,9 +0,0 @@
-import { WebWorkerPool } from './WebWorkerPool';
-import { executeLockdownEvents } from '../../common/lockdown/lockdown-events';
-import { executeLockdownMore } from '../../common/lockdown/lockdown-more';
-
-// Lockdown is already applied in LavaMoat
-executeLockdownMore();
-executeLockdownEvents();
-
-WebWorkerPool.initialize();
