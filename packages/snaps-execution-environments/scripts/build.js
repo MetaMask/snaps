6180c449b020bbca94dc5f4eed0afa555b7cc03f
Frederik Bolding <frederik.bolding@gmail.com>
2025-05-08 13:07:50 +0200

    build: Handle Webpack compilation errors when building execution environments (#3385)
    Handle Webpack compilation errors when building execution environments
    and set `exitCode` for CI purposes.
    
    ---------
    
    Co-authored-by: Maarten Zuidhoorn <maarten@zuidhoorn.com>

--- packages/snaps-execution-environments/scripts/build.js
+++ packages/snaps-execution-environments/scripts/build.js
@@ -25,7 +25,12 @@ webpack(config, (error, stats) => {
     );
 
     console.log(indent(error.message, 2));
+    process.exitCode = 1;
     return;
+  }
+
+  if (stats.hasErrors()) {
+    process.exitCode = 1;
   }
 
   console.log(
