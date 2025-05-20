58f018c6a0545b4703edf9dcce308ab000b84b76
Frederik Bolding <frederik.bolding@gmail.com>
2025-05-20 15:11:44 +0200

    Fix lint

--- packages/snaps-rpc-methods/src/preinstalled/middleware.ts
+++ packages/snaps-rpc-methods/src/preinstalled/middleware.ts
@@ -1,62 +1,81 @@
 import type { JsonRpcMiddleware } from '@metamask/json-rpc-engine';
-import { PermissionConstraint, RequestedPermissions } from '@metamask/permission-controller';
-import { hasProperty, type Json, type JsonRpcParams } from '@metamask/utils';
-import { SnapEndowments } from '../endowments';
+import type {
+  PermissionConstraint,
+  RequestedPermissions,
+} from '@metamask/permission-controller';
 import { isEqual } from '@metamask/snaps-utils';
+import { hasProperty, type Json, type JsonRpcParams } from '@metamask/utils';
 
-export interface PreinstalledSnapsMiddlewareHooks {
-    getPermittedEvmAccounts: () => string[];
-    getAllEvmAccounts: () => string[];
-    getPermissions:  () => Record<string, PermissionConstraint> | undefined;
-    grantPermissions: (permissions: RequestedPermissions) => void;
-}
+import { SnapEndowments } from '../endowments';
+
+export type PreinstalledSnapsMiddlewareHooks = {
+  getPermittedEvmAccounts: () => string[];
+  getAllEvmAccounts: () => string[];
+  getPermissions: () => Record<string, PermissionConstraint> | undefined;
+  grantPermissions: (permissions: RequestedPermissions) => void;
+};
 
 /**
  * Creates a middleware that automatically grants permissions to preinstalled Snaps
  * that want to use the Ethereum provider endowment.
  *
  * @param hooks - The hooks used by the middleware.
+ * @param hooks.getPermittedEvmAccounts
+ * @param hooks.getAllEvmAccounts
+ * @param hooks.getPermissions
+ * @param hooks.grantPermissions
  * @returns The middleware.
  */
-export function createPreinstalledSnapsMiddleware(
-  { getPermittedEvmAccounts, getAllEvmAccounts, getPermissions, grantPermissions }: PreinstalledSnapsMiddlewareHooks,
-): JsonRpcMiddleware<JsonRpcParams, Json> {
+export function createPreinstalledSnapsMiddleware({
+  getPermittedEvmAccounts,
+  getAllEvmAccounts,
+  getPermissions,
+  grantPermissions,
+}: PreinstalledSnapsMiddlewareHooks): JsonRpcMiddleware<JsonRpcParams, Json> {
   return function methodMiddleware(request, _response, next, _end) {
-    if (!request.method.startsWith("wallet") && !request.method.startsWith("eth")) {
-        return next();
+    if (
+      !request.method.startsWith('wallet') &&
+      !request.method.startsWith('eth')
+    ) {
+      return next();
     }
 
     const permissions = getPermissions();
 
-    if (!permissions || !hasProperty(permissions, SnapEndowments.EthereumProvider)) {
-        return next();
+    if (
+      !permissions ||
+      !hasProperty(permissions, SnapEndowments.EthereumProvider)
+    ) {
+      return next();
     }
 
     const evmAccounts = getAllEvmAccounts();
     const existingEvmAccounts = getPermittedEvmAccounts();
 
     if (isEqual(evmAccounts, existingEvmAccounts)) {
-        return next();
+      return next();
     }
 
     grantPermissions({
-        'endowment:caip25': {
-            caveats: [
-              {
-                type: 'authorizedScopes',
-                value: {
-                  requiredScopes: {},
-                  optionalScopes: {
-                    'wallet:eip155': {
-                      accounts: evmAccounts.map(account => `wallet:eip155:${account}`),
-                    },
-                  },
-                  sessionProperties: {},
-                  isMultichainOrigin: false,
+      'endowment:caip25': {
+        caveats: [
+          {
+            type: 'authorizedScopes',
+            value: {
+              requiredScopes: {},
+              optionalScopes: {
+                'wallet:eip155': {
+                  accounts: evmAccounts.map(
+                    (account) => `wallet:eip155:${account}`,
+                  ),
                 },
               },
-            ],
+              sessionProperties: {},
+              isMultichainOrigin: false,
+            },
           },
+        ],
+      },
     });
 
     return next();
