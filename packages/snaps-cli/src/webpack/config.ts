21290d8dcf84d1dd5401503882cbc8b6186f7932
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2025-05-21 15:11:35 +0200

    Support eval in watch mode

--- packages/snaps-cli/src/webpack/config.ts
+++ packages/snaps-cli/src/webpack/config.ts
@@ -307,7 +307,7 @@ export async function getDefaultConfiguration(
       new SnapsWebpackPlugin({
         manifestPath: config.manifest.path,
         writeManifest: config.manifest.update,
-        eval: !options.watch && options.evaluate,
+        eval: options.evaluate,
       }),
 
       /**
