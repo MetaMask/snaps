9be26efaa7ae17052fe600e30d1cb0ed32612ca9
Frederik Bolding <frederik.bolding@gmail.com>
2025-05-23 15:02:12 +0200

    Move to separate validator

--- packages/snaps-utils/src/manifest/validators/platform-version.ts
+++ packages/snaps-utils/src/manifest/validators/platform-version.ts
@@ -1,34 +1,6 @@
 import { createRequire } from 'module';
-import { minVersion, gt } from 'semver';
 
 import type { ValidatorMeta } from '../validator-types';
-
-/**
- * Determine the production version of the Snaps platform by inspecting
- * the latest GitHub release of the MetaMask extension.
- *
- * @returns The production version of the Snaps platform or null if any error occurred.
- */
-async function determineProductionVersion() {
-  try {
-    // TODO: Cache this check.
-    const latestRelease = await fetch(
-      'https://api.github.com/repos/metamask/metamask-extension/releases/latest',
-    ).then(async (response) => response.json());
-
-    const latestReleaseCommit = latestRelease.target_commitish;
-
-    const packageJson = await fetch(
-      `https://raw.githubusercontent.com/MetaMask/metamask-extension/${latestReleaseCommit}/package.json`,
-    ).then(async (response) => response.json());
-
-    const versionRange = packageJson.dependencies['@metamask/snaps-sdk'];
-
-    return minVersion(versionRange);
-  } catch {
-    return null;
-  }
-}
 
 /**
  * Check if the platform version in manifest matches the version of the Snaps
@@ -46,14 +18,6 @@ export const platformVersion: ValidatorMeta = {
     const packageJson = require.resolve('@metamask/snaps-sdk/package.json');
     // eslint-disable-next-line import-x/no-dynamic-require
     const actualVersion = require(packageJson).version;
-
-    const maximumVersion = await determineProductionVersion();
-
-    if (maximumVersion && gt(actualVersion, maximumVersion)) {
-      context.report(
-        `The "platformVersion" in use is not supported in the production version of MetaMask yet. The current production version is "${maximumVersion.format()}".`,
-      );
-    }
 
     if (!manifestPlatformVersion) {
       context.report(
