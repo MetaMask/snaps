3f39337132b5814b18d92a4e78ca1ca365ec67e6
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2025-06-10 10:59:50 +0200

    Add assertions for `crypto` and `SubtleCrypto`

--- packages/snaps-execution-environments/src/common/endowments/crypto.ts
+++ packages/snaps-execution-environments/src/common/endowments/crypto.ts
@@ -1,6 +1,18 @@
+import { assert } from '@metamask/utils';
+
 import { rootRealmGlobal } from '../globalObject';
 
 export const createCrypto = () => {
+  assert(
+    rootRealmGlobal.crypto,
+    'Crypto endowment requires `globalThis.crypto` to be defined.',
+  );
+
+  assert(
+    rootRealmGlobal.SubtleCrypto,
+    'Crypto endowment requires `globalThis.SubtleCrypto` to be defined.',
+  );
+
   return {
     crypto: harden(rootRealmGlobal.crypto),
     SubtleCrypto: harden(rootRealmGlobal.SubtleCrypto),
