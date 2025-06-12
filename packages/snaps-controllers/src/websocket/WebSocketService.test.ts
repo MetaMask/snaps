bcabd8d15aaa9c43ce77f3f01096a44e4892b739
Frederik Bolding <frederik.bolding@gmail.com>
2025-06-12 12:42:56 +0200

    Add more tests

--- packages/snaps-controllers/src/websocket/WebSocketService.test.ts
+++ packages/snaps-controllers/src/websocket/WebSocketService.test.ts
@@ -1,17 +1,20 @@
-import { HandlerType } from '@metamask/snaps-utils';
+import { HandlerType, isEqual } from '@metamask/snaps-utils';
 import {
   getSnapObject,
   MOCK_ORIGIN,
   MOCK_SNAP_ID,
 } from '@metamask/snaps-utils/test-utils';
+import { stringToBytes } from '@metamask/utils';
 
 import { WebSocketService } from './WebSocketService';
 import {
   getRestrictedWebSocketServiceMessenger,
   getRootWebSocketServiceMessenger,
+  sleep,
 } from '../test-utils';
 
 const MOCK_WEBSOCKET_URI = 'wss://metamask.io';
+const MOCK_WEBSOCKET_BROKEN_URI = 'wss://broken.metamask.io';
 
 class MockWebSocket {
   readonly #origin: string;
@@ -25,10 +28,13 @@ class MockWebSocket {
   }
 
   addEventListener(type: string, listener: EventListener) {
-    if (type === 'open') {
+    if (type === 'open' && this.#origin === MOCK_WEBSOCKET_URI) {
       listener(new Event('open'));
     } else if (type === 'close') {
       this.#closeListener = listener;
+    } else if (type === 'error' && this.#origin === MOCK_WEBSOCKET_BROKEN_URI) {
+      listener(new Event('error'));
+      this.#closeWithCode(1006, '', false);
     } else if (type === 'message') {
       this.#messageListener = listener;
     }
@@ -39,16 +45,37 @@ class MockWebSocket {
       this.#messageListener(
         new MessageEvent('message', { origin: this.#origin, data: 'Pong' }),
       );
+      return;
     }
+
+    if (
+      data instanceof Uint8Array &&
+      isEqual(Array.from(data), Array.from(stringToBytes('Ping')))
+    ) {
+      const bytes = stringToBytes('Pong');
+      this.#messageListener(
+        new MessageEvent('message', {
+          origin: this.#origin,
+          data:
+            // eslint-disable-next-line no-restricted-globals
+            new Blob([bytes]),
+        }),
+      );
+    }
+  }
+
+  #closeWithCode(code: number, reason: string, wasClean: boolean) {
+    // For some reason CloseEvent is not available during test.
+    const event = new Event('close') as any;
+    event.code = code;
+    event.reason = reason;
+    event.wasClean = wasClean;
+    this.#closeListener(event);
   }
 
   close() {
     // Mirrors the browser behavior
-    const event = new Event('close') as any;
-    event.code = 1005;
-    event.reason = '';
-    event.wasClean = true;
-    this.#closeListener(event);
+    this.#closeWithCode(1005, '', true);
   }
 }
 
@@ -130,6 +157,110 @@ describe('WebSocketService', () => {
         snapId: 'npm:@metamask/example-snap',
       },
     );
+  });
+
+  it('supports binary messages', async () => {
+    const rootMessenger = getRootWebSocketServiceMessenger();
+    const messenger = getRestrictedWebSocketServiceMessenger(rootMessenger);
+
+    rootMessenger.registerActionHandler(
+      'SnapController:handleRequest',
+      async ({ handler }) => {
+        if (handler === HandlerType.OnWebSocketEvent) {
+          return null;
+        }
+        throw new Error('Unmocked request');
+      },
+    );
+
+    /* eslint-disable-next-line no-new */
+    new WebSocketService({ messenger });
+
+    const id = await messenger.call(
+      'WebSocketService:open',
+      MOCK_SNAP_ID,
+      MOCK_WEBSOCKET_URI,
+    );
+
+    await messenger.call(
+      'WebSocketService:sendMessage',
+      MOCK_SNAP_ID,
+      id,
+      Array.from(stringToBytes('Ping')),
+    );
+
+    expect(rootMessenger.call).toHaveBeenNthCalledWith(
+      2,
+      'SnapController:handleRequest',
+      {
+        handler: 'onWebSocketEvent',
+        origin: 'metamask',
+        request: {
+          method: '',
+          params: {
+            event: {
+              id,
+              origin: MOCK_WEBSOCKET_URI,
+              type: 'open',
+            },
+          },
+        },
+        snapId: 'npm:@metamask/example-snap',
+      },
+    );
+
+    // Wait for binary decoding.
+    await sleep(1);
+
+    expect(rootMessenger.call).toHaveBeenNthCalledWith(
+      4,
+      'SnapController:handleRequest',
+      {
+        handler: 'onWebSocketEvent',
+        origin: 'metamask',
+        request: {
+          method: '',
+          params: {
+            event: {
+              id,
+              data: {
+                message: [80, 111, 110, 103],
+                type: 'binary',
+              },
+              origin: MOCK_WEBSOCKET_URI,
+              type: 'message',
+            },
+          },
+        },
+        snapId: 'npm:@metamask/example-snap',
+      },
+    );
+  });
+
+  it('throws if the WebSocket connection fails to open', async () => {
+    const rootMessenger = getRootWebSocketServiceMessenger();
+    const messenger = getRestrictedWebSocketServiceMessenger(rootMessenger);
+
+    rootMessenger.registerActionHandler(
+      'SnapController:handleRequest',
+      async ({ handler }) => {
+        if (handler === HandlerType.OnWebSocketEvent) {
+          return null;
+        }
+        throw new Error('Unmocked request');
+      },
+    );
+
+    /* eslint-disable-next-line no-new */
+    new WebSocketService({ messenger });
+
+    await expect(
+      messenger.call(
+        'WebSocketService:open',
+        MOCK_SNAP_ID,
+        MOCK_WEBSOCKET_BROKEN_URI,
+      ),
+    ).rejects.toThrow('An error occurred while opening the WebSocket.');
   });
 
   it('closes an open connection when requested', async () => {
