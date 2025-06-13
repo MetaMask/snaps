d2f53d503e6f039ebb96bfd907f8b3d6287eecf0
Frederik Bolding <frederik.bolding@gmail.com>
2025-06-13 13:20:47 +0200

    More JSDoc

--- packages/snaps-sdk/src/types/handlers/web-socket-event.ts
+++ packages/snaps-sdk/src/types/handlers/web-socket-event.ts
@@ -1,9 +1,26 @@
+/**
+ * The WebSocket open event.
+ *
+ * @property type - Always "open".
+ * @property id - The unique identifier for the WebSocket connection.
+ * @property origin - The origin of the WebSocket connection.
+ */
 export type WebSocketOpenEvent = {
   type: 'open';
   id: string;
   origin: string;
 };
 
+/**
+ * The WebSocket close event.
+ *
+ * @property type - Always "close".
+ * @property id - The unique identifier for the WebSocket connection.
+ * @property origin - The origin of the WebSocket connection.
+ * @property code - The close code sent by the WebSocket server.
+ * @property reason - The string indicating the reason for the connection being closed.
+ * @property wasClean - A boolean value that indicates whether the connection was closed without errors.
+ */
 export type WebSocketCloseEvent = {
   type: 'close';
   id: string;
@@ -13,10 +30,23 @@ export type WebSocketCloseEvent = {
   wasClean: boolean;
 };
 
+/**
+ * The WebSocket text message data.
+ *
+ * @property type - Always "text".
+ * @property message - A string containing the message.
+ */
 export type WebSocketTextMessage = {
   type: 'text';
   message: string;
 };
+
+/**
+ * The WebSocket binary message data.
+ *
+ * @property type - Always "binary".
+ * @property message - An array of bytes containing the message.
+ */
 export type WebSocketBinaryMessage = {
   type: 'binary';
   message: number[];
@@ -26,6 +56,14 @@ export type WebSocketMessageData =
   | WebSocketTextMessage
   | WebSocketBinaryMessage;
 
+/**
+ * The WebSocket message event.
+ *
+ * @property type - Always "message".
+ * @property id - The unique identifier for the WebSocket connection.
+ * @property origin - The origin of the WebSocket connection.
+ * @property data - The message data.
+ */
 export type WebSocketMessage = {
   type: 'message';
   id: string;
