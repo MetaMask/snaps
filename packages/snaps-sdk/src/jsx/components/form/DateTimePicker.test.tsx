bf57fa78f500bf2a95673e896dbe76f2f0928733
Guillaume Roux <hello@guillaumerx.fr>
2025-10-24 12:37:34 +0200

    Add `DateTimePicker` component

---
+++ packages/snaps-sdk/src/jsx/components/form/DateTimePicker.test.tsx
@@ -0,0 +1,66 @@
+import { DateTimePicker } from './DateTimePicker';
+
+describe('DateTimePicker', () => {
+  it('renders a date picker', () => {
+    const element = <DateTimePicker name="test" type="date" />;
+
+    expect(element).toStrictEqual({
+      type: 'DateTimePicker',
+      props: {
+        name: 'test',
+        type: 'date',
+      },
+      key: null,
+    });
+  });
+
+  it('renders a time picker', () => {
+    const element = <DateTimePicker name="test" type="time" />;
+
+    expect(element).toStrictEqual({
+      type: 'DateTimePicker',
+      props: {
+        name: 'test',
+        type: 'time',
+      },
+      key: null,
+    });
+  });
+
+  it('renders a date and time picker', () => {
+    const element = <DateTimePicker name="test" type="datetime" />;
+
+    expect(element).toStrictEqual({
+      type: 'DateTimePicker',
+      props: {
+        name: 'test',
+        type: 'datetime',
+      },
+      key: null,
+    });
+  });
+
+  it('renders a date and time picker with optional props', () => {
+    const element = (
+      <DateTimePicker
+        name="test"
+        type="datetime"
+        placeholder="Select date and time"
+        disabled={true}
+        value="2024-01-01T12:00"
+      />
+    );
+
+    expect(element).toStrictEqual({
+      type: 'DateTimePicker',
+      props: {
+        name: 'test',
+        type: 'datetime',
+        placeholder: 'Select date and time',
+        disabled: true,
+        value: '2024-01-01T12:00',
+      },
+      key: null,
+    });
+  });
+});
