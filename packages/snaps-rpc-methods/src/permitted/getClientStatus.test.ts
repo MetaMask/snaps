ed97239af206ab939584f8d9c238eddea7d21a37
Frederik Bolding <frederik.bolding@gmail.com>
2025-11-05 12:26:10 +0100

    Improve assertion

--- packages/snaps-rpc-methods/src/permitted/getClientStatus.test.ts
+++ packages/snaps-rpc-methods/src/permitted/getClientStatus.test.ts
@@ -14,6 +14,7 @@ describe('snap_getClientStatus', () => {
         hookNames: {
           getIsLocked: true,
           getIsActive: true,
+          getVersion: true,
         },
       });
     });
