6945f3c8eacb41ddad0114b80834e9c15193fc43
Frederik Bolding <frederik.bolding@gmail.com>
2025-12-08 10:52:01 +0100

    wip: Partial simulation support

--- packages/snaps-simulation/src/simulation.ts
+++ packages/snaps-simulation/src/simulation.ts
@@ -62,6 +62,7 @@ import { getOptions } from './options';
 import type { Interface, RunSagaFunction, Store } from './store';
 import { createStore, getCurrentInterface } from './store';
 import { addSnapMetadataToAccount } from './utils/account';
+import { Caveat } from '@metamask/permission-controller';
 
 /**
  * Options for the execution service, without the options that are shared
@@ -93,16 +94,16 @@ export type InstallSnapOptions<
   ) => InstanceType<typeof AbstractExecutionService<unknown>>,
 > =
   ExecutionServiceOptions<Service> extends Record<string, never>
-    ? {
-        executionService: Service;
-        executionServiceOptions?: ExecutionServiceOptions<Service>;
-        options?: SimulationUserOptions;
-      }
-    : {
-        executionService: Service;
-        executionServiceOptions: ExecutionServiceOptions<Service>;
-        options?: SimulationUserOptions;
-      };
+  ? {
+    executionService: Service;
+    executionServiceOptions?: ExecutionServiceOptions<Service>;
+    options?: SimulationUserOptions;
+  }
+  : {
+    executionService: Service;
+    executionServiceOptions: ExecutionServiceOptions<Service>;
+    options?: SimulationUserOptions;
+  };
 
 export type InstalledSnap = {
   snapId: SnapId;
@@ -155,6 +156,8 @@ export type RestrictedMiddlewareHooks = {
    * @returns The metadata for the given Snap.
    */
   getSnap: (snapId: string) => Snap;
+
+  getCaveat: (permission: string, caveatType: string) => Caveat<string, Json>;
 };
 
 export type PermittedMiddlewareHooks = {
@@ -373,7 +376,7 @@ export async function installSnap<
   registerActions(controllerMessenger, runSaga, options, snapId);
 
   // Set up controllers and JSON-RPC stack.
-  const restrictedHooks = getRestrictedHooks(options);
+  const restrictedHooks = getRestrictedHooks(snapId, options, controllerMessenger);
   const permittedHooks = getPermittedHooks(
     snapId,
     snapFiles,
@@ -388,13 +391,24 @@ export async function installSnap<
     options,
   });
 
+  const permissionMiddleware = permissionController.createPermissionMiddleware({
+    origin: snapId,
+  });
+
   const engine = createJsonRpcEngine({
     store,
     restrictedHooks,
     permittedHooks,
-    permissionMiddleware: permissionController.createPermissionMiddleware({
-      origin: snapId,
-    }),
+    permissionMiddleware,
+    isMultichain: false,
+  });
+
+  const multichainEngine = createJsonRpcEngine({
+    store,
+    restrictedHooks,
+    permittedHooks,
+    permissionMiddleware,
+    isMultichain: true,
   });
 
   // Create execution service.
@@ -413,6 +427,16 @@ export async function installSnap<
       // Error function is difficult to test, so we ignore it.
       /* istanbul ignore next 2 */
       pipeline(stream, providerStream, stream, (error) => {
+        if (error && !error.message?.match('Premature close')) {
+          logError(`Provider stream failure.`, error);
+        }
+      });
+
+      const multichainStream = mux.createStream('metamask-multichain-provider');
+      const multichainProviderStream = createEngineStream({ engine: multichainEngine });
+
+      /* istanbul ignore next 2 */
+      pipeline(multichainStream, multichainProviderStream, multichainStream, (error) => {
         if (error && !error.message?.match('Premature close')) {
           logError(`Provider stream failure.`, error);
         }
@@ -460,7 +484,9 @@ export async function installSnap<
  * @returns The hooks for the simulation.
  */
 export function getRestrictedHooks(
+  snapId: SnapId,
   options: SimulationOptions,
+  controllerMessenger: RootControllerMessenger,
 ): RestrictedMiddlewareHooks {
   return {
     getMnemonic: getGetMnemonicImplementation(options.secretRecoveryPhrase),
@@ -470,6 +496,7 @@ export function getRestrictedHooks(
     getIsLocked: () => false,
     getClientCryptography: () => ({}),
     getSnap: getGetSnapImplementation(true),
+    getCaveat: (permission: string, caveatType: string) => controllerMessenger.call('PermissionController:getCaveat', snapId, permission, caveatType),
   };
 }
 
