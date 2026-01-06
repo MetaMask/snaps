0f906f0e4df1a48d163723a75fe3a4ec5bcf70c9
Frederik Bolding <frederik.bolding@gmail.com>
2026-01-06 12:13:11 +0100

    Fix lint

--- packages/snaps-simulation/src/middleware/internal-methods/multichain/get-session.ts
+++ packages/snaps-simulation/src/middleware/internal-methods/multichain/get-session.ts
@@ -2,13 +2,29 @@ import type {
   JsonRpcEngineEndCallback,
   JsonRpcEngineNextCallback,
 } from '@metamask/json-rpc-engine';
-import { Caveat } from '@metamask/permission-controller';
-import type { Json, JsonRpcRequest, PendingJsonRpcResponse } from '@metamask/utils';
+import type { Caveat } from '@metamask/permission-controller';
+import type {
+  Json,
+  JsonRpcRequest,
+  PendingJsonRpcResponse,
+} from '@metamask/utils';
 
 export type GetSessionHandlerHooks = {
   getCaveat: (permission: string, caveatType: string) => Caveat<string, Json>;
 };
 
+/**
+ * A handler that implements `wallet_getSession`.
+ *
+ * @param _request - Incoming JSON-RPC request. Ignored for this specific
+ * handler.
+ * @param response - The outgoing JSON-RPC response, modified to return the
+ * result.
+ * @param _next - The `json-rpc-engine` middleware next handler.
+ * @param end - The `json-rpc-engine` middleware end handler.
+ * @param hooks - The method hooks.
+ * @returns The JSON-RPC response.
+ */
 export async function getSessionHandler(
   _request: JsonRpcRequest,
   response: PendingJsonRpcResponse,
@@ -16,7 +32,6 @@ export async function getSessionHandler(
   end: JsonRpcEngineEndCallback,
   hooks: GetSessionHandlerHooks,
 ) {
-
-  response.result = hooks.getCaveat('endowment:caip25', "authorizedScopes");
+  response.result = hooks.getCaveat('endowment:caip25', 'authorizedScopes');
   return end();
 }
