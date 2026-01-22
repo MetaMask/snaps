0fde0150425e83029c2c15d1ccf038a5223092f9
Guillaume Roux <guillaumeroux123@gmail.com>
2026-01-22 14:54:52 +0100

    fix test

--- packages/snaps-controllers/src/snaps/SnapController.test.tsx
+++ packages/snaps-controllers/src/snaps/SnapController.test.tsx
@@ -10710,13 +10710,13 @@ describe('SnapController', () => {
       expect(updatedSnap.preinstalled).toBe(true);
 
       expect(messenger.call).toHaveBeenNthCalledWith(
-        10,
+        9,
         'PermissionController:revokePermissions',
         { [snapId]: [SnapEndowments.Rpc, SnapEndowments.LifecycleHooks] },
       );
 
       expect(messenger.call).toHaveBeenNthCalledWith(
-        11,
+        10,
         'PermissionController:grantPermissions',
         {
           approvedPermissions: {
