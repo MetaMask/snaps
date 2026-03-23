e5867a2364eb65066bdc694e2a4056fc646fdb82
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2026-03-23 14:38:58 +0100

    Update exports

--- packages/snaps-controllers/src/cronjob/index.ts
+++ packages/snaps-controllers/src/cronjob/index.ts
@@ -1,1 +1,13 @@
-export * from './CronjobController';
+export type {
+  CronjobControllerGetStateAction,
+  CronjobControllerState,
+  CronjobControllerStateChangeEvent,
+  CronjobControllerStateManager,
+} from './CronjobController';
+export { CronjobController } from './CronjobController';
+export type {
+  CronjobControllerInitAction,
+  CronjobControllerScheduleAction,
+  CronjobControllerCancelAction,
+  CronjobControllerGetAction,
+} from './CronjobController-method-action-types';
