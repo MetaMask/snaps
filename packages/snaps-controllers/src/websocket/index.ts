05d60f7a6424075d9884e2e5e410d8a22a557ae8
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2026-03-24 15:44:51 +0100

    Refactor `WebSocketService` to use messenger exposed methods

--- packages/snaps-controllers/src/websocket/index.ts
+++ packages/snaps-controllers/src/websocket/index.ts
@@ -1,1 +1,13 @@
-export * from './WebSocketService';
+export type {
+  WebSocketServiceActions,
+  WebSocketServiceEvents,
+  WebSocketServiceMessenger,
+} from './WebSocketService';
+export { WebSocketService } from './WebSocketService';
+export type {
+  WebSocketServiceCloseAction,
+  WebSocketServiceGetAllAction,
+  WebSocketServiceMethodActions,
+  WebSocketServiceOpenAction,
+  WebSocketServiceSendMessageAction,
+} from './WebSocketService-method-action-types';
