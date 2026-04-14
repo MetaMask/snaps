2d9232648e308a9118fe4096717ccd0b3fb36b45
Frederik Bolding <frederik.bolding@gmail.com>
2026-04-14 16:14:45 +0200

    fix: Make KeyringOrigin caveat explicitly optional

--- packages/snaps-rpc-methods/src/endowments/keyring.ts
+++ packages/snaps-rpc-methods/src/endowments/keyring.ts
@@ -20,7 +20,7 @@ import {
   SnapCaveatType,
 } from '@metamask/snaps-utils';
 import type { Json, NonEmptyArray } from '@metamask/utils';
-import { assert, hasProperty, isObject, isPlainObject } from '@metamask/utils';
+import { hasProperty, isObject, isPlainObject } from '@metamask/utils';
 
 import { createGenericPermissionValidator } from './caveats';
 import { SnapEndowments } from './enum';
@@ -58,7 +58,7 @@ const specificationBuilder: PermissionSpecificationBuilder<
     ],
     endowmentGetter: (_getterOptions?: EndowmentGetterParams) => null,
     validator: createGenericPermissionValidator([
-      { type: SnapCaveatType.KeyringOrigin },
+      { type: SnapCaveatType.KeyringOrigin, optional: true },
       { type: SnapCaveatType.KeyringCapabilities, optional: true },
       { type: SnapCaveatType.MaxRequestTime, optional: true },
     ]),
@@ -155,8 +155,7 @@ export function getKeyringCaveatOrigins(
     (permCaveat) => permCaveat.type === SnapCaveatType.KeyringOrigin,
   ) as Caveat<string, KeyringOrigins> | undefined;
 
-  assert(caveat);
-  return caveat.value;
+  return caveat?.value ?? { allowedOrigins: [] };
 }
 
 /**
